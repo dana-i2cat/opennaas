87e8ff3f5d3c6385c1b46d1d0653079f1d2d6faf
pau_minoves <pau_minoves@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-05-27 14:32:04 +0000

    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@10 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ net.i2cat.mantychore.engine.protocols.netconf/src/main/java/net/i2cat/mantychore/engine/protocols/netconf/ExampleService.java
@@ -0,0 +1,12 @@
+package net.i2cat.mantychore.engine.protocols.netconf;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
