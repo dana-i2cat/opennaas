2cb73cc9ce6ed86ad6330722b341958b28858d48
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-05-27 14:58:01 +0000

    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@14 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ net.i2cat.mantychore.engine.transports.ssh/src/main/java/net/i2cat/mantychore/engine/transports/ssh/ExampleService.java
@@ -0,0 +1,12 @@
+package net.i2cat.mantychore.engine.transports.ssh;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
