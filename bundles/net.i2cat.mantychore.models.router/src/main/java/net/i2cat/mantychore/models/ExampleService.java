090079ca55a343da536056bb421b1f803d7ac17a
pau_minoves <pau_minoves@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-07-13 11:00:23 +0000

    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@64 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.models.router/src/main/java/net/i2cat/mantychore/models/ExampleService.java
@@ -0,0 +1,12 @@
+package net.i2cat.mantychore.models;
+
+/**
+ * Public API representing an example OSGi service
+ */
+public interface ExampleService
+{
+    // public methods go here...
+
+    String scramble( String text );
+}
+
