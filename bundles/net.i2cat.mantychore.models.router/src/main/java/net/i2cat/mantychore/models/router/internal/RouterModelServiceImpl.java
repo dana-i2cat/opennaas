76abf79d1819add96a7a01445efdd7a681dc4746
pau_minoves <pau_minoves@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-07-14 12:14:19 +0000

    remove example code
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@89 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.models.router/src/main/java/net/i2cat/mantychore/models/router/internal/RouterModelServiceImpl.java
+++ bundles/net.i2cat.mantychore.models.router/src/main/java/net/i2cat/mantychore/models/router/internal/RouterModelServiceImpl.java
@@ -1,9 +1,5 @@
 package net.i2cat.mantychore.models.router.internal;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
 import net.i2cat.mantychore.models.router.RouterModelService;
 
 /**
@@ -13,20 +9,6 @@ public final class RouterModelServiceImpl implements RouterModelService {
 	// implementation methods go here...
 
 	public String scramble(String text) {
-		List charList = new ArrayList();
-
-		char[] textChars = text.toCharArray();
-		for (int i = 0; i < textChars.length; i++) {
-			charList.add(new Character(textChars[i]));
-		}
-
-		Collections.shuffle(charList);
-
-		char[] mixedChars = new char[text.length()];
-		for (int i = 0; i < mixedChars.length; i++) {
-			mixedChars[i] = ((Character) charList.get(i)).charValue();
-		}
-
-		return new String(mixedChars);
+		return "not implemented";
 	}
 }
