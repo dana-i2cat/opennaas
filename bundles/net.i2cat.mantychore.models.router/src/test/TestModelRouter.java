dabc50562b82558acc1234df1141dc478442dde7
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-08-04 13:05:09 +0000

    Template to test router model
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@157 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.models.router/src/test/TestModelRouter.java
+++ bundles/net.i2cat.mantychore.models.router/src/test/TestModelRouter.java
@@ -1,7 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import net.i2cat.mantychore.models.router.RouterModelFactory;
+
+import com.iaasframework.capabilities.model.IResourceModel;
+import com.iaasframework.capabilities.model.IResourceModelFactory;
+import com.iaasframework.capabilities.model.ModelException;
+import com.iaasframework.resources.core.descriptor.CapabilityDescriptor;
+import com.iaasframework.resources.core.descriptor.CapabilityProperty;
 
 public class TestModelRouter {
+	IResourceModelFactory	modelFactory	= null;
 
 	public void testWellFormedModel() {
-		
+		modelFactory = new RouterModelFactory();
+
+		List<CapabilityProperty> capabilities = new ArrayList<CapabilityProperty>();
+		CapabilityDescriptor capabDescriptor = new CapabilityDescriptor();
+		capabDescriptor.setCapabilityProperties(capabilities);
+
+		try {
+			IResourceModel routerResource = modelFactory.createResourceModelInstance(capabDescriptor);
+			routerResource.toXML();
+
+		} catch (ModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
+
 }
