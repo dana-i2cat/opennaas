fbeff5a68801525ac847b16a00793e3072e717cf
lisset_diaz <lisset_diaz@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-08-05 09:21:13 +0000

    CapabilityDescriptor Tests
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@159 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.models.router/src/test/TestModelRouter.java
+++ bundles/net.i2cat.mantychore.models.router/src/test/TestModelRouter.java
@@ -3,6 +3,10 @@ import java.util.List;
 
 import net.i2cat.mantychore.models.router.RouterModelFactory;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 import com.iaasframework.capabilities.model.IResourceModel;
 import com.iaasframework.capabilities.model.IResourceModelFactory;
 import com.iaasframework.capabilities.model.ModelException;
@@ -10,24 +14,62 @@ import com.iaasframework.resources.core.descriptor.CapabilityDescriptor;
 import com.iaasframework.resources.core.descriptor.CapabilityProperty;
 
 public class TestModelRouter {
-	IResourceModelFactory	modelFactory	= null;
+	IResourceModelFactory			modelFactory					= null;
 
-	public void testWellFormedModel() {
+	private CapabilityDescriptor	badFormedCapabilityDescriptor	= null;
+	private CapabilityDescriptor	wellFormedCapabilityDescriptor	= null;
+
+	@Before
+	public void setUp() {
 		modelFactory = new RouterModelFactory();
 
+		wellFormedCapabilityDescriptor = new CapabilityDescriptor();
 		List<CapabilityProperty> capabilities = new ArrayList<CapabilityProperty>();
-		CapabilityDescriptor capabDescriptor = new CapabilityDescriptor();
-		capabDescriptor.setCapabilityProperties(capabilities);
+		capabilities.add(createCapability("routerName", "RouterTest"));
+		wellFormedCapabilityDescriptor.setCapabilityProperties(capabilities);
 
+		badFormedCapabilityDescriptor = new CapabilityDescriptor();
+
+	}
+
+	private CapabilityProperty createCapability(String name, String value) {
+		CapabilityProperty capabilityProperty = new CapabilityProperty();
+		capabilityProperty.setName(name);
+		capabilityProperty.setValue(value);
+		return capabilityProperty;
+	}
+
+	@Test
+	public void testBadFormedCapabilityDescriptor() {
 		try {
-			IResourceModel routerResource = modelFactory.createResourceModelInstance(capabDescriptor);
+			modelFactory
+					.createResourceModelInstance(badFormedCapabilityDescriptor);
+
+		} catch (ModelException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Assert.assertTrue(true);
+			return;
+		}
+		Assert.assertTrue(false);
+
+	}
+
+	@Test
+	public void testWellFormedCapabilityDescriptor() {
+		IResourceModel routerResource = null;
+		try {
+			routerResource = modelFactory
+					.createResourceModelInstance(wellFormedCapabilityDescriptor);
 			routerResource.toXML();
 
 		} catch (ModelException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			Assert.assertTrue(false);
+			return;
 		}
-
+		Assert.assertNotNull(routerResource);
 	}
 
 }
