3438f13da1f3bdb81a30fa7ad82b1fdca36abe5f
pau_minoves <pau_minoves@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-10-25 15:40:08 +0000

    delete tests/model (empty skeleton)
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@251 e5892e71-24e5-41bb-b533-28a584b410ba

--- tests/model/src/test/java/CommandSetTest.java
+++
@@ -1,5 +0,0 @@
-import org.apache.felix.karaf.testing.AbstractIntegrationTest;
-
-public class CommandSetTest extends AbstractIntegrationTest {
-
-}
--- tests/model/src/test/java/ProtocolTest.java
+++
@@ -1,5 +0,0 @@
-import org.apache.felix.karaf.testing.AbstractIntegrationTest;
-
-public class ProtocolTest extends AbstractIntegrationTest {
-
-}
