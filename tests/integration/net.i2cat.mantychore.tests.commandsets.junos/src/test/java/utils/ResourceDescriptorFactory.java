2917ead093afcd131e6d9c6fab90d8fa6b175d26
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-11-04 09:22:03 +0000

    It was added utils classes to use in tests with commandsets junos
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@276 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ tests/integration/net.i2cat.mantychore.tests.commandsets.junos/src/test/java/utils/ResourceDescriptorFactory.java
@@ -0,0 +1,99 @@
+package utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import net.i2cat.mantychore.protocols.netconf.NetconfProtocolSession;
+
+import com.iaasframework.capabilities.commandset.ICommandSetConstants;
+import com.iaasframework.capabilities.protocol.IProtocolConstants;
+import com.iaasframework.resources.core.descriptor.CapabilityDescriptor;
+import com.iaasframework.resources.core.descriptor.CapabilityProperty;
+import com.iaasframework.resources.core.descriptor.Information;
+import com.iaasframework.resources.core.descriptor.ResourceDescriptor;
+
+public class ResourceDescriptorFactory {
+
+	public ResourceDescriptor newInstanceDescriptorJunOS() {
+		ResourceDescriptor resourceDescriptor = new ResourceDescriptor();
+
+		/* identifier */
+		resourceDescriptor.setId(UUID.randomUUID().toString());
+
+		/* resource information */
+		Information information = new Information();
+		information.setName("JunOS Test");
+		information.setType("junos"); // Specifies id router
+
+		resourceDescriptor.setInformation(information);
+
+		/* add capability descriptors */
+		List<CapabilityDescriptor> capabilityDescriptors = new ArrayList<CapabilityDescriptor>();
+
+		resourceDescriptor.setCapabilityDescriptors(capabilityDescriptors);
+
+		return resourceDescriptor;
+
+	}
+
+	private CapabilityDescriptor newProtocolDescriptor(long id) {
+
+		CapabilityDescriptor capabDescriptor = new CapabilityDescriptor();
+		/* protocol info */
+		Information information = new Information();
+		information.setDescription("junos protocol");
+		information.setName("junos protocol");
+		information.setType(IProtocolConstants.PROTOCOL);
+		information.setVersion("1.0.0");
+		capabDescriptor.setCapabilityInformation(information);
+
+		List<CapabilityProperty> properties = new ArrayList<CapabilityProperty>();
+
+		properties.add(newProperty(IProtocolConstants.PROTOCOL, "Netconf"));
+		properties.add(newProperty(IProtocolConstants.PROTOCOL_VERSION, "1.0.0"));
+		properties.add(newProperty(NetconfProtocolSession.PROTOCOL_URI, "virtual://foouser:foopass@fooserver:22/okServer"));
+
+		/* netconf parameters */
+
+		capabDescriptor.setCapabilityProperties(properties);
+
+		capabDescriptor.setId(id);
+
+		return capabDescriptor;
+
+	}
+
+	private CapabilityDescriptor newCommandSetDescriptor(long id) {
+
+		CapabilityDescriptor capabDescriptor = new CapabilityDescriptor();
+
+		/* model info */
+		Information information = new Information();
+		information.setDescription("junos commandset");
+		information.setName("junos Commandset");
+		information.setType(ICommandSetConstants.COMMANDSET);
+		information.setVersion("1.0.0");
+		capabDescriptor.setCapabilityInformation(information);
+
+		List<CapabilityProperty> properties = new ArrayList<CapabilityProperty>();
+
+		properties.add(newProperty(ICommandSetConstants.COMMANDSET_NAME, "junos"));
+		properties.add(newProperty(ICommandSetConstants.COMMANDSET_VERSION, "1.0.0"));
+
+		capabDescriptor.setCapabilityProperties(properties);
+
+		capabDescriptor.setId(id);
+
+		return capabDescriptor;
+
+	}
+
+	private CapabilityProperty newProperty(String name, String value) {
+
+		CapabilityProperty property = new CapabilityProperty();
+		property.setName(name);
+		property.setValue(value);
+		return property;
+	}
+}
