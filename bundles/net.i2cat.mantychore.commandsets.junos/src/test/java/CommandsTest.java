22fefa0519f5536d1b931e59a70692afe830f25b
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2010-12-16 16:22:34 +0000

    ->File for mock pattern of the libnetconf ->Test to check a command with libnetconf. It includes a command mock which simulates a call with the IaaS Framework
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@319 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.commandsets.junos/src/test/java/CommandsTest.java
@@ -0,0 +1,111 @@
+import java.net.URI;
+import java.util.List;
+
+import net.i2cat.mantychore.commandsets.junos.JunosCommandFactory;
+import net.i2cat.mantychore.commandsets.junos.commands.RefreshCommand;
+import net.i2cat.mantychore.model.ComputerSystem;
+import net.i2cat.mantychore.model.EthernetPort;
+import net.i2cat.mantychore.model.IPProtocolEndpoint;
+import net.i2cat.mantychore.model.ManagedSystemElement;
+import net.i2cat.mantychore.model.ProtocolEndpoint;
+import net.i2cat.netconf.NetconfSession;
+import net.i2cat.netconf.SessionContext;
+import net.i2cat.netconf.rpc.Query;
+import net.i2cat.netconf.rpc.Reply;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.iaasframework.capabilities.commandset.CommandException;
+import com.iaasframework.resources.core.message.CapabilityMessage;
+import com.iaasframework.resources.core.message.ICapabilityMessage;
+
+public class CommandsTest {
+	Logger	log	= LoggerFactory
+						.getLogger(CommandsTest.class);
+
+	public class RefreshMockCommand extends RefreshCommand {
+		SessionContext	sessionContext;
+		NetconfSession	session;
+
+		RefreshMockCommand() {
+			try {
+				sessionContext = new SessionContext();
+			} catch (ConfigurationException e) {
+				log.error(e.getMessage());
+
+			}
+		}
+
+		@Override
+		public void responseReceived(ICapabilityMessage responseMessage) throws CommandException {
+			// TODO Auto-generated method stub
+			super.responseReceived(responseMessage);
+		}
+
+		/**
+		 * Library netconf
+		 */
+		public void sendCommandToProtocol(Object command) {
+			try {
+				sessionContext.setURI(new URI("mock://foo:bar@foo:22/okServer"));
+
+				session = new NetconfSession(sessionContext);
+				session.connect();
+				Reply reply = session.sendSyncQuery((Query) command);
+				response = new CapabilityMessage();
+				response.setMessage(reply.getContain());
+
+				session.disconnect();
+
+			} catch (Exception e) {
+				log.error(e.getMessage());
+			}
+
+		}
+
+	}
+
+	public CommandsTest() {
+
+	}
+
+	@Test
+	public void refreshTest() {
+		log.info("Preparing refresh test....");
+
+		JunosCommandFactory factory = new JunosCommandFactory();
+		try {
+			RefreshMockCommand refreshCommand = new RefreshMockCommand();
+			refreshCommand.executeCommand();
+			ComputerSystem routerModel = new ComputerSystem();
+
+			refreshCommand.parseResponse(routerModel);
+
+			List<ManagedSystemElement> listManagedSystemElems = routerModel.getManagedSystemElements();
+			for (ManagedSystemElement elem : listManagedSystemElems) {
+				EthernetPort ethernet = (EthernetPort) elem;
+				log.info(ethernet.getOtherPortType());
+				for (ProtocolEndpoint protocolEndpoint : ethernet.getProtocolEndpoints()) {
+					if (protocolEndpoint instanceof IPProtocolEndpoint) {
+						IPProtocolEndpoint ipProtocolEndpoint = (IPProtocolEndpoint) protocolEndpoint;
+						log.info("ipv4: " + ipProtocolEndpoint.getIPv4Address());
+						log.info("ipv6: " + ipProtocolEndpoint.getIPv6Address());
+
+					}
+
+				}
+
+			}
+
+		} catch (CommandException e) {
+			Assert.fail(e.getMessage());
+		}
+		log.info("Finished!!!");
+
+	}
+
+}
