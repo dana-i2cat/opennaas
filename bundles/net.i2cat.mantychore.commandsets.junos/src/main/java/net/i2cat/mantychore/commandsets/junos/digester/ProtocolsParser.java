347a25658d89e89d06c120fd068c2a305b0f19a5
evelyn_torras <evelyn_torras@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-01-11 09:04:34 +0000

    ProtocolParser updated
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@346 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.commandsets.junos/src/main/java/net/i2cat/mantychore/commandsets/junos/digester/ProtocolsParser.java
+++ bundles/net.i2cat.mantychore.commandsets.junos/src/main/java/net/i2cat/mantychore/commandsets/junos/digester/ProtocolsParser.java
@@ -1,8 +1,5 @@
 package net.i2cat.mantychore.commandsets.junos.digester;
 
-import net.i2cat.mantychore.model.IPProtocolEndpoint;
-import net.i2cat.mantychore.model.NextHopIPRoute;
-
 import org.apache.commons.digester.Digester;
 import org.apache.commons.digester.RuleSetBase;
 
@@ -23,11 +20,7 @@ public class ProtocolsParser extends DigesterEngine {
 		public void addRuleInstances(Digester arg0) {
 			// FIXME the path pattern can't be global , must distinguish between
 			// routers
-			addObjectCreate("*/protocols/ospf", NextHopIPRoute.class);
-			addMyRule("*/routing-options/static/route/name", "setDestinationAddress", 1);
-			addMyRule("*/routing-options/static/route/next-hop", "setNextHop", 1);
-			/* Add NesxtHopIpRoute to the parent */
-			addSetNext("*/routing-options/static/route", "addInterface");
+			// TODO add rules to parser protocols bgp ospf
 		}
 	}
 
@@ -39,62 +32,4 @@ public class ProtocolsParser extends DigesterEngine {
 		ruleSet = new ParserRuleSet(prefix);
 	}
 
-	public void addInterface(NextHopIPRoute ipRoute) {
-		String location = ipRoute.getDestinationAddress();
-		// TODO implements the case where is needed merge the classes, if it is.
-		// also, add merge method into the class
-		mapElements.put(location, ipRoute);
-
-	}
-
-	/* Parser methods */
-
-	public void setDestinationAddress(String ipv4) {
-
-		NextHopIPRoute ipRoute = (NextHopIPRoute) peek();
-		try {
-
-			// Parsing ip/mask
-			String shortMask = ipv4.split("/")[1];
-			String ip = ipv4.split("/")[0];
-			String maskIpv4 = IPUtilsHelper.parseShortToLongIpv4NetMask(shortMask);
-
-			// adding to the model
-			ipRoute.setDestinationAddress(ip);
-			ipRoute.setPrefixLength(Byte.parseByte(shortMask));
-			ipRoute.setDestinationMask(maskIpv4);
-			ipRoute.setIsStatic(true);
-
-		} catch (Exception e) {
-			log.error(e.getMessage());
-		}
-	}
-
-	public void setNextHop(String nextHop) {
-
-		NextHopIPRoute nextHopIPRoute = (NextHopIPRoute) peek(0);
-		// creating IpEndPoint that will be the next hop
-		IPProtocolEndpoint ipNextHop = new IPProtocolEndpoint();
-		ipNextHop.setIPv4Address(nextHop);
-		try {
-			// adding next hop to the destination address (NextHopIPRoute class)
-			nextHopIPRoute.setRouteUsesEndPoint(ipNextHop);
-		} catch (Exception e) {
-			log.error(e.getMessage());
-		}
-	}
-
-	public String toPrint() {
-
-		String str = "" + '\n';
-		for (String key : mapElements.keySet()) {
-			NextHopIPRoute port = (NextHopIPRoute) mapElements.get(key);
-			str += "- Routing options " + '\n';
-			str += "IP adress " + port.getDestinationAddress() + '\n';
-			str += "Mask " + port.getDestinationMask() + '\n';
-			str += "is Static " + String.valueOf(port.isIsStatic()) + '\n';
-			str += "Next hop " + port.getRouteUsesEndPoint().getIPv4Address() + '\n';
-		}
-		return str;
-	}
 }
