a95ea3d2e57093dfde4783fc21c1f8272a146a54
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-01-17 17:28:54 +0000

    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@366 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.queueManager/src/main/java/net/i2cat/mantychore/queueManager/IAction.java
+++
@@ -1,9 +0,0 @@
-package net.i2cat.mantychore.queueManager;
-
-import java.util.List;
-
-import com.iaasframework.capabilities.commandset.ICommand;
-
-public interface IAction {
-	public List<ICommand> getCommands();
-}
