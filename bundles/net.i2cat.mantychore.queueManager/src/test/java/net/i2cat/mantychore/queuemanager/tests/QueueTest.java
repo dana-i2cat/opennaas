5debef1c86959c7b6a6923647e31d98e8b2c9734
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-01-18 09:20:30 +0000

    Added tests and fixed problems with the queue and tests
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@372 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.queueManager/src/test/java/net/i2cat/mantychore/queuemanager/tests/QueueTest.java
@@ -0,0 +1,83 @@
+package net.i2cat.mantychore.queuemanager.tests;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.i2cat.mantychore.queuemanager.Action;
+import net.i2cat.mantychore.queuemanager.Command;
+import net.i2cat.mantychore.queuemanager.QueueManagerFactory;
+import net.i2cat.mantychore.queuemanager.QueueManagerService;
+import net.i2cat.mantychore.queuemanager.mock.Interface;
+import net.i2cat.mantychore.queuemanager.mock.MockCommand;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class QueueTest {
+
+	static Logger	log	= LoggerFactory
+								.getLogger(QueueTest.class);
+
+	@Test
+	public void firstActionTest() {
+		String resourceId = "junos";
+		String protocolId = "netconf";
+
+		log.info("Calling queue factory");
+		QueueManagerFactory queueFactory = new QueueManagerFactory();
+		QueueManagerService queueManager = queueFactory.createQueueManager(resourceId);
+
+		log.info("Creating action");
+
+		Action action = new Action();
+		action.setResourceId(resourceId);
+		action.setProtocolId(protocolId);
+
+		List<Interface> interfaces = new ArrayList<Interface>();
+		action.setModelToUpdate(interfaces);
+
+		List<Command> commands = new ArrayList<Command>();
+
+		log.info("Creating commands");
+		Command command1 = new MockCommand();
+		command1.setCommandId("prepare");
+		action.setPrepare(command1);
+
+		Command command2 = new MockCommand();
+		command2.setCommandId("command1");
+		commands.add(command2);
+
+		Command command3 = new MockCommand();
+		command3.setCommandId("command2");
+		commands.add(command3);
+		action.setCommands(commands);
+
+		Command command4 = new MockCommand();
+		command4.setCommandId("commit");
+		action.setCommit(command4);
+
+		log.info("executing...");
+		queueManager.queueAction(action);
+
+		if (queueManager.getActions().size() != 1) {
+			Assert.fail("Ups, the action disappeared");
+		}
+
+		if (queueManager.getActions().get(0).getCommands().size() != 2) {
+			Assert.fail("Ups, the commands disappeared");
+		}
+
+		queueManager.execute();
+
+		for (Interface interf : interfaces) {
+			log.info("interface");
+			log.info(interf.getIpv4());
+			log.info(interf.getIpv6());
+			log.info("--------");
+
+		}
+
+	}
+}
