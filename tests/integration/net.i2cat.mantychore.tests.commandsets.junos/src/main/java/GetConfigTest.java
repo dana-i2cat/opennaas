9a1a15633f054518532b167d489d760106648456
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-02-01 16:43:52 +0000

    Added skeleton for testing commands
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@426 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ tests/integration/net.i2cat.mantychore.tests.commandsets.junos/src/main/java/GetConfigTest.java
@@ -0,0 +1,185 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import net.i2cat.mantychore.commons.Action;
+import net.i2cat.mantychore.commons.Command;
+import net.i2cat.mantychore.protocols.sessionmanager.ProtocolException;
+import net.i2cat.mantychore.protocols.sessionmanager.ProtocolSessionContext;
+import net.i2cat.mantychore.queuemanager.IQueueManagerFactory;
+import net.i2cat.mantychore.queuemanager.IQueueManagerService;
+import net.i2cat.netconf.rpc.Query;
+import net.i2cat.netconf.rpc.QueryFactory;
+
+import org.apache.felix.karaf.testing.AbstractIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RunWith(JUnit4TestRunner.class)
+public class PAXTest extends AbstractIntegrationTest {
+
+	static Logger			log				= LoggerFactory
+													.getLogger(PAXTest.class);
+
+	IQueueManagerService	queueManager;
+	String					deviceID		= "junos";
+
+	@Inject
+	BundleContext			bundleContext	= null;
+
+	@Configuration
+	public static Option[] configure() {
+		return PAXHelper.newQueueManagerTest();
+	}
+
+	private void listBundles() {
+		Bundle b = null;
+		String listBundles = "";
+		for (int i = 0; i < bundleContext.getBundles().length; i++) {
+			b = bundleContext.getBundles()[i];
+			listBundles += b.toString() + " : " + getStateString(b.getState()) + '\n';
+			if (getStateString(b.getState()).equals("INSTALLED")) {
+				try {
+					b.start();
+				} catch (Exception e) {
+					listBundles += "ERROR: " + e.getMessage() + '\n';
+					e.printStackTrace();
+				}
+			}
+		}
+		log.info(listBundles);
+	}
+
+	private static String getStateString(int value) {
+		if (value == Bundle.ACTIVE) {
+			return "ACTIVE";
+		} else if (value == Bundle.INSTALLED) {
+			return "INSTALLED";
+		} else if (value == Bundle.RESOLVED) {
+			return "RESOLVED";
+		} else if (value == Bundle.UNINSTALLED) {
+			return "UNINSTALLED";
+		}
+
+		return "UNKNOWN";
+	}
+
+	/* initialize client */
+
+	private void prepareQueueManagerTest() {
+		try {
+			log.info("Starting the test");
+
+			/* get queueManager as a service */
+			log.info("Getting queue manager factory...");
+			IQueueManagerFactory queueManagerFactory = getOsgiService(IQueueManagerFactory.class);
+			log.info("Getting queue manager...");
+			queueManager = queueManagerFactory.createQueueManager(deviceID);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error(e.getMessage());
+			Assert.fail();
+		}
+	}
+
+	@Test
+	public void testListAction() {
+		log.info("This is running inside Equinox. With all configuration set up like you specified. ");
+		try {
+			Thread.sleep(100000);
+		} catch (InterruptedException e) {
+			log.error(e.getMessage());
+			e.printStackTrace();
+		}
+
+		/* list bundles */
+		listBundles();
+
+		/* prepare queue manager test */
+		prepareQueueManagerTest();
+
+		Action action = newAction();
+		queueManager.queueAction(action, newSessionContextNetconf());
+		Action action2 = newAction();
+		queueManager.queueAction(action2, newSessionContextNetconf());
+		Action action3 = newAction();
+		queueManager.queueAction(action3, newSessionContextNetconf());
+		Action action4 = newAction();
+		queueManager.queueAction(action4, newSessionContextNetconf());
+		Action action5 = newAction();
+		queueManager.queueAction(action5, newSessionContextNetconf());
+
+		/* ------------------------ */
+
+		List<Action> actions = queueManager.getActions();
+		if (actions == null || actions.size() != 5)
+			Assert.fail("the queuemanager does not include any action");
+
+		try {
+			queueManager.execute();
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+			log.error(e.getMessage());
+			Assert.fail(e.getMessage());
+		}
+
+		queueManager.empty();
+		actions = queueManager.getActions();
+
+		if (actions != null && actions.size() != 0)
+			Assert.fail("the queuemanager is not empty");
+	}
+
+	private Action newAction() {
+		Action action = new Action();
+
+		Command command = new MockCommand();
+
+		List<Command> commands = new ArrayList<Command>();
+		commands.add(command);
+		action.setCommands(commands);
+
+		return action;
+	}
+
+	private ProtocolSessionContext newSessionContextNetconf() {
+		ProtocolSessionContext protocolSessionContext = new ProtocolSessionContext();
+		protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL_URI, "ssh://i2cat:mant6WWe@lola.hea.net:22/netconf");
+		protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL, "netconf");
+		// ADDED
+		return protocolSessionContext;
+
+	}
+
+	class MockCommand extends Command {
+
+		@Override
+		public void initialize() {
+			log.info("Initialization");
+
+		}
+
+		@Override
+		public Object message() {
+			log.info("sending a message");
+			Query query = QueryFactory.newKeepAlive();
+			return query;
+		}
+
+		@Override
+		public void parseResponse(Object arg0, Object arg1) {
+			log.info("It is parsed the message");
+
+		}
+	}
+
+}
---
+++ tests/integration/net.i2cat.mantychore.tests.commandsets.junos/src/main/java/PAXHelper.java
@@ -0,0 +1,112 @@
+import static org.ops4j.pax.exam.CoreOptions.equinox;
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.CoreOptions.options;
+import static org.ops4j.pax.exam.CoreOptions.systemProperty;
+import static org.ops4j.pax.exam.CoreOptions.waitForFrameworkStartup;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.excludeDefaultRepositories;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.repositories;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.scanFeatures;
+import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.workingDirectory;
+
+import org.apache.felix.karaf.testing.Helper;
+import org.ops4j.pax.exam.Option;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+//import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
+public class PAXHelper {
+
+	public static final String		WORKING_DIRECTORY				= "target/paxrunner/features/";
+
+	public static final Option		MANTYCHORE_REPOS				= repositories(
+																			"http://repository.inocybe.ca/content/groups/public/",
+																			"http://repository.inocybe.ca/content/groups/public-snapshots/",
+																			"http://repo.fusesource.com/maven2",
+																			"http://repo1.maven.org/maven2");
+	public static final Option[]	REPOSITORIES					= options(excludeDefaultRepositories(), MANTYCHORE_REPOS);
+
+	/* specify log level */
+	public static final Option[]	HELPER_DEFAULT_OPTIONS			= Helper.getDefaultOptions(
+																			systemProperty("org.ops4j.pax.logging.DefaultServiceLog.level")
+																					.value("INFO"));
+
+	public static final Option		OPT_WORKING_DIRECTORY			= workingDirectory(WORKING_DIRECTORY);
+
+	/* service mix features */
+
+	public static final String		SERVICE_MIX_FEATURES_REPO_V4_3	= "mvn:org.apache.servicemix/apache-servicemix/4.3.0-fuse-01-00/xml/features";
+
+	public static final String[]	SERVICE_MIX_FEATURES_V4_3		= { "servicemix-cxf-bc", "camel", "camel-cxf", "camel-jms", "camel-jaxb" };
+
+	public static final Option		OPT_SERVICE_MIX_FEATURES		= scanFeatures(SERVICE_MIX_FEATURES_REPO_V4_3, SERVICE_MIX_FEATURES_V4_3);
+	/* iaas features */
+
+	public static final String		IAAS_FEATURES_REPO				= "mvn:com.iaasframework/iaas-framework/1.0.0/xml/features";
+
+	public static final String[]	IAAS_FEATURES					= { "iaas-thirdparty", "iaas-core", "iaas-capabilities", "iaas-extras" };
+
+	public static final Option		OPT_IAAS_FEATURES				= scanFeatures(IAAS_FEATURES_REPO, IAAS_FEATURES);
+
+	/* mantychore features */
+
+	private static final String		MTCHORE_FEATURES_REPO			= "mvn:net.i2cat.mantychore/mantychore/1.0.0-SNAPSHOT/xml/features";
+
+	private static final String[]	MTCHORE_FEATURES				= { "i2cat-mantychore-core" };
+	private static final Option		OPT_MANTYCHORE_FEATURES			= scanFeatures(MTCHORE_FEATURES_REPO, MTCHORE_FEATURES);
+
+	static Logger					log								= LoggerFactory.getLogger(PAXHelper.class);
+
+	/**
+	 * Bundle loader
+	 * 
+	 * @param bundleDescriptor
+	 * @return
+	 */
+	private static Option addBundle(String[] bundleDescriptor) {
+		if (bundleDescriptor.length == 3) {
+			return mavenBundle().groupId(bundleDescriptor[0]).artifactId(bundleDescriptor[1]).version(bundleDescriptor[2]);
+		} else {
+			return mavenBundle().groupId(bundleDescriptor[0]).artifactId(bundleDescriptor[1]);
+		}
+
+	}
+
+	public static long	minInMillis	= 60;	// 60 secs * 1000 (1 milli)
+
+	public static Option[] newSimpleTest() {
+		long waitInMillis = minInMillis * 1000;
+		Option[] optssimpleTest = combine(HELPER_DEFAULT_OPTIONS
+				, OPT_WORKING_DIRECTORY // directory where pax-runner saves OSGi
+				// bundles
+				// ,
+				// vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5006")
+				, waitForFrameworkStartup() // wait for a length of time
+				, equinox());
+		Option[] opts_with_repos = combine(optssimpleTest, REPOSITORIES); // repositories
+		return opts_with_repos;
+
+	}
+
+	public static Option[] newServiceMixTest() {
+		Option[] optssimpleTest = newSimpleTest();
+
+		Option[] optsServiceMix = combine(optssimpleTest, OPT_SERVICE_MIX_FEATURES); // service
+
+		return optsServiceMix;
+
+	}
+
+	public static Option[] newExampleTest() {
+		return combine(HELPER_DEFAULT_OPTIONS, OPT_SERVICE_MIX_FEATURES);
+	}
+
+	public static Option[] newQueueManagerTest() {
+
+		Option[] optsServiceMix = newServiceMixTest();
+		Option[] opts_with_Mantychore = combine(optsServiceMix, OPT_MANTYCHORE_FEATURES); // service
+
+		return opts_with_Mantychore;
+	}
+
+}
