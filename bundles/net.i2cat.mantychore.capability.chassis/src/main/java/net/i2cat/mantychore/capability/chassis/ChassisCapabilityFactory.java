5eabf2c0aa3131a08b8bdd194b79b132def228fd
evelyn_torras <evelyn_torras@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-03-02 15:37:16 +0000

    Created new bundle capability.chassis
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@504 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.capability.chassis/src/main/java/net/i2cat/mantychore/capability/chassis/ChassisCapabilityFactory.java
@@ -0,0 +1,17 @@
+package net.i2cat.mantychore.capability.chassis;
+
+import net.i2cat.nexus.resources.capability.CapabilityException;
+import net.i2cat.nexus.resources.capability.ICapability;
+import net.i2cat.nexus.resources.capability.ICapabilityFactory;
+import net.i2cat.nexus.resources.descriptor.CapabilityDescriptor;
+
+public class ChassisCapabilityFactory implements ICapabilityFactory {
+
+	public ICapability create(CapabilityDescriptor moduleDescriptor, String resourceId) throws CapabilityException {
+		// TODO Auto-generated method stub
+		ChassisCapability chassiscapability = new ChassisCapability(resourceId);
+		chassiscapability.setCapabilityDescriptor(moduleDescriptor);
+		return chassiscapability;
+	}
+
+}
