eb51045889a307b999cd6e60bc34efd0e0224043
evelyn_torras <evelyn_torras@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-03-15 16:26:45 +0000

    Refreshcommand updated parsing response TEmplate SetIp updated ( correct Netconf taggin configuration) SimpleTestCLient working
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@589 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.commandsets.junos/src/main/java/net/i2cat/mantychore/commandsets/junos/commands/RefreshCommand.java
+++ bundles/net.i2cat.mantychore.commandsets.junos/src/main/java/net/i2cat/mantychore/commandsets/junos/commands/RefreshCommand.java
@@ -1,14 +1,13 @@
 package net.i2cat.mantychore.commandsets.junos.commands;
 
 import java.io.ByteArrayInputStream;
+import java.io.FileWriter;
 import java.io.IOException;
-import java.io.StringReader;
 
 import net.i2cat.mantychore.commandsets.junos.digester.DigesterEngine;
 import net.i2cat.mantychore.commandsets.junos.digester.IPConfigurationInterfaceParser;
 import net.i2cat.mantychore.model.LogicalDevice;
 import net.i2cat.netconf.rpc.QueryFactory;
-import net.i2cat.netconf.rpc.RPCElement;
 import net.i2cat.netconf.rpc.Reply;
 
 import org.slf4j.Logger;
@@ -17,22 +16,20 @@ import org.xml.sax.SAXException;
 
 public class RefreshCommand extends JunosCommand {
 
-	public static final String REFRESH = "refresh";
-	public static final String TEMPLATE = "/VM_files/getconfiguration.vm";
+	public static final String	REFRESH		= "refresh";
+	public static final String	TEMPLATE	= "/VM_files/getconfiguration.vm";
 
-	//FIXME HOW WE SEND THE REFRESH COMMAND?
-	private String source = "running";
-	private String filter = null;
-	private String attrFilter = null;
+	// FIXME HOW WE SEND THE REFRESH COMMAND?
+	private String				source		= "running";
+	private String				filter		= null;
+	private String				attrFilter	= null;
 
 	/** logger **/
-	Logger log = LoggerFactory.getLogger(JunosCommand.class);
+	Logger						log			= LoggerFactory.getLogger(JunosCommand.class);
 
 	public RefreshCommand() {
 		super(REFRESH, TEMPLATE);
 	}
-	
-
 
 	@Override
 	public Object sendQuery() {
@@ -43,52 +40,53 @@ public class RefreshCommand extends JunosCommand {
 
 	@Override
 	public void parseResponse(Object response, Object model) {
-		
-		 try {
-		 net.i2cat.mantychore.model.System routerModel = (net.i2cat.mantychore.model.System) model;
-		 DigesterEngine logicalInterfParser = new IPConfigurationInterfaceParser();
-		 logicalInterfParser.init();
-		 
-		 //IT MUST TO RECEIVE A REPLY, THIS ASSERT CAN NOT HAPPEN
 
-		 /* getting interface information */
-		 Reply rpcReply = (Reply)response;
-		 String message = rpcReply.getContain();
-		 logicalInterfParser.configurableParse(new ByteArrayInputStream(message.getBytes()));
-		 
-		 for (String keyInterf: logicalInterfParser.getMapElements().keySet()) {
-			 routerModel.addLogicalDevice((LogicalDevice) logicalInterfParser.getMapElements().get(keyInterf));
-		 }
-		
-		 
-//		 /* Parse routing options info */
-//		 DigesterEngine routingOptionsfParser = new RoutingOptionsParser();
-//		 routingOptionsfParser.init();
-//		
-//		 if (response.getMessage() != null) {
-//		 routingOptionsfParser.configurableParse(new ByteArrayInputStream(response.getMessage().getBytes("UTF-8")));
-//		 // add to the router model
-//		 for (String keyInterf : routingOptionsfParser.getMapElements().keySet()) {
-//		 NextHopRoute nh = (NextHopRoute) routingOptionsfParser.getMapElements().get(keyInterf);
-//		 routerModel.addNextHopRoute(nh);
-//		 }
-//		 }
-		
-		 } catch (IOException e) {
-		 log.error(e.getMessage());
-		 } catch (SAXException e) {
-		 log.error(e.getMessage());
-		 }
-		
+		try {
+			net.i2cat.mantychore.model.System routerModel = (net.i2cat.mantychore.model.System) model;
+			DigesterEngine logicalInterfParser = new IPConfigurationInterfaceParser();
+			logicalInterfParser.init();
+
+			// IT MUST TO RECEIVE A REPLY, THIS ASSERT CAN NOT HAPPEN
+
+			/* getting interface information */
+			Reply rpcReply = (Reply) response;
+			String message = rpcReply.getContain();
+
+			FileWriter file = new FileWriter("./response_command.txt");
+			file.write(message);
+			file.flush();
+			file.close();
+
+			logicalInterfParser.configurableParse(new ByteArrayInputStream(message.getBytes()));
+
+			for (String keyInterf : logicalInterfParser.getMapElements().keySet()) {
+				routerModel.addLogicalDevice((LogicalDevice) logicalInterfParser.getMapElements().get(keyInterf));
+			}
+
+			// /* Parse routing options info */
+			// DigesterEngine routingOptionsfParser = new RoutingOptionsParser();
+			// routingOptionsfParser.init();
+			//		
+			// if (response.getMessage() != null) {
+			// routingOptionsfParser.configurableParse(new ByteArrayInputStream(response.getMessage().getBytes("UTF-8")));
+			// // add to the router model
+			// for (String keyInterf : routingOptionsfParser.getMapElements().keySet()) {
+			// NextHopRoute nh = (NextHopRoute) routingOptionsfParser.getMapElements().get(keyInterf);
+			// routerModel.addNextHopRoute(nh);
+			// }
+			// }
+
+		} catch (IOException e) {
+			log.error(e.getMessage());
+		} catch (SAXException e) {
+			log.error(e.getMessage());
+		}
+
 	}
-
-
 
 	public String getSource() {
 		return source;
 	}
-
-
 
 	public void setSource(String source) {
 		this.source = source;
--- tests/integration/net.i2cat.mantychore.tests.simpleclient/src/test/java/net/i2cat/mantychore/simpleclient/tests/SimpleClientTest.java
+++ tests/integration/net.i2cat.mantychore.tests.simpleclient/src/test/java/net/i2cat/mantychore/simpleclient/tests/SimpleClientTest.java
@@ -1,6 +1,5 @@
 package net.i2cat.mantychore.simpleclient.tests;
 
-
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.OptionUtils.combine;
 import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
@@ -8,6 +7,28 @@ import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.i2cat.mantychore.actionsets.junos.BasicActionSetFactory;
+import net.i2cat.mantychore.actionsets.junos.ChassisActionSetFactory;
+import net.i2cat.mantychore.capability.chassis.ChassisCapability;
+import net.i2cat.mantychore.capability.chassis.IChassisCapabilityFactory;
+import net.i2cat.mantychore.commons.Action;
+import net.i2cat.mantychore.commons.ActionResponse;
+import net.i2cat.mantychore.commons.CommandException;
+import net.i2cat.mantychore.commons.IActionSetFactory;
+import net.i2cat.mantychore.model.ComputerSystem;
+import net.i2cat.mantychore.model.EthernetPort;
+import net.i2cat.mantychore.model.IPProtocolEndpoint;
+import net.i2cat.mantychore.model.LogicalDevice;
+import net.i2cat.mantychore.queuemanager.IQueueManagerFactory;
+import net.i2cat.mantychore.queuemanager.IQueueManagerService;
+import net.i2cat.nexus.protocols.sessionmanager.ProtocolException;
+import net.i2cat.nexus.protocols.sessionmanager.ProtocolSessionContext;
+import net.i2cat.nexus.tests.IntegrationTestsHelper;
+
+import org.apache.karaf.testing.AbstractIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.ops4j.pax.exam.Inject;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
@@ -15,60 +36,34 @@ import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.osgi.framework.BundleContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.apache.karaf.testing.AbstractIntegrationTest;
-import org.junit.Assert;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-
-import net.i2cat.mantychore.actionsets.junos.BasicActionSetFactory;
-import net.i2cat.mantychore.actionsets.junos.ChassisActionSetFactory;
-import net.i2cat.mantychore.commons.IActionSetFactory;
-import net.i2cat.mantychore.capability.chassis.ChassisCapability;
-import net.i2cat.mantychore.capability.chassis.ChassisCapabilityFactory;
-import net.i2cat.mantychore.capability.chassis.IChassisCapabilityFactory;
-import net.i2cat.mantychore.commons.Action;
-import net.i2cat.mantychore.commons.ActionResponse;
-import net.i2cat.mantychore.commons.CommandException;
-import net.i2cat.nexus.protocols.sessionmanager.ProtocolException;
-import net.i2cat.nexus.protocols.sessionmanager.ProtocolSessionContext;
-import net.i2cat.nexus.tests.IntegrationTestsHelper;
-import net.i2cat.mantychore.model.ComputerSystem;
-import net.i2cat.mantychore.model.EthernetPort;
-import net.i2cat.mantychore.model.IPProtocolEndpoint;
-import net.i2cat.mantychore.model.LogicalDevice;
-import net.i2cat.mantychore.model.System;
-import net.i2cat.mantychore.queuemanager.IQueueManagerFactory;
-import net.i2cat.mantychore.queuemanager.IQueueManagerService;
 
 @RunWith(JUnit4TestRunner.class)
 public class SimpleClientTest extends AbstractIntegrationTest {
 
-	static Logger			log				= LoggerFactory
-													.getLogger(SimpleClientTest.class);
+	static Logger			log					= LoggerFactory
+														.getLogger(SimpleClientTest.class);
 	IQueueManagerService	queueManager;
 
-
-	String					deviceID		= "junos";
-	String					queueID			= "queue";
+	String					deviceID			= "junos";
+	String					queueID				= "queue";
 	ProtocolSessionContext	protocolSessionContext;
-	ChassisCapability chassisCapability;
-	
-	IActionSetFactory basicActionFactory = new BasicActionSetFactory();
-	
-	IActionSetFactory actionFactory = new ChassisActionSetFactory();
-	
-	
+	ChassisCapability		chassisCapability;
+
+	IActionSetFactory		basicActionFactory	= new BasicActionSetFactory();
+
+	IActionSetFactory		actionFactory		= new ChassisActionSetFactory();
+
 	@Inject
-	BundleContext			bundleContext	= null;
+	BundleContext			bundleContext		= null;
 
 	@Configuration
 	public static Option[] configure() {
 		return combine(
-					   IntegrationTestsHelper.getMantychoreTestOptions(),
-					   mavenBundle().groupId("net.i2cat.mantychore.capability").artifactId("net.i2cat.mantychore.capability.chassis"),					   
-					   mavenBundle().groupId("net.i2cat.nexus").artifactId("net.i2cat.nexus.tests.helper")
-//					   ,vmOption( "-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005" )
-					   
+						IntegrationTestsHelper.getMantychoreTestOptions(),
+						mavenBundle().groupId("net.i2cat.mantychore.capability").artifactId("net.i2cat.mantychore.capability.chassis"),
+						mavenBundle().groupId("net.i2cat.nexus").artifactId("net.i2cat.nexus.tests.helper")
+						//, vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
+
 		);
 	}
 
@@ -90,7 +85,6 @@ public class SimpleClientTest extends AbstractIntegrationTest {
 			Assert.fail();
 		}
 	}
-	
 
 	private void prepareChassisCapability() {
 		try {
@@ -98,18 +92,18 @@ public class SimpleClientTest extends AbstractIntegrationTest {
 
 			/* get queueManager as a service */
 			log.info("Getting chassis capability factory...");
-			
+
 			IChassisCapabilityFactory chassisFactory = getOsgiService(IChassisCapabilityFactory.class, 5000);
-			
+
 			log.info("Getting chassis capability...");
 
-			List<String> list=actionFactory.getActionNames();
-		
+			List<String> list = actionFactory.getActionNames();
+
 			list.addAll(basicActionFactory.getActionNames());
-			//FIXME ADD ALL ACTIONS AVALIABLE FOR ACTIONSET	
-			
-			chassisCapability = chassisFactory.createChassisCapability(list,newSessionContextNetconf() , deviceID);
-			//FIXME IT IS A PATCH TO GET THE QUEUE MANAGER SERVICE
+			// FIXME ADD ALL ACTIONS AVALIABLE FOR ACTIONSET
+
+			chassisCapability = chassisFactory.createChassisCapability(list, newSessionContextNetconf(), deviceID);
+			// FIXME IT IS A PATCH TO GET THE QUEUE MANAGER SERVICE
 			chassisCapability.setQueueManager(queueManager);
 			chassisCapability.initialize();
 		} catch (Exception e) {
@@ -118,52 +112,30 @@ public class SimpleClientTest extends AbstractIntegrationTest {
 			Assert.fail();
 		}
 	}
-	
-	
-@Test
-	public void testActions() {	
+
+	@Test
+	public void testActions() {
 		log.info("This is running inside Equinox. With all configuration set up like you specified. ");
 		/* Wait for the activation of all the bundles */
-		IntegrationTestsHelper.waitForAllBundlesActive(bundleContext);		
-		
+		IntegrationTestsHelper.waitForAllBundlesActive(bundleContext);
+
 		prepareQueueManagerTest();
-		
+
 		prepareChassisCapability();
-		
-		
-		//chassisCapability.initialize();
-		String actionId =ChassisActionSetFactory.SETINTERFACE;
+
+		// chassisCapability.initialize();
+		String actionId = ChassisActionSetFactory.SETINTERFACE;
 		ComputerSystem model = new ComputerSystem();
 		chassisCapability.setResource(model);
-		
-		Object params = newParamsInterface();
-		chassisCapability.sendMessage(actionId,params);
-		
-		//check if it is added
-		Assert.assertFalse(queueManager.getActions().size()!=1);
-		
-		try {
-			List<ActionResponse> responses = queueManager.execute(newActionCommit(),newActionRollback(),newSessionContextNetconf());
-		} catch (ProtocolException e) {
-			e.printStackTrace();
-			log.error(e.getMessage());
-			Assert.fail();
-		} catch (CommandException e) {
-			e.printStackTrace();
-			log.error(e.getMessage());
-			Assert.fail();
-		}
-		
-		//check if it is added
-		Assert.assertFalse(queueManager.getActions().size()!=0);
 
-		
-		chassisCapability.sendMessage(ChassisActionSetFactory.GETCONFIG,null);
-		//check if it is added
-		Assert.assertFalse(queueManager.getActions().size()!=1);
-		
+		Object params = newParamsInterface();
+		chassisCapability.sendMessage(actionId, params);
+
+		// check if it is added
+		Assert.assertFalse(queueManager.getActions().size() != 1);
+
 		try {
-			List<ActionResponse> responses = queueManager.execute(newActionCommit(),newActionRollback(),newSessionContextNetconf());
+			List<ActionResponse> responses = queueManager.execute(newActionCommit(), newActionRollback(), newSessionContextNetconf());
 		} catch (ProtocolException e) {
 			e.printStackTrace();
 			log.error(e.getMessage());
@@ -173,39 +145,54 @@ public class SimpleClientTest extends AbstractIntegrationTest {
 			log.error(e.getMessage());
 			Assert.fail();
 		}
-		
-		//check if it is added
-		Assert.assertFalse(queueManager.getActions().size()!=0);
-		//We only can test it if we are working with real routers
-//		Assert.assertFalse(checkPort(model));
-	
-		
-		Assert.assertNotNull(model.getLogicalDevices());
-		for (LogicalDevice logicalDevice: model.getLogicalDevices()) {
-			EthernetPort ethernetPort = (EthernetPort)logicalDevice;
-			log.info("ethernetPort name: "+ethernetPort.getElementName());	
+
+		// check if it is added
+		Assert.assertFalse(queueManager.getActions().size() != 0);
+
+		chassisCapability.sendMessage(ChassisActionSetFactory.GETCONFIG, null);
+		// check if it is added
+		Assert.assertFalse(queueManager.getActions().size() != 1);
+
+		try {
+			List<ActionResponse> responses = queueManager.execute(newActionCommit(), newActionRollback(), newSessionContextNetconf());
+		} catch (ProtocolException e) {
+			e.printStackTrace();
+			log.error(e.getMessage());
+			Assert.fail();
+		} catch (CommandException e) {
+			e.printStackTrace();
+			log.error(e.getMessage());
+			Assert.fail();
 		}
-		
-		Assert.assertFalse(model.getLogicalDevices().size()==0);
-		
-		
-	
+
+		// check if it is added
+		Assert.assertFalse(queueManager.getActions().size() != 0);
+		// We only can test it if we are working with real routers
+		// Assert.assertFalse(checkPort(model));
+
+		Assert.assertNotNull(model.getLogicalDevices());
+		for (LogicalDevice logicalDevice : model.getLogicalDevices()) {
+			EthernetPort ethernetPort = (EthernetPort) logicalDevice;
+			log.info("ethernetPort name: " + ethernetPort.getElementName());
+		}
+
+		Assert.assertFalse(model.getLogicalDevices().size() == 0);
 
 	}
 
 	private Action newActionRollback() {
-	// TODO Auto-generated method stub
-	return null;
-}
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	private Action newActionCommit() {
-		return	basicActionFactory.createAction("commit");
-	 
-}
+		return basicActionFactory.createAction("commit");
+
+	}
 
 	private ProtocolSessionContext newSessionContextNetconf() {
 		ProtocolSessionContext protocolSessionContext = new ProtocolSessionContext();
-		//protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL_URI, "mock://user:pass@host.net:2212/mocksubsystem");
+		// protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL_URI, "mock://user:pass@host.net:2212/mocksubsystem");
 		protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL_URI, "ssh://i2cat:mant6WWe@lola.hea.net:22/netconf");
 
 		protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL, "netconf");
@@ -215,33 +202,33 @@ public class SimpleClientTest extends AbstractIntegrationTest {
 	}
 
 	private Object newParamsInterface() {
-		 EthernetPort eth = new EthernetPort();
-		 eth.setElementName("fe-0/3/1");
-		 eth.setPortNumber(30);
-		 IPProtocolEndpoint ip = new IPProtocolEndpoint();
-		 ip.setIPv4Address("193.1.24.88");
-		 ip.setSubnetMask("255.255.255.0");
-		 eth.addProtocolEndpoint(ip);
-		 ArrayList params = new ArrayList();
-		 params.add(eth);
-		 return params;
-		 
+		EthernetPort eth = new EthernetPort();
+		eth.setElementName("fe-0/3/2");
+		eth.setPortNumber(30);
+		IPProtocolEndpoint ip = new IPProtocolEndpoint();
+		ip.setIPv4Address("192.168.33.2");
+		ip.setSubnetMask("255.255.255.0");
+		eth.addProtocolEndpoint(ip);
+		ArrayList params = new ArrayList();
+		params.add(eth);
+		return params;
+
 	}
-	
-	private boolean checkPort (ComputerSystem model) {
-		if (model.getLogicalDevices()==null || model.getLogicalDevices().size()==0) return false;		
-		EthernetPort ethernetPort = (EthernetPort)model.getLogicalDevices();
+
+	private boolean checkPort(ComputerSystem model) {
+		if (model.getLogicalDevices() == null || model.getLogicalDevices().size() == 0)
+			return false;
+		EthernetPort ethernetPort = (EthernetPort) model.getLogicalDevices();
 		boolean resultEquals = ethernetPort.getElementName().equals("ge-0/1/0");
-		resultEquals = resultEquals && ethernetPort.getPortNumber()==30;
-		if (ethernetPort.getProtocolEndpoint()==null || ethernetPort.getProtocolEndpoint().size()==0) return false;
-		IPProtocolEndpoint ip = (IPProtocolEndpoint)ethernetPort.getProtocolEndpoint().get(0);
+		resultEquals = resultEquals && ethernetPort.getPortNumber() == 30;
+		if (ethernetPort.getProtocolEndpoint() == null || ethernetPort.getProtocolEndpoint().size() == 0)
+			return false;
+		IPProtocolEndpoint ip = (IPProtocolEndpoint) ethernetPort.getProtocolEndpoint().get(0);
 		resultEquals = resultEquals && ip.getIPv4Address().equals("193.1.24.88");
 		resultEquals = resultEquals && ip.getSubnetMask().equals("255.255.255.0");
-		
+
 		return resultEquals;
-		
+
 	}
-
-
 
 }
