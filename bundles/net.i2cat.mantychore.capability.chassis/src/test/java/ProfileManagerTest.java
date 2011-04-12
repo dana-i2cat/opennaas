ec30043050802f7837a23569ae8bdd24aaaba32c
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-04-12 16:32:30 +0000

    Added first prototype for ProfileManagerCapability
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/carlos@640 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.capability.chassis/src/test/java/ProfileManagerTest.java
@@ -0,0 +1,31 @@
+import org.junit.Before;
+
+public class ProfileManagerTest {
+
+	@Before
+	public void initProfileManager() {
+		// ProfileManagerCapability profileManager = new ProfileManagerCapability(List<String> actionIds,);
+
+	}
+
+	public void addOperation() {
+
+	}
+
+	public void removeOperation() {
+
+	}
+
+	public void listOperations() {
+
+	}
+
+	public void queueOperations() {
+
+	}
+
+	public void manageOperation() {
+
+	}
+
+}
