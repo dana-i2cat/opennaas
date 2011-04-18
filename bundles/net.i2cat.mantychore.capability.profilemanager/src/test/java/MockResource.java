4000a7955caab41f0fa16a7fc9d60bc8d84ad123
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-04-18 07:43:56 +0000

    Add profile manager project
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/carlos@653 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.capability.profilemanager/src/test/java/MockResource.java
@@ -0,0 +1,119 @@
+import java.util.ArrayList;
+import java.util.List;
+
+import net.i2cat.mantychore.model.ManagedElement;
+import net.i2cat.nexus.resources.IResource;
+import net.i2cat.nexus.resources.IResourceIdentifier;
+import net.i2cat.nexus.resources.ResourceException;
+import net.i2cat.nexus.resources.capability.ICapability;
+import net.i2cat.nexus.resources.descriptor.CapabilityDescriptor;
+import net.i2cat.nexus.resources.descriptor.Information;
+import net.i2cat.nexus.resources.descriptor.ResourceDescriptor;
+
+public class MockResource implements IResource {
+	ResourceDescriptor	resourceDescriptor	= new ResourceDescriptor();
+
+	public MockResource() {
+		resourceDescriptor.setCapabilityDescriptors(new ArrayList<CapabilityDescriptor>());
+	}
+
+	public void addCapabilityDescriptor(CapabilityDescriptor capabilityDescriptor) {
+		resourceDescriptor.getCapabilityDescriptors().add(capabilityDescriptor);
+
+	}
+
+	public void activate() throws ResourceException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void deactivate() throws ResourceException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public State getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void initialize() throws ResourceException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setState(State arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void shutdown() throws ResourceException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void addCapability(ICapability arg0) {
+
+	}
+
+	public List<ICapability> getCapabilities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ICapability getCapability(Information arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ManagedElement getModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ResourceDescriptor getResourceDescriptor() {
+		// TODO Auto-generated method stub
+		return resourceDescriptor;
+	}
+
+	public IResourceIdentifier getResourceIdentifier() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ICapability removeCapability(Information arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void setCapabilities(List<ICapability> arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setModel(ManagedElement arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setResourceDescriptor(ResourceDescriptor arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void setResourceIdentifier(IResourceIdentifier arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void start() throws ResourceException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void stop() throws ResourceException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
