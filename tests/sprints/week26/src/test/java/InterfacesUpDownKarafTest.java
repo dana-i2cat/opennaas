efc7234c7b0afb1a980e1f340397826da20677ab
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-07-06 08:05:20 +0000

    Added test to check interface status
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@998 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ tests/sprints/week26/src/test/java/InterfacesUpDownKarafTest.java
@@ -0,0 +1,174 @@
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.i2cat.mantychore.model.ComputerSystem;
+import net.i2cat.mantychore.model.LogicalDevice;
+import net.i2cat.mantychore.model.LogicalPort;
+import net.i2cat.mantychore.model.ManagedSystemElement.OperationalStatus;
+import net.i2cat.nexus.resources.IResource;
+import net.i2cat.nexus.resources.IResourceRepository;
+import net.i2cat.nexus.resources.ResourceException;
+import net.i2cat.nexus.resources.descriptor.ResourceDescriptor;
+import net.i2cat.nexus.resources.protocol.IProtocolManager;
+import net.i2cat.nexus.resources.protocol.ProtocolException;
+import net.i2cat.nexus.resources.protocol.ProtocolSessionContext;
+import net.i2cat.nexus.tests.IntegrationTestsHelper;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.karaf.testing.AbstractIntegrationTest;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.osgi.service.command.CommandProcessor;
+import org.osgi.service.command.CommandSession;
+
+public class InterfacesUpDownKarafTest extends AbstractIntegrationTest {
+	static Log			log	= LogFactory
+									.getLog(InterfacesUpDownKarafTest.class);
+	IResourceRepository	repository;
+	String				resourceFriendlyID;
+	IResource			resource;
+
+	@Configuration
+	public static Option[] configuration() throws Exception {
+
+		Option[] options = combine(
+				IntegrationTestsHelper.getMantychoreTestOptions(),
+				mavenBundle().groupId("net.i2cat.nexus").artifactId(
+						"net.i2cat.nexus.tests.helper")
+				// , vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
+				);
+
+		return options;
+	}
+
+	public Object executeCommand(String command) throws Exception {
+		// Run some commands to make sure they are installed properly
+		CommandProcessor cp = getOsgiService(CommandProcessor.class);
+		CommandSession cs = cp.createSession(System.in, System.out, System.err);
+		Object commandOutput = null;
+		try {
+			commandOutput = cs.execute(command);
+			return commandOutput;
+		} catch (IllegalArgumentException e) {
+			Assert.fail("Action should have thrown an exception because: " + e.toString());
+		} catch (NoSuchMethodException a) {
+			log.error("Method for command not found: " + a.getLocalizedMessage());
+			Assert.fail("Method for command not found.");
+		}
+
+		cs.close();
+		return commandOutput;
+	}
+
+	@Before
+	public void initTest() {
+		List<String> capabilities = new ArrayList<String>();
+
+		ResourceDescriptor resourceDescriptor = ResourceDescriptorFactory.newResourceDescriptor("junosm20", "junos", capabilities);
+		resourceFriendlyID = resourceDescriptor.getInformation().getType() + ":" + resourceDescriptor.getInformation().getName();
+
+		try {
+			resource = repository.createResource(resourceDescriptor);
+			IProtocolManager protocolManager = getOsgiService(IProtocolManager.class, 5000);
+			protocolManager.getProtocolSessionManagerWithContext(resource.getResourceIdentifier().getId(), newSessionContextNetconf());
+			repository.startResource(resource.getResourceDescriptor().getId());
+
+		} catch (ResourceException e) {
+
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		} catch (ProtocolException e) {
+
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Configure the protocol to connect
+	 */
+	private ProtocolSessionContext newSessionContextNetconf() {
+		String uri = System.getProperty("protocol.uri");
+		if (uri == null || uri.equals("${protocol.uri}")) {
+			uri = "mock://user:pass@host.net:2212/mocksubsystem";
+		}
+
+		ProtocolSessionContext protocolSessionContext = new ProtocolSessionContext();
+
+		protocolSessionContext.addParameter(
+				ProtocolSessionContext.PROTOCOL_URI, uri);
+		protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL,
+				"netconf");
+		// ADDED
+		return protocolSessionContext;
+	}
+
+	/**
+	 * UP interface test
+	 */
+	@Test
+	public void UpInterfaceTest() {
+
+		try {
+			// chassis:setVLAN interface VLANid
+			Object response = executeCommand("chassis:up " + resourceFriendlyID + " fe-0/1/2");
+			Assert.assertNotNull(response);
+
+			Object response1 = executeCommand("queue:execute " + resourceFriendlyID);
+			Assert.assertNotNull(response1);
+
+			ComputerSystem system = (ComputerSystem) resource.getModel();
+			List<LogicalDevice> ld = system.getLogicalDevices();
+			for (LogicalDevice logicalDevice : ld) {
+				if (logicalDevice instanceof LogicalPort && logicalDevice.getElementName().equals("fe-0/1/2")) {
+					LogicalPort logicalPort = (LogicalPort) logicalDevice;
+					Assert.assertTrue(logicalPort.getOperationalStatus() == OperationalStatus.OK);
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+
+	}
+
+	/**
+	 * DOWN interface test
+	 */
+	@Test
+	public void DownInterfaceTest() {
+
+		try {
+			// chassis:setVLAN interface VLANid
+			Object response = executeCommand("chassis:down " + resourceFriendlyID + " fe-0/1/2");
+			Assert.assertNotNull(response);
+
+			Object response1 = executeCommand("queue:execute " + resourceFriendlyID);
+			Assert.assertNotNull(response1);
+
+			ComputerSystem system = (ComputerSystem) resource.getModel();
+			List<LogicalDevice> ld = system.getLogicalDevices();
+			for (LogicalDevice logicalDevice : ld) {
+				if (logicalDevice instanceof LogicalPort && logicalDevice.getElementName().equals("fe-0/1/2")) {
+					LogicalPort logicalPort = (LogicalPort) logicalDevice;
+					Assert.assertTrue(logicalPort.getOperationalStatus() == OperationalStatus.STOPPED);
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+
+	}
+
+}
