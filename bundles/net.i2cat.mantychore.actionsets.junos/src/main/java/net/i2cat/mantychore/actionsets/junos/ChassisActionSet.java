7e9db9ea2fd2bb4b48c5800ff77843feaf7f0382
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-07-11 07:46:28 +0000

    Fixed bug. It has necessary to include setVLAN action in the action list
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@1016 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/ChassisActionSet.java
+++ bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/ChassisActionSet.java
@@ -3,6 +3,7 @@ package net.i2cat.mantychore.actionsets.junos;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.i2cat.mantychore.actionsets.junos.actions.ConfigureVLANAction;
 import net.i2cat.mantychore.actionsets.junos.actions.CreateSubInterfaceAction;
 import net.i2cat.mantychore.actionsets.junos.actions.DeleteSubInterfaceAction;
 import net.i2cat.mantychore.actionsets.junos.actions.GetConfigurationAction;
@@ -20,6 +21,7 @@ public class ChassisActionSet extends ActionSet {
 		this.putAction(ActionConstants.GETCONFIG, GetConfigurationAction.class);
 		this.putAction(ActionConstants.DELETESUBINTERFACE, DeleteSubInterfaceAction.class);
 		this.putAction(ActionConstants.CREATESUBINTERFACE, CreateSubInterfaceAction.class);
+		this.putAction(ActionConstants.SETVLAN, ConfigureVLANAction.class);
 
 	}
 
@@ -29,6 +31,7 @@ public class ChassisActionSet extends ActionSet {
 		actionNames.add(ActionConstants.GETCONFIG);
 		actionNames.add(ActionConstants.DELETESUBINTERFACE);
 		actionNames.add(ActionConstants.CREATESUBINTERFACE);
+		actionNames.add(ActionConstants.SETVLAN);
 		return actionNames;
 	}
 }
