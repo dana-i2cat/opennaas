2b095ea6a859d59c06cd5af07291a84479d7e823
evelyn_torras <evelyn_torras@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-07-26 14:57:56 +0000

    added completers to Karaf commands
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@1074 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.capability.ip/src/main/java/net/i2cat/mantychore/capability/ip/shell/completers/ResourceNameCompleter.java
@@ -0,0 +1,34 @@
+package net.i2cat.mantychore.capability.ip.shell.completers;
+
+import java.util.List;
+
+import net.i2cat.nexus.resources.Activator;
+import net.i2cat.nexus.resources.IResource;
+import net.i2cat.nexus.resources.IResourceManager;
+
+import org.apache.karaf.shell.console.Completer;
+import org.apache.karaf.shell.console.completer.StringsCompleter;
+
+public class ResourceNameCompleter implements Completer {
+
+	@Override
+	public int complete(String arg0, int arg1, List<String> arg2) {
+		IResourceManager resourceManager = null;
+		List<IResource> list = null;
+		StringsCompleter delegate = new StringsCompleter();
+
+		try {
+			resourceManager = Activator.getResourceManagerService();
+			list = resourceManager.listResources();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		for (IResource resource : list) {
+			String value = resource.getResourceDescriptor().getInformation().getType() + ":" + resource.getResourceDescriptor().getInformation()
+					.getName();
+			delegate.getStrings().add(value);
+		}
+		// complete(java.lang.String buffer, int cursor, java.util.List candidates)
+		return delegate.complete(arg0, arg1, arg2);
+	}
+}
