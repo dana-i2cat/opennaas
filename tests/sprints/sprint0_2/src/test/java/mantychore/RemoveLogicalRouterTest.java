fa86b43054c3197cc4238819f68e996fbee66566
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-09-09 13:19:45 +0000

    Skeleton tests for sprint 0.2
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/SprintWeek35@1190 e5892e71-24e5-41bb-b533-28a584b410ba

--- tests/sprints/sprint0_2/src/test/java/mantychore/RemoveLogicalRouterTest.java
+++ tests/sprints/sprint0_2/src/test/java/mantychore/RemoveLogicalRouterTest.java
@@ -1,6 +1,111 @@
 package mantychore;
 
-public class RemoveLogicalRouterTest {
+import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
+import static org.ops4j.pax.exam.OptionUtils.combine;
+import net.i2cat.nexus.resources.IResource;
+import net.i2cat.nexus.resources.IResourceManager;
+import net.i2cat.nexus.resources.ResourceException;
+import net.i2cat.nexus.tests.IntegrationTestsHelper;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.karaf.testing.AbstractIntegrationTest;
+import org.junit.runner.RunWith;
+import org.ops4j.pax.exam.Inject;
+import org.ops4j.pax.exam.Option;
+import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.JUnit4TestRunner;
+import org.osgi.framework.BundleContext;
+import org.osgi.service.command.CommandProcessor;
+
+@RunWith(JUnit4TestRunner.class)
+public class RemoveLogicalRouterTest extends AbstractIntegrationTest {
+	static Log					log				= LogFactory
+															.getLog(RemoveLogicalRouterTest.class);
+	IResourceManager			resourceManager;
+	String						resourceFriendlyID;
+	IResource					resource;
+	private CommandProcessor	commandprocessor;
+	private boolean				isMock			= false;
+	@Inject
+	BundleContext				bundleContext	= null;
+
+	@Configuration
+	public static Option[] configuration() throws Exception {
+
+		Option[] options = combine(
+					IntegrationTestsHelper.getMantychoreTestOptions(),
+					mavenBundle().groupId("net.i2cat.nexus").artifactId(
+							"net.i2cat.nexus.tests.helper")
+									// , vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
+									// ////////import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
+
+									);
+
+		return options;
+	}
+
+	public void clearRepo() throws ResourceException {
+		for (IResource resource : resourceManager.listResources()) {
+			resourceManager.removeResource(resource.getResourceIdentifier());
+		}
+	}
+
+	// public Boolean createProtocolForResource(String resourceId) throws ProtocolException {
+	// IProtocolManager protocolManager = getOsgiService(IProtocolManager.class, 5000);
+	//
+	// // ProtocolSessionContext context = ProtocolSessionHelper.newSessionContextNetconf();
+	// IProtocolSessionManager protocolSessionManager = protocolManager.getProtocolSessionManagerWithContext(resourceId, context);
+	//
+	// if (context.getSessionParameters().get(context.PROTOCOL_URI).toString().contains("mock")) {
+	// return true;
+	// }
+	//
+	// return false;
+	// }
+
+	// public void initTest() {
+	//
+	// List<String> capabilities = new ArrayList<String>();
+	//
+	// capabilities.add("ipv4");
+	// capabilities.add("queue");
+	//
+	// ResourceDescriptor resourceDescriptor = ResourceDescriptorFactory.newResourceDescriptor("junosm20", "router", capabilities);
+	// resourceFriendlyID = resourceDescriptor.getInformation().getType() + ":" + resourceDescriptor.getInformation().getName();
+	// try {
+	// clearRepo();
+	// resource = resourceManager.createResource(resourceDescriptor);
+	// isMock = createProtocolForResource(resource.getResourceIdentifier().getId());
+	// resourceManager.startResource(resource.getResourceIdentifier());
+	//
+	// // call the command to initialize the model
+	// } catch (ResourceException e) {
+	// Assert.fail(e.getMessage());
+	// } catch (ProtocolException e) {
+	// Assert.fail(e.getMessage());
+	// }
+	//
+	// }
+
+	// @Before
+	public void initBundles() {
+		log.info("Waiting to load all bundles");
+		/* Wait for the activation of all the bundles */
+		IntegrationTestsHelper.waitForAllBundlesActive(bundleContext);
+		log.info("Loaded all bundles");
+
+		/* init capability */
+
+		log.info("This is running inside Equinox. With all configuration set up like you specified. ");
+
+		resourceManager = getOsgiService(IResourceManager.class);
+
+		log.info("INFO: Initialized!");
+		commandprocessor = getOsgiService(CommandProcessor.class);
+		// initTest();
+
+	}
 
 	public void RemoveLogicalRouterAdminPhysical() {
 		// chassis:deleteLogicalRouter
