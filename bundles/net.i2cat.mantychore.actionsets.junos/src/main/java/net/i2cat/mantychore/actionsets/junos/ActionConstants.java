8e30aba4eb5237eb3a9f78a605771676d66c01ae
carlos_baez <carlos_baez@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-09-14 13:13:07 +0000

    Add list logical routers action
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/SprintWeek35@1224 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/ActionConstants.java
+++ bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/ActionConstants.java
@@ -16,5 +16,6 @@ public class ActionConstants {
 	public static final String	CONFIGURESTATUS		= "configureStatus";
 	public static final String	DELETELOGICALROUTER	= "deleteLogicalRouter";
 	public static final String	CREATELOGICALROUTER	= "createLogicalRouter";
+	public static final String	GETLOGICALROUTERS	= "getLogicalRouters";
 
 }
