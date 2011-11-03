2538df7ee7ad78d704c034ecf38d5bf7b098d3a7
isart_canyameres <isart_canyameres@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-11-03 15:36:18 +0000

    Created karaf command to setInterfaceDescription (for physical and logical interfaces)
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/sprint_0_4@1497 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.capability.chassis/src/main/java/net/i2cat/mantychore/capability/chassis/shell/SetInterfaceDescriptionCommand.java
@@ -0,0 +1,95 @@
+package net.i2cat.mantychore.capability.chassis.shell;
+
+import net.i2cat.mantychore.actionsets.junos.ActionConstants;
+import net.i2cat.mantychore.capability.chassis.ChassisCapability;
+import net.i2cat.mantychore.model.EthernetPort;
+import net.i2cat.mantychore.model.LogicalPort;
+
+import org.apache.felix.gogo.commands.Argument;
+import org.apache.felix.gogo.commands.Command;
+import org.opennaas.core.resources.IResource;
+import org.opennaas.core.resources.IResourceIdentifier;
+import org.opennaas.core.resources.IResourceManager;
+import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.capability.ICapability;
+import org.opennaas.core.resources.shell.GenericKarafCommand;
+
+@Command(scope = "chassis", name = "setInterfaceDescription", description = "Sets given description to given interface.")
+public class SetInterfaceDescriptionCommand extends GenericKarafCommand {
+
+	@Argument(index = 0, name = "resourceType:resourceName", description = "The resource name.", required = true, multiValued = false)
+	private String	resourceId;
+
+	@Argument(index = 1, name = "interface", description = "The interface whose description is set.", required = true, multiValued = false)
+	private String	subinterface;
+	
+	@Argument(index = 2, name = "description", description = "Description of the interface.", required = true, multiValued = false)
+	private String	description = "";
+
+	@Override
+	protected Object doExecute() throws Exception {
+		printInitCommand("set interface description");
+
+		try {
+			IResourceManager manager = getResourceManager();
+
+			String[] argsRouterName = new String[2];
+			try {
+				argsRouterName = splitResourceName(resourceId);
+			} catch (Exception e) {
+				printError(e.getMessage());
+				printEndCommand();
+				return -1;
+			}
+
+			IResourceIdentifier resourceIdentifier = null;
+
+			resourceIdentifier = manager.getIdentifierFromResourceName(argsRouterName[0], argsRouterName[1]);
+			if (resourceIdentifier == null) {
+				printError("Error in identifier.");
+				printEndCommand();
+				return -1;
+			}
+
+			IResource resource = manager.getResource(resourceIdentifier);
+
+			validateResource(resource);
+
+			ICapability chassisCapability = getCapability(resource.getCapabilities(), ChassisCapability.CHASSIS);
+			printInfo("Sending message to the queue");
+			chassisCapability.sendMessage(ActionConstants.SETINTERFACEDESCRIPTION, prepareParams());
+
+		} catch (ResourceException e) {
+			printError(e);
+			printEndCommand();
+			return -1;
+		} catch (Exception e) {
+			printError("Error setting description");
+			printError(e);
+			printEndCommand();
+			return -1;
+		}
+		printEndCommand();
+		return null;
+	}
+	
+	
+	private LogicalPort prepareParams() {
+		String[] args = subinterface.split("\\.");
+		
+		LogicalPort port;
+		if (args.length > 1 ) {
+			// it's a subinterface
+			EthernetPort eth = new EthernetPort();
+			eth.setName(args[0]);
+			eth.setPortNumber(Integer.parseInt(args[1]));
+			eth.setDescription(description);
+			port = eth;
+		} else {
+			port = new LogicalPort();
+			port.setName(args[0]);
+			port.setDescription(description);
+		}
+		return port;
+	}
+}
