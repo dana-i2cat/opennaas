0c98295e5b944ba29c86bb66d7b90fca3271e20b
isart_canyameres <isart_canyameres@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-11-04 14:27:41 +0000

    code style
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/sprint_0_4@1517 e5892e71-24e5-41bb-b533-28a584b410ba

--- tests/sprints/sprint0_4/src/test/java/setdesc/SetInterfaceDescriptionActionTest.java
+++ tests/sprints/sprint0_4/src/test/java/setdesc/SetInterfaceDescriptionActionTest.java
@@ -3,7 +3,6 @@ package setdesc;
 import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
 import static org.ops4j.pax.exam.OptionUtils.combine;
 import helpers.CheckParametersHelper;
-import helpers.ParamCreationHelper;
 import helpers.ProtocolSessionHelper;
 import helpers.ResourceHelper;
 
@@ -15,7 +14,6 @@ import net.i2cat.mantychore.actionsets.junos.ActionConstants;
 import net.i2cat.mantychore.model.ComputerSystem;
 import net.i2cat.mantychore.model.EthernetPort;
 import net.i2cat.mantychore.model.LogicalPort;
-import net.i2cat.mantychore.model.NetworkPort;
 import net.i2cat.nexus.tests.IntegrationTestsHelper;
 
 import org.apache.karaf.testing.AbstractIntegrationTest;
@@ -44,225 +42,220 @@ import org.osgi.framework.BundleContext;
 @RunWith(JUnit4TestRunner.class)
 public class SetInterfaceDescriptionActionTest extends AbstractIntegrationTest {
 
-		@Inject
-		BundleContext				bundleContext	= null;
-		
-		boolean				isMock;
-		ResourceDescriptor	resourceDescriptor;
-		IResource			resource		= null;
-		String				deviceID;
-		String				type;
-		IResourceManager	resourceManager;
-		IProfileManager		profileManager;
-
-		@Configuration
-		public static Option[] configure() {
-
-			Option[] options = combine(
-					IntegrationTestsHelper.getMantychoreTestOptions(),
-					mavenBundle().groupId("net.i2cat.nexus").artifactId(
-							"net.i2cat.nexus.tests.helper")
-					// , vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
-					);
-			return options;
-		}
-
-		public SetInterfaceDescriptionActionTest() {
-			this.type = "router";
-			this.deviceID = "junos";
-		}
-
-		/**
-		 * Prepare the resource to do the test
-		 * 
-		 * @throws ResourceException
-		 * @throws ProtocolException
-		 * 
-		 */
-		public void setUp() throws ResourceException, ProtocolException {
-
-			IntegrationTestsHelper.waitForAllBundlesActive(bundleContext);
-
-			resourceManager = getOsgiService(IResourceManager.class, 50000);
-			profileManager = getOsgiService(IProfileManager.class, 30000);
-			
-			List<String> capabilities = new ArrayList<String>();
-			capabilities.add("chassis");
-			capabilities.add("queue");
-			resourceDescriptor = ResourceDescriptorFactory.newResourceDescriptor(deviceID, type, capabilities);
-			resource = resourceManager.createResource(resourceDescriptor);
-			createProtocolForResource(resource.getResourceIdentifier().getId());
-
-		}
-
-		/**
-		 * Reset info for next tests
-		 * 
-		 * @throws ResourceException
-		 * 
-		 */
-		public void tearDown() throws ResourceException {
-
-			// Reset repository
-			IResource[] toRemove = new IResource[resourceManager.listResources().size()];
-			toRemove = resourceManager.listResources().toArray(toRemove);
-
-			for (IResource resource : toRemove) {
-				if (resource.getState().equals(State.ACTIVE))
-					resourceManager.stopResource(resource.getResourceIdentifier());
-
-				resourceManager.removeResource(resource.getResourceIdentifier());
-			}
-		}
-
-		@Test
-		public void configureSubInterfaceTest() {
-			try {
-				setUp();
-			} catch (Exception e) {
-				Assert.fail("Impossible set up test: " + e.getMessage());
-			}
-
-			setSubInterfaceDescriptionTest();
-			setInterfaceDescriptionTest();
-			
-			try {
-				tearDown();
-			} catch (ResourceException e) {
-				Assert.fail("Impossible tear down test: " + e.getMessage());
-			}
-		}
-
-		/**
-		 * Put related task
-		 * */
-		public void setSubInterfaceDescriptionTest() {
-
-			/* send action */
-			int posChassis = ResourceHelper.containsCapability(resource, "chassis");
-			if (posChassis == -1)
-				Assert.fail("Could not get Chassis capability for given resource");
-			ICapability chassisCapability = resource.getCapabilities().get(posChassis);
-			
-			EthernetPort ethernetPort = new EthernetPort();
-			ethernetPort.setName("fe-0/3/2");
-			ethernetPort.setPortNumber(2);
-			ethernetPort.setDescription("Description for the setSubInterfaceDescription test");
-			
-			try {
-				chassisCapability.sendMessage(ActionConstants.CREATESUBINTERFACE, ethernetPort);
-				chassisCapability.sendMessage(ActionConstants.SETINTERFACEDESCRIPTION, ethernetPort);
-			} catch (CapabilityException e) {
-				Assert.fail("It was impossible to send the following message: " + e.getMessage());
-			}
-
-			/* execute action */
-			int posQueue = ResourceHelper.containsCapability(resource, "queue");
-			if (posQueue == -1)
-				Assert.fail("Could not get Queue capability for given resource");
-			ICapability queueCapability = resource.getCapabilities().get(posQueue);
-			try {
-				QueueResponse response = (QueueResponse) queueCapability.sendMessage(QueueConstants.EXECUTE, null);
-				Assert.assertTrue(response.isOk());
-			} catch (CapabilityException e) {
-				Assert.fail("It was impossible to send the following message: " + e.getMessage());
-			}
-
-			/* refresh model */
-			try {
-				chassisCapability.sendMessage(ActionConstants.GETCONFIG, ethernetPort);
-			} catch (CapabilityException e) {
-				Assert.fail("It was impossible to send the following message: " + e.getMessage());
-			}
-
-			if (isMock)
-				return;
-
-			/* check the update model, it is only possible to check it with a real router */
-			int pos = CheckParametersHelper.containsSubInterface((ComputerSystem) resource.getModel(), ethernetPort);
-			Assert.assertTrue(pos != -1);
-			
-			String desc = ((EthernetPort)((ComputerSystem) resource.getModel()).getLogicalDevices().get(pos)).getDescription();
-			Assert.assertTrue(desc.equals(ethernetPort.getDescription()));
-			
-			//delete created sub interface
-			try {
-				chassisCapability.sendMessage(ActionConstants.DELETESUBINTERFACE, ethernetPort);
-				QueueResponse response = (QueueResponse) queueCapability.sendMessage(QueueConstants.EXECUTE, null);
-				Assert.assertTrue(response.isOk());
-			} catch (CapabilityException e) {
-				Assert.fail("It was impossible to send the following message: " + e.getMessage());
-			}
-		}
-
-		/**
-		 * Test the possibility to configure subinterfaces with an encapsulation
-		 * */
-		public void setInterfaceDescriptionTest() {
-			/* send action */
-			int posChassis = ResourceHelper.containsCapability(resource, "chassis");
-			if (posChassis == -1)
-				Assert.fail("Could not get Chassis capability for given resource");
-			ICapability chassisCapability = resource.getCapabilities().get(posChassis);
-			
-			LogicalPort logicalPort = new LogicalPort();
-			logicalPort.setName("fe-0/3/2");
-			logicalPort.setDescription("Description for the setSubInterfaceDescription test");
-			
-			try {
-				chassisCapability.sendMessage(ActionConstants.SETINTERFACEDESCRIPTION, logicalPort);
-			} catch (CapabilityException e) {
-				Assert.fail("It was impossible to send the following message: " + e.getMessage());
-			}
-
-			/* execute action */
-			int posQueue = ResourceHelper.containsCapability(resource, "queue");
-			if (posQueue == -1)
-				Assert.fail("Could not get Queue capability for given resource");
-			ICapability queueCapability = resource.getCapabilities().get(posQueue);
-			try {
-				QueueResponse response = (QueueResponse) queueCapability.sendMessage(QueueConstants.EXECUTE, null);
-				Assert.assertTrue(response.isOk());
-			} catch (CapabilityException e) {
-				Assert.fail("It was impossible to send the following message: " + e.getMessage());
-			}
-
-			/* refresh model */
-			try {
-				chassisCapability.sendMessage(ActionConstants.GETCONFIG, logicalPort);
-			} catch (CapabilityException e) {
-				Assert.fail("It was impossible to send the following message: " + e.getMessage());
-			}
-
-			if (isMock)
-				return;
-
-			/* check the update model, it is only possible to check it with a real router */
-			int pos = CheckParametersHelper.containsInterface((ComputerSystem) resource.getModel(), logicalPort);
-			Assert.assertTrue(pos != -1);
-			
-			String desc = ((LogicalPort)((ComputerSystem) resource.getModel()).getLogicalDevices().get(pos)).getDescription();
-			Assert.assertTrue(desc.equals(logicalPort.getDescription()));
-
-		}
-
-		/**
-		 * TODO This class has to be moved to the share helper
-		 */
-		private void createProtocolForResource(String resourceId) throws ProtocolException {
-			IProtocolManager protocolManager = getOsgiService(IProtocolManager.class, 15000);
-			ProtocolSessionContext context = ProtocolSessionHelper.newSessionContextNetconf();
-			protocolManager.getProtocolSessionManagerWithContext(resourceId, context);
-			
-			isMock = false;
-			if (context.getSessionParameters().containsKey(ProtocolSessionContext.PROTOCOL_URI)){
-				if (((String)context.getSessionParameters().get(ProtocolSessionContext.PROTOCOL_URI)).startsWith("mock")){
-					isMock = true;
-				}
-			}
-		}
-
-
-
+	@Inject
+	BundleContext				bundleContext	= null;
 	
+	boolean				isMock;
+	ResourceDescriptor	resourceDescriptor;
+	IResource			resource		= null;
+	String				deviceID;
+	String				type;
+	IResourceManager	resourceManager;
+	IProfileManager		profileManager;
+
+	@Configuration
+	public static Option[] configure() {
+
+		Option[] options = combine(
+				IntegrationTestsHelper.getMantychoreTestOptions(),
+				mavenBundle().groupId("net.i2cat.nexus").artifactId(
+						"net.i2cat.nexus.tests.helper")
+				// , vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
+				);
+		return options;
+	}
+
+	public SetInterfaceDescriptionActionTest() {
+		this.type = "router";
+		this.deviceID = "junos";
+	}
+
+	/**
+	 * Prepare the resource to do the test
+	 * 
+	 * @throws ResourceException
+	 * @throws ProtocolException
+	 * 
+	 */
+	public void setUp() throws ResourceException, ProtocolException {
+
+		IntegrationTestsHelper.waitForAllBundlesActive(bundleContext);
+
+		resourceManager = getOsgiService(IResourceManager.class, 50000);
+		profileManager = getOsgiService(IProfileManager.class, 30000);
+		
+		List<String> capabilities = new ArrayList<String>();
+		capabilities.add("chassis");
+		capabilities.add("queue");
+		resourceDescriptor = ResourceDescriptorFactory.newResourceDescriptor(deviceID, type, capabilities);
+		resource = resourceManager.createResource(resourceDescriptor);
+		createProtocolForResource(resource.getResourceIdentifier().getId());
+
+	}
+
+	/**
+	 * Reset info for next tests
+	 * 
+	 * @throws ResourceException
+	 * 
+	 */
+	public void tearDown() throws ResourceException {
+
+		// Reset repository
+		IResource[] toRemove = new IResource[resourceManager.listResources().size()];
+		toRemove = resourceManager.listResources().toArray(toRemove);
+
+		for (IResource resource : toRemove) {
+			if (resource.getState().equals(State.ACTIVE))
+				resourceManager.stopResource(resource.getResourceIdentifier());
+
+			resourceManager.removeResource(resource.getResourceIdentifier());
+		}
+	}
+
+	@Test
+	public void configureSubInterfaceTest() {
+		try {
+			setUp();
+		} catch (Exception e) {
+			Assert.fail("Impossible set up test: " + e.getMessage());
+		}
+
+		setSubInterfaceDescriptionTest();
+		setInterfaceDescriptionTest();
+		
+		try {
+			tearDown();
+		} catch (ResourceException e) {
+			Assert.fail("Impossible tear down test: " + e.getMessage());
+		}
+	}
+
+	/**
+	 * Put related task
+	 * */
+	public void setSubInterfaceDescriptionTest() {
+
+		/* send action */
+		int posChassis = ResourceHelper.containsCapability(resource, "chassis");
+		if (posChassis == -1)
+			Assert.fail("Could not get Chassis capability for given resource");
+		ICapability chassisCapability = resource.getCapabilities().get(posChassis);
+		
+		EthernetPort ethernetPort = new EthernetPort();
+		ethernetPort.setName("fe-0/3/2");
+		ethernetPort.setPortNumber(2);
+		ethernetPort.setDescription("Description for the setSubInterfaceDescription test");
+		
+		try {
+			chassisCapability.sendMessage(ActionConstants.CREATESUBINTERFACE, ethernetPort);
+			chassisCapability.sendMessage(ActionConstants.SETINTERFACEDESCRIPTION, ethernetPort);
+		} catch (CapabilityException e) {
+			Assert.fail("It was impossible to send the following message: " + e.getMessage());
+		}
+
+		/* execute action */
+		int posQueue = ResourceHelper.containsCapability(resource, "queue");
+		if (posQueue == -1)
+			Assert.fail("Could not get Queue capability for given resource");
+		ICapability queueCapability = resource.getCapabilities().get(posQueue);
+		try {
+			QueueResponse response = (QueueResponse) queueCapability.sendMessage(QueueConstants.EXECUTE, null);
+			Assert.assertTrue(response.isOk());
+		} catch (CapabilityException e) {
+			Assert.fail("It was impossible to send the following message: " + e.getMessage());
+		}
+
+		/* refresh model */
+		try {
+			chassisCapability.sendMessage(ActionConstants.GETCONFIG, ethernetPort);
+		} catch (CapabilityException e) {
+			Assert.fail("It was impossible to send the following message: " + e.getMessage());
+		}
+
+		if (isMock)
+			return;
+
+		/* check the update model, it is only possible to check it with a real router */
+		int pos = CheckParametersHelper.containsSubInterface((ComputerSystem) resource.getModel(), ethernetPort);
+		Assert.assertTrue(pos != -1);
+		
+		String desc = ((EthernetPort)((ComputerSystem) resource.getModel()).getLogicalDevices().get(pos)).getDescription();
+		Assert.assertTrue(desc.equals(ethernetPort.getDescription()));
+		
+		//delete created sub interface
+		try {
+			chassisCapability.sendMessage(ActionConstants.DELETESUBINTERFACE, ethernetPort);
+			QueueResponse response = (QueueResponse) queueCapability.sendMessage(QueueConstants.EXECUTE, null);
+			Assert.assertTrue(response.isOk());
+		} catch (CapabilityException e) {
+			Assert.fail("It was impossible to send the following message: " + e.getMessage());
+		}
+	}
+
+	/**
+	 * Test the possibility to configure subinterfaces with an encapsulation
+	 * */
+	public void setInterfaceDescriptionTest() {
+		/* send action */
+		int posChassis = ResourceHelper.containsCapability(resource, "chassis");
+		if (posChassis == -1)
+			Assert.fail("Could not get Chassis capability for given resource");
+		ICapability chassisCapability = resource.getCapabilities().get(posChassis);
+		
+		LogicalPort logicalPort = new LogicalPort();
+		logicalPort.setName("fe-0/3/2");
+		logicalPort.setDescription("Description for the setSubInterfaceDescription test");
+		
+		try {
+			chassisCapability.sendMessage(ActionConstants.SETINTERFACEDESCRIPTION, logicalPort);
+		} catch (CapabilityException e) {
+			Assert.fail("It was impossible to send the following message: " + e.getMessage());
+		}
+
+		/* execute action */
+		int posQueue = ResourceHelper.containsCapability(resource, "queue");
+		if (posQueue == -1)
+			Assert.fail("Could not get Queue capability for given resource");
+		ICapability queueCapability = resource.getCapabilities().get(posQueue);
+		try {
+			QueueResponse response = (QueueResponse) queueCapability.sendMessage(QueueConstants.EXECUTE, null);
+			Assert.assertTrue(response.isOk());
+		} catch (CapabilityException e) {
+			Assert.fail("It was impossible to send the following message: " + e.getMessage());
+		}
+
+		/* refresh model */
+		try {
+			chassisCapability.sendMessage(ActionConstants.GETCONFIG, logicalPort);
+		} catch (CapabilityException e) {
+			Assert.fail("It was impossible to send the following message: " + e.getMessage());
+		}
+
+		if (isMock)
+			return;
+
+		/* check the update model, it is only possible to check it with a real router */
+		int pos = CheckParametersHelper.containsInterface((ComputerSystem) resource.getModel(), logicalPort);
+		Assert.assertTrue(pos != -1);
+		
+		String desc = ((LogicalPort)((ComputerSystem) resource.getModel()).getLogicalDevices().get(pos)).getDescription();
+		Assert.assertTrue(desc.equals(logicalPort.getDescription()));
+	}
+
+	/**
+	 * TODO This class has to be moved to the share helper
+	 */
+	private void createProtocolForResource(String resourceId) throws ProtocolException {
+		IProtocolManager protocolManager = getOsgiService(IProtocolManager.class, 15000);
+		ProtocolSessionContext context = ProtocolSessionHelper.newSessionContextNetconf();
+		protocolManager.getProtocolSessionManagerWithContext(resourceId, context);
+		
+		isMock = false;
+		if (context.getSessionParameters().containsKey(ProtocolSessionContext.PROTOCOL_URI)){
+			if (((String)context.getSessionParameters().get(ProtocolSessionContext.PROTOCOL_URI)).startsWith("mock")){
+				isMock = true;
+			}
+		}
+	}
 }
