97d631d4757f2765b436e3b3d81cee31fff2f5aa
isart_canyameres <isart_canyameres@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-11-04 13:54:45 +0000

    Added test for setInterfaceDescriptionAction
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/sprint_0_4@1512 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ tests/sprints/sprint0_4/src/test/java/helpers/ResourceHelper.java
@@ -0,0 +1,20 @@
+package helpers;
+
+import org.opennaas.core.resources.IResource;
+import org.opennaas.core.resources.capability.ICapability;
+
+public class ResourceHelper {
+
+	public static int containsCapability(IResource resource, String idCapability) {
+
+		int pos = 0;
+		for (ICapability capability : resource.getCapabilities()) {
+			if (capability.getCapabilityInformation().getType().equals(idCapability)) {
+				return pos;
+			}
+			pos++;
+		}
+		return -1;
+
+	}
+}
