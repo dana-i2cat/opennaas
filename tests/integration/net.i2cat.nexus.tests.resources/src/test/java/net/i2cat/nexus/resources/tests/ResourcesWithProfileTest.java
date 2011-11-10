2aa08a17aaa6ed0e553ec4bc1e9c305f9284f6ac
roc_valles <roc_valles@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-11-10 14:51:58 +0000

    net.i2cat.nexus.tests.resources became pax-exam 2 with karaf container proof of concept. Developers rejoiced.
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/sprint_0_4@1551 e5892e71-24e5-41bb-b533-28a584b410ba

--- tests/integration/net.i2cat.nexus.tests.resources/src/test/java/net/i2cat/nexus/resources/tests/ResourcesWithProfileTest.java
+++ tests/integration/net.i2cat.nexus.tests.resources/src/test/java/net/i2cat/nexus/resources/tests/ResourcesWithProfileTest.java
@@ -1,57 +1,101 @@
 package net.i2cat.nexus.resources.tests;
 
-import static org.ops4j.pax.exam.CoreOptions.mavenBundle;
-import static org.ops4j.pax.exam.OptionUtils.combine;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
 
 import org.opennaas.core.resources.ILifecycle.State;
 import org.opennaas.core.resources.IResource;
 import org.opennaas.core.resources.IResourceManager;
 import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.action.ActionSet;
+import org.opennaas.core.resources.action.IActionSet;
+import org.opennaas.core.resources.descriptor.ResourceDescriptor;
+import org.opennaas.core.resources.helpers.MockAction;
+import org.opennaas.core.resources.helpers.MockProfileFactory;
+import org.opennaas.core.resources.helpers.ResourceDescriptorFactory;
+import org.opennaas.core.resources.profile.IProfile;
 import org.opennaas.core.resources.profile.IProfileManager;
+import org.opennaas.core.resources.profile.ProfileDescriptor;
 import org.opennaas.core.resources.protocol.IProtocolManager;
 import org.opennaas.core.resources.protocol.ProtocolException;
 import org.opennaas.core.resources.protocol.ProtocolSessionContext;
-import net.i2cat.nexus.tests.IntegrationTestsHelper;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.apache.karaf.testing.AbstractIntegrationTest;
+import org.junit.Assert;
+import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.ops4j.pax.exam.Inject;
+
+import org.osgi.framework.BundleContext;
+//import org.ops4j.pax.exam.Inject;
+
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
+import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
-import org.osgi.framework.BundleContext;
+import org.ops4j.pax.exam.spi.reactors.AllConfinedStagedReactorFactory;
+import org.slf4j.LoggerFactory;
+import org.ops4j.pax.exam.util.*;
+import static org.ops4j.pax.exam.CoreOptions.*;
+import static org.ops4j.pax.exam.OptionUtils.*;
+import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.karafDistributionConfiguration;
+
 
 @RunWith(JUnit4TestRunner.class)
-public class ResourcesWithProfileTest extends AbstractIntegrationTest {
+@ExamReactorStrategy(AllConfinedStagedReactorFactory.class)
+//@ExamReactorStrategy(EagerSingleStagedReactorFactory.class)
+public class ResourcesWithProfileTest {
 	// import static org.ops4j.pax.exam.container.def.PaxRunnerOptions.vmOption;
-
+	
 	static Log			log				= LogFactory.getLog(ResourcesWithProfileTest.class);
-
+	
 	IResourceManager	resourceManager;
 	IProfileManager		profileManager;
-
+	
 	@Inject
 	BundleContext		bundleContext	= null;
-
+	
 	@Configuration
-	public static Option[] configure() {
-		//log.warn("HERE HERE HERE LOOK HERE");
+	public Option[] config() {
+		//return new Option[]{karafDistributionConfiguration().frameworkUrl(maven().groupId("org.apache.karaf").artifactId("apache-karaf").type("zip").versionAsInProject()).karafVersion("2.2.4").name("Apache Karaf")};
+		//return new Option[]{karafDistributionConfiguration().frameworkUrl("file:/home/rvalles/.m2/repository/net/i2cat/mantychore/assembly/1.0.0-SNAPSHOT/assembly-1.0.0-SNAPSHOT-bin.zip").karafVersion("2.2.4").name("mantychore")};
+		return new Option[]{karafDistributionConfiguration().frameworkUrl("mvn:net.i2cat.mantychore/assembly/1.0.0-SNAPSHOT/zip/bin").karafVersion("2.2.2").name("mantychore")};
+	}
+	
+	/*@Configuration
+	public Option[] config()
+	{
+		return options(junitBundles(),equinox(),IntegrationTestsHelper.getMantychoreTestOptions());
+	}*/
+	
+	/*public static Option[] configure() {
+		log.warn("HERE HERE HERE LOOK HERE");
 		Option[] options = combine(
 				IntegrationTestsHelper.getMantychoreTestOptions(),mavenBundle().groupId("net.i2cat.nexus").artifactId("net.i2cat.nexus.tests.helper")
 					 //, vmOption("-Xrunjdwp:transport=dt_socket,server=y,suspend=y,address=5005")
 					);
 		return options;
-	}
+		//return IntegrationTestsHelper.getMantychoreTestOptions();
+	}*/
+	
+	/*public static void main(String[] args) throws IOException {
+		//createContainer(createTestSystem(combine(new ResourcesWithProfileTest().config(),profile("gogo")))).start();
+		//createContainer(createTestSystem(new ResourcesWithProfileTest().config()));
+	}*/
 
+	
 	// @Before
 	public void initBundles() {
 		
-		IntegrationTestsHelper.waitForAllBundlesActive(bundleContext);
+		//IntegrationTestsHelper.waitForAllBundlesActive(bundleContext);
 
-		resourceManager = getOsgiService(IResourceManager.class, 20000);
-		profileManager = getOsgiService(IProfileManager.class, 20000);
+		resourceManager = ServiceLookup.getService(bundleContext, "org.opennaas.core.resources.IResourceManager");
+		profileManager = ServiceLookup.getService(bundleContext, "org.opennaas.core.resources.profile.IProfileManager");
 
 		clearRepo();
 
@@ -83,62 +127,62 @@ public class ResourcesWithProfileTest extends AbstractIntegrationTest {
 
 		log.info("Resource repo cleared!");
 	}
-//
-//	/**
-//	 * Creates a resource indicating a profileId in its descriptor. Checks profile is loaded correctly and profile actions are called instead of
-//	 * original ones.
-//	 */
-//	@Test
-//	public void createResourceWithProfile() {
-//
-//		initBundles();
-//
-//		try {
-//
-//			List<String> capabilities = new ArrayList<String>();
-//			capabilities.add("chassis");
-//			capabilities.add("queue");
-//
-//			ProfileDescriptor profileDescriptor = ResourceDescriptorFactory.newProfileDescriptor("profile", "router");
-//			ResourceDescriptor resourceDescriptor = ResourceDescriptorFactory.newResourceDescriptor("TestResource", "router", capabilities);
-//			/* specify profiles */
-//			Map<String, IActionSet> actionSets = new HashMap<String, IActionSet>();
-//			ActionSet actionSet = new ActionSet();
-//			actionSet.putAction("setIPv4", MockAction.class);
-//
-//			actionSets.put("chassis", actionSet);
-//
-//			IProfile profile = MockProfileFactory.newMockProfilefactory(profileDescriptor, actionSets);
-//
-//			profileManager.addProfile(profile);
-//
-//			resourceDescriptor.setProfileId(profile.getProfileName());
-//
-//			// call createResource(resourceDescriptor)
-//			IResource resource = resourceManager.createResource(resourceDescriptor);
-//			createProtocolForResource(resource.getResourceIdentifier().getId());
-//			resourceManager.startResource(resource.getResourceIdentifier());
-//
-//			// assert profile loading has been correct
-//			Assert.assertNotNull(resource.getProfile());
-//			Assert.assertTrue(resource.getProfile().equals(profile));
-//
-//			// // TODO launch setInterface Action and assert DummyAction is executed instead of original one
-//
-//		} catch (ResourceException e) {
-//			log.error("Error ocurred!!!", e);
-//			Assert.fail(e.getMessage());
-//		} catch (ProtocolException e) {
-//			log.error("Error ocurred!!!", e);
-//			Assert.fail(e.getMessage());
-//		} finally {
-//			clearRepo();
-//		}
-//
-//	}
+
+	/**
+	 * Creates a resource indicating a profileId in its descriptor. Checks profile is loaded correctly and profile actions are called instead of
+	 * original ones.
+	 */
+	@Test
+	public void createResourceWithProfile() {
+
+		initBundles();
+
+		try {
+
+			List<String> capabilities = new ArrayList<String>();
+			capabilities.add("chassis");
+			capabilities.add("queue");
+
+			ProfileDescriptor profileDescriptor = ResourceDescriptorFactory.newProfileDescriptor("profile", "router");
+			ResourceDescriptor resourceDescriptor = ResourceDescriptorFactory.newResourceDescriptor("TestResource", "router", capabilities);
+			/* specify profiles */
+			Map<String, IActionSet> actionSets = new HashMap<String, IActionSet>();
+			ActionSet actionSet = new ActionSet();
+			actionSet.putAction("setIPv4", MockAction.class);
+
+			actionSets.put("chassis", actionSet);
+
+			IProfile profile = MockProfileFactory.newMockProfilefactory(profileDescriptor, actionSets);
+
+			profileManager.addProfile(profile);
+
+			resourceDescriptor.setProfileId(profile.getProfileName());
+
+			// call createResource(resourceDescriptor)
+			IResource resource = resourceManager.createResource(resourceDescriptor);
+			createProtocolForResource(resource.getResourceIdentifier().getId());
+			resourceManager.startResource(resource.getResourceIdentifier());
+
+			// assert profile loading has been correct
+			Assert.assertNotNull(resource.getProfile());
+			Assert.assertTrue(resource.getProfile().equals(profile));
+
+			// // TODO launch setInterface Action and assert DummyAction is executed instead of original one
+
+		} catch (ResourceException e) {
+			log.error("Error ocurred!!!", e);
+			Assert.fail(e.getMessage());
+		} catch (ProtocolException e) {
+			log.error("Error ocurred!!!", e);
+			Assert.fail(e.getMessage());
+		} finally {
+			clearRepo();
+		}
+
+	}
 
 	private void createProtocolForResource(String resourceId) throws ProtocolException {
-		IProtocolManager protocolManager = getOsgiService(IProtocolManager.class, 15000);
+		IProtocolManager protocolManager = ServiceLookup.getService(bundleContext, "org.opennaas.core.resources.protocol.IProtocolManager");
 		protocolManager.getProtocolSessionManagerWithContext(resourceId, newSessionContextNetconf());
 
 	}
