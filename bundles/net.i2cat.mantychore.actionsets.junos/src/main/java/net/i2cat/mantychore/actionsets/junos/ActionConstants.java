6dfe49ec357cd1ca6dbd67fa06a61ebdccde174a
isart_canyameres <isart_canyameres@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-12-05 14:50:59 +0000

    Removed unused action name
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@1649 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/ActionConstants.java
+++ bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/ActionConstants.java
@@ -2,17 +2,16 @@ package net.i2cat.mantychore.actionsets.junos;
 
 public class ActionConstants {
 
-	public static final String	GETCONFIG			= "getConfiguration";
-	public static final String	SETIPv4				= "setIPv4";
+	public static final String	GETCONFIG				= "getConfiguration";
+	public static final String	SETIPv4					= "setIPv4";
 	// public static final String CONFIGLT = "configureLogicalTunnel";
 
-	public static final String	ISALIVE				= "isAlive";
-	public static final String	VALIDATE			= "validate";
+	public static final String	ISALIVE					= "isAlive";
+	public static final String	VALIDATE				= "validate";
 
 	// Chassis ActionSET
 	public static final String	DELETESUBINTERFACE		= "deletesubinterface";
-	public static final String	CREATESUBINTERFACE		= "createsubinterface";
-	public static final String  CONFIGURESUBINTERFACE	= "configureSubInterface";
+	public static final String	CONFIGURESUBINTERFACE	= "configureSubInterface";
 	public static final String	SETENCAPSULATION		= "setEncapsulation";
 	public static final String	CONFIGURESTATUS			= "configureStatus";
 	public static final String	DELETELOGICALROUTER		= "deleteLogicalRouter";
@@ -20,10 +19,9 @@ public class ActionConstants {
 	public static final String	SETINTERFACEDESCRIPTION	= "setInterfaceDescription";
 
 	// TODO THE SAME NAME FOR THE OTHER LIST LOGICAL ROUTERS
-	public static final String	GETLOGICALROUTERS	= "getLogicalRouters";
+	public static final String	GETLOGICALROUTERS		= "getLogicalRouters";
 
 	// TODO LISTLOGICALROUTER!!!
-	public static final String	LISTLOGICALROUTER	= "listLogicalRouter";
-	
+	public static final String	LISTLOGICALROUTER		= "listLogicalRouter";
 
 }
