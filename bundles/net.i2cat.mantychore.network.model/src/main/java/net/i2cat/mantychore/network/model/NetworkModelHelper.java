75645f982501ede18104bf27ec7ae1c2b7de07ba
isart_canyameres <isart_canyameres@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-12-14 16:23:35 +0000

    NetworkModelHelper added.
    git-svn-id: http://svn.i2cat.net/repos/manticore/trunk@1677 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/net.i2cat.mantychore.network.model/src/main/java/net/i2cat/mantychore/network/model/NetworkModelHelper.java
@@ -0,0 +1,103 @@
+package net.i2cat.mantychore.network.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import net.i2cat.mantychore.network.model.domain.NetworkDomain;
+import net.i2cat.mantychore.network.model.layer.Layer;
+import net.i2cat.mantychore.network.model.topology.ConnectionPoint;
+import net.i2cat.mantychore.network.model.topology.Device;
+import net.i2cat.mantychore.network.model.topology.Link;
+import net.i2cat.mantychore.network.model.topology.NetworkElement;
+import net.i2cat.mantychore.network.model.topology.TransportNetworkElement;
+
+public class NetworkModelHelper {
+
+	public static List<NetworkElement> getNetworkElementsExceptTransportElements(NetworkModel model) {
+		return getNetworkElementsExceptTransportElements(model.getNetworkElements());
+	}
+
+	public static List<NetworkElement> getNetworkElementsExceptTransportElements(List<NetworkElement> networkElements) {
+		List<NetworkElement> toReturn = new ArrayList<NetworkElement>();
+		for (NetworkElement elem : networkElements) {
+			if (!(elem instanceof TransportNetworkElement)) {
+				toReturn.add(elem);
+			}
+		}
+		return toReturn;
+	}
+
+	public static List<Device> getDevices(NetworkModel model) {
+		return getDevices(model.getNetworkElements());
+	}
+
+	public static List<Device> getDevices(List<NetworkElement> networkElements) {
+		List<Device> toReturn = new ArrayList<Device>();
+		for (NetworkElement elem : networkElements) {
+			if (elem instanceof Device) {
+				toReturn.add((Device) elem);
+			}
+		}
+		return toReturn;
+	}
+
+	public static List<NetworkDomain> getDomains(NetworkModel model) {
+		return getDomains(model.getNetworkElements());
+	}
+
+	public static List<NetworkDomain> getDomains(List<NetworkElement> networkElements) {
+		List<NetworkDomain> toReturn = new ArrayList<NetworkDomain>();
+		for (NetworkElement elem : networkElements) {
+			if (elem instanceof NetworkDomain) {
+				toReturn.add((NetworkDomain) elem);
+			}
+		}
+		return toReturn;
+	}
+
+	public static List<Link> getLinks(NetworkModel model) {
+		return getLinks(model.getNetworkElements());
+	}
+
+	public static List<Link> getLinks(List<NetworkElement> networkElements) {
+		List<Link> toReturn = new ArrayList<Link>();
+		for (NetworkElement elem : networkElements) {
+			if (elem instanceof Link) {
+				toReturn.add((Link) elem);
+			}
+		}
+		return toReturn;
+	}
+
+	public static List<Layer> getLayers(NetworkModel model) {
+		return getLayers(model.getNetworkElements());
+	}
+
+	public static List<Layer> getLayers(List<NetworkElement> networkElements) {
+		List<Layer> toReturn = new ArrayList<Layer>();
+		for (NetworkElement elem : networkElements) {
+			if (elem instanceof ConnectionPoint) {
+				Layer layer = ((ConnectionPoint) elem).getLayer();
+				// TODO implement equals in Layer
+				if (!toReturn.contains(layer))
+					toReturn.add(layer);
+			}
+		}
+		return toReturn;
+	}
+
+	public static List<ConnectionPoint> getConnectionPoints(NetworkModel model) {
+		return getConnectionPoints(model.getNetworkElements());
+	}
+
+	public static List<ConnectionPoint> getConnectionPoints(List<NetworkElement> networkElements) {
+		List<ConnectionPoint> toReturn = new ArrayList<ConnectionPoint>();
+		for (NetworkElement elem : networkElements) {
+			if (elem instanceof ConnectionPoint) {
+				toReturn.add((ConnectionPoint) elem);
+			}
+		}
+		return toReturn;
+	}
+
+}
