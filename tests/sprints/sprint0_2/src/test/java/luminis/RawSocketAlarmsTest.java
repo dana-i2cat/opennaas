91c35a02c79a84c126116a94674c1a1aef7ebb7a
isart_canyameres <isart_canyameres@e5892e71-24e5-41bb-b533-28a584b410ba>
2011-12-20 13:57:28 +0000

    Fixes in test
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/pax2_3@1711 e5892e71-24e5-41bb-b533-28a584b410ba

--- tests/sprints/sprint0_2/src/test/java/luminis/RawSocketAlarmsTest.java
+++ tests/sprints/sprint0_2/src/test/java/luminis/RawSocketAlarmsTest.java
@@ -209,7 +209,7 @@ public class RawSocketAlarmsTest extends AbstractIntegrationTest implements Even
 
 	@Test
 	public void checkAllAlarmsAreSupportedTest() {
-		String chassis = "01";
+		String chassis = "00";
 		String slot = "01";
 
 		OpticalSwitchFactory factory = new OpticalSwitchFactory();
@@ -239,7 +239,7 @@ public class RawSocketAlarmsTest extends AbstractIntegrationTest implements Even
 		}
 
 		// check an unknown alarm is created
-		String unknownAlarmMessage = "FFFF0000FFFFFFFFFFFF";
+		String unknownAlarmMessage = "FFFF0000FFFFFFFFFF";
 		Properties properties = WonesysAlarmFactory.loadAlarmProperties(unknownAlarmMessage);
 		alarm = WonesysAlarmFactory.createAlarm(properties);
 
