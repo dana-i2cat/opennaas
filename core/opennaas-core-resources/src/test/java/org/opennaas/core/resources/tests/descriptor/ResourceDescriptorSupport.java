379fa383b3b628bccd78de3e6b9e063778103528
carlos <carlos@mint>
2011-12-20 09:37:03 +0100

    Refactorized test

--- core/opennaas-core-resources/src/test/java/org/opennaas/core/resources/tests/descriptor/ResourceDescriptorSupport.java
+++ core/opennaas-core-resources/src/test/java/org/opennaas/core/resources/tests/descriptor/ResourceDescriptorSupport.java
@@ -5,19 +5,17 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import junit.framework.TestCase;
+
 import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
 import org.opennaas.core.resources.descriptor.CapabilityProperty;
 import org.opennaas.core.resources.descriptor.Information;
 import org.opennaas.core.resources.descriptor.ResourceDescriptor;
 import org.opennaas.core.resources.helpers.MockNetworkDescriptor;
 
-
-
-import junit.framework.TestCase;
-
 public class ResourceDescriptorSupport extends TestCase
 {
-	
+
 	protected ResourceDescriptor createSampleDescriptor() {
 		ResourceDescriptor config = new ResourceDescriptor();
 		config.setId(new String("1"));
@@ -36,12 +34,13 @@ public class ResourceDescriptorSupport extends TestCase
 		config.setCapabilityDescriptors(capabilityDescriptors);
 		return config;
 	}
-	
+
 	/**
-	 * Create a resource with properties 
+	 * Create a resource with properties
+	 * 
 	 * @return
 	 */
-	protected ResourceDescriptor createNetworkDescriptor() {	
+	protected ResourceDescriptor createNetworkDescriptor() {
 		ResourceDescriptor config = new ResourceDescriptor();
 		config.setId(new String("1"));
 		/* resource description */
@@ -51,33 +50,34 @@ public class ResourceDescriptorSupport extends TestCase
 		info.setName("networklayer1.0");
 		info.setVersion("1.0.0");
 		config.setInformation(info);
-		
+
 		/* capability description */
 		CapabilityDescriptor capabilityDescriptor = new CapabilityDescriptor();
 		List<CapabilityProperty> properties = new ArrayList<CapabilityProperty>();
 		properties.add(new CapabilityProperty("name", "value"));
 		capabilityDescriptor.setCapabilityProperties(properties);
 		List<CapabilityDescriptor> capabilityDescriptors = new ArrayList<CapabilityDescriptor>();
-		capabilityDescriptors.add(capabilityDescriptor);		
+		capabilityDescriptors.add(capabilityDescriptor);
 		config.setCapabilityDescriptors(capabilityDescriptors);
-		
-	
+
 		/* read from a file descriptor */
 		config.setFileTopology("network/network_example1.xml");
-		
-		//Testing if it loads a network topology 
+
+		// Testing if it loads a network topology
 		config.setNetworkTopology(MockNetworkDescriptor.newSimpleNDLNetworkDescriptor());
-		
+
 		return config;
 	}
-	
+
 	/**
-	 * Create a resource with properties 
+	 * Create a resource with properties
+	 * 
 	 * @return
 	 */
-	protected ResourceDescriptor createNetworkDescriptorWithNetworkDomains() {	
+	protected ResourceDescriptor createNetworkDescriptorWithNetworkDomains() {
 		ResourceDescriptor config = new ResourceDescriptor();
 		config.setId(new String("1"));
+
 		/* resource description */
 		Information info = new Information();
 		info.setDescription("network description");
@@ -85,30 +85,28 @@ public class ResourceDescriptorSupport extends TestCase
 		info.setName("networklayer1.0");
 		info.setVersion("1.0.0");
 		config.setInformation(info);
-		
+
 		/* capability description */
 		CapabilityDescriptor capabilityDescriptor = new CapabilityDescriptor();
 		List<CapabilityProperty> properties = new ArrayList<CapabilityProperty>();
 		properties.add(new CapabilityProperty("name", "value"));
 		capabilityDescriptor.setCapabilityProperties(properties);
 		List<CapabilityDescriptor> capabilityDescriptors = new ArrayList<CapabilityDescriptor>();
-		capabilityDescriptors.add(capabilityDescriptor);		
+		capabilityDescriptors.add(capabilityDescriptor);
 		config.setCapabilityDescriptors(capabilityDescriptors);
-		
-	
+
 		/* read from a file descriptor */
 		config.setFileTopology("network/network_diffs_layer.xml");
-		
-		//Testing if it loads a network topology 
+
+		// Testing if it loads a network topology
 		config.setNetworkTopology(MockNetworkDescriptor.newNetworkDescriptorWithNetworkDomain());
-		
+
 		return config;
 	}
-	
-	
-	
+
 	/**
 	 * Create network descriptor
+	 * 
 	 * @return
 	 */
 	protected ResourceDescriptor createVirtualResourceDescriptor() {
@@ -120,7 +118,7 @@ public class ResourceDescriptorSupport extends TestCase
 		info.setName("logical1");
 		info.setVersion("1.0.0");
 		config.setInformation(info);
-				
+
 		CapabilityDescriptor capabilityDescriptor = new CapabilityDescriptor();
 		List<CapabilityProperty> properties = new ArrayList<CapabilityProperty>();
 		properties.add(new CapabilityProperty("name", "value"));
@@ -128,15 +126,12 @@ public class ResourceDescriptorSupport extends TestCase
 		List<CapabilityDescriptor> capabilityDescriptors = new ArrayList<CapabilityDescriptor>();
 		capabilityDescriptors.add(capabilityDescriptor);
 		config.setCapabilityDescriptors(capabilityDescriptors);
-	
-		Map<String, String> props = new HashMap<String,String>();
-		props.put("virtual","true");
+
+		Map<String, String> props = new HashMap<String, String>();
+		props.put("virtual", "true");
 		config.setProperties(props);
-		
+
 		return config;
 	}
 
-
-	
-	
 }
