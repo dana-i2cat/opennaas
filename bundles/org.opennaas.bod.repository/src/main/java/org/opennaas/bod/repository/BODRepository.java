c92feda020dff026c949b5daf0ab1396aef39f4d
jordi_puig <jordi_puig@e5892e71-24e5-41bb-b533-28a584b410ba>
2012-01-25 09:58:26 +0000

    OPENNAAS-167 - Create BoD repository Implement BODRepository.class and core.xml
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/bod@1753 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/org.opennaas.bod.repository/src/main/java/org/opennaas/bod/repository/BODRepository.java
+++ bundles/org.opennaas.bod.repository/src/main/java/org/opennaas/bod/repository/BODRepository.java
@@ -3,11 +3,40 @@ package org.opennaas.bod.repository;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opennaas.core.resources.ResourceRepository;
+import org.opennaas.core.resources.capability.ICapabilityFactory;
 
 public class BODRepository extends ResourceRepository {
+
+	/**
+	 * Logger
+	 */
 	Log	log	= LogFactory.getLog(BODRepository.class);
 
+	/**
+	 * @param resourceType
+	 * @param persistenceUnit
+	 */
 	public BODRepository(String resourceType, String persistenceUnit) {
 		super(resourceType, persistenceUnit);
+	}
+
+	/**
+	 * Add a factory to the capability factories
+	 * 
+	 * @param capabilityFactory
+	 */
+	public void capabilityFactoryAdded(ICapabilityFactory capabilityFactory) {
+		log.info("Adding factory: " + capabilityFactory.getType());
+		this.capabilityFactories.put(capabilityFactory.getType(), capabilityFactory);
+	}
+
+	/**
+	 * Remove a factory from the capability factories
+	 * 
+	 * @param capabilityFactory
+	 */
+	public void capabilityFactoryDeleted(ICapabilityFactory capabilityFactory) {
+		log.info("Deleting factory: " + capabilityFactory.getType());
+		this.capabilityFactories.remove(capabilityFactory.getType());
 	}
 }
