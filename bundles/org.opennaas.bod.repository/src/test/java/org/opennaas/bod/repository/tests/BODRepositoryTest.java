613fe501686659c122fac4a83efe195966fd491c
jordi_puig <jordi_puig@e5892e71-24e5-41bb-b533-28a584b410ba>
2012-01-25 13:31:56 +0000

    OPENNAAS-167 - Create BoD repository Implemented unit tests
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/bod@1758 e5892e71-24e5-41bb-b533-28a584b410ba

--- bundles/org.opennaas.bod.repository/src/test/java/org/opennaas/bod/repository/tests/BODRepositoryTest.java
+++ bundles/org.opennaas.bod.repository/src/test/java/org/opennaas/bod/repository/tests/BODRepositoryTest.java
@@ -1,7 +1,19 @@
 package org.opennaas.bod.repository.tests;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
 import org.junit.Test;
 import org.opennaas.bod.repository.BODRepository;
+import org.opennaas.core.resources.IResource;
+import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
+import org.opennaas.core.resources.descriptor.Information;
+import org.opennaas.core.resources.descriptor.ResourceDescriptor;
+import org.opennaas.core.resources.descriptor.ResourceDescriptorConstants;
 
 public class BODRepositoryTest {
 	BODRepository	bodRepository;
@@ -11,16 +23,132 @@ public class BODRepositoryTest {
 	@Test
 	public void testCapabilityFactories() {
 
+		bodRepository = new BODRepository("bodrerepository", persistenceUnit);
+		bodRepository.setResourceDescriptorRepository(new MockDescriptorRepository());
+
+		bodRepository.capabilityFactoryAdded(new MockCapabilityFactory("factory1"));
+		bodRepository.capabilityFactoryAdded(new MockCapabilityFactory("factory2"));
+
+		Assert.assertTrue(bodRepository.getCapabilityFactories().size() == 2);
+		Assert.assertNotNull(bodRepository.getCapabilityFactories().get("factory1"));
+		Assert.assertNotNull(bodRepository.getCapabilityFactories().get("factory2"));
 	}
 
 	@Test
 	public void testCreateRemoveResources() {
 
+		bodRepository = new BODRepository("bod", persistenceUnit);
+		bodRepository.setResourceDescriptorRepository(new MockDescriptorRepository());
+
+		bodRepository.capabilityFactoryAdded(new MockCapabilityFactory("factory1"));
+		bodRepository.capabilityFactoryAdded(new MockCapabilityFactory("factory2"));
+
+		try {
+			/* type of resources */
+
+			ResourceDescriptor descriptor = newResourceDescriptor("bod", "bod1");
+			IResource resource = bodRepository.createResource(descriptor);
+			String id = resource.getResourceIdentifier().getId();
+			Assert.assertNotNull(id);
+			Assert.assertTrue(resource.getResourceDescriptor().equals(descriptor));
+			Assert.assertTrue(resource.getResourceIdentifier().getType().equals("bod"));
+
+			boolean thrown = false;
+			bodRepository.removeResource(id);
+			try {
+				bodRepository.getResource(id);
+			} catch (Exception e) {
+				thrown = true;
+			}
+			Assert.assertTrue(thrown);
+
+		} catch (ResourceException e) {
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
 	}
 
 	@Test
 	public void testModifyResources() {
 
+		bodRepository = new BODRepository("bod", persistenceUnit);
+		bodRepository.setResourceDescriptorRepository(new MockDescriptorRepository());
+
+		bodRepository.capabilityFactoryAdded(new MockCapabilityFactory("factory1"));
+		bodRepository.capabilityFactoryAdded(new MockCapabilityFactory("factory2"));
+
+		try {
+			/* type of resources */
+			IResource resource = bodRepository.createResource(newResourceDescriptor("bod", "bod1"));
+			String id = resource.getResourceIdentifier().getId();
+			Assert.assertTrue(resource.getResourceDescriptor().getCapabilityDescriptors().size() == 2);
+			Assert.assertTrue(resource.getResourceIdentifier().getType().equals("bod"));
+
+			resource = bodRepository.modifyResource(id, oldResourceDescriptor(id, "bod", "bod1"));
+			IResource newResource = bodRepository.getResource(resource.getResourceIdentifier().getId());
+			Assert.assertTrue(newResource.getResourceDescriptor().getCapabilityDescriptors().size() == 1);
+			Assert.assertTrue(newResource.getResourceIdentifier().getType().equals("bod"));
+
+		} catch (ResourceException e) {
+			e.printStackTrace();
+			Assert.fail(e.getMessage());
+		}
+	}
+
+	private ResourceDescriptor newResourceDescriptor(String type, String name) {
+
+		ResourceDescriptor resourceDescriptor = new ResourceDescriptor();
+
+		Map<String, String> properties = new HashMap<String, String>();
+
+		/* FIXME PUT PROTOCOL_URI IN RESOURCE DESCRIPTOR CONSTANTS */
+		properties.put(ResourceDescriptorConstants.PROTOCOL_URI,
+				"user:pass@host.net:2212");
+
+		List<CapabilityDescriptor> capabilityDescriptors = new ArrayList<CapabilityDescriptor>();
+
+		/* factory1 descriptor */
+		capabilityDescriptors.add(MockResource.createCapabilityDescriptor("factory1"));
+
+		/* factory2 descriptor */
+		capabilityDescriptors.add(MockResource.createCapabilityDescriptor("factory2"));
+
+		resourceDescriptor.setProperties(properties);
+		resourceDescriptor.setCapabilityDescriptors(capabilityDescriptors);
+		resourceDescriptor.setId("JunosTest");
+		/* information. It is only necessary to add type */
+		Information information = new Information();
+		information.setType(type);
+		information.setName(name);
+		resourceDescriptor.setInformation(information);
+		return resourceDescriptor;
+	}
+
+	private ResourceDescriptor oldResourceDescriptor(String resourceId, String type, String name) {
+
+		ResourceDescriptor resourceDescriptor = new ResourceDescriptor();
+
+		Map<String, String> properties = new HashMap<String, String>();
+
+		/* FIXME PUT PROTOCOL_URI IN RESOURCE DESCRIPTOR CONSTANTS */
+		properties.put(ResourceDescriptorConstants.PROTOCOL_URI,
+				"user:pass@host.net:2212");
+
+		List<CapabilityDescriptor> capabilityDescriptors = new ArrayList<CapabilityDescriptor>();
+
+		/* factory1 descriptor */
+		capabilityDescriptors.add(MockResource.createCapabilityDescriptor("factory1"));
+
+		resourceDescriptor.setProperties(properties);
+		resourceDescriptor.setCapabilityDescriptors(capabilityDescriptors);
+		resourceDescriptor.setId(resourceId);
+
+		/* information. It is only necessary to add type */
+		Information information = new Information();
+		information.setType(type);
+		information.setName(name);
+		resourceDescriptor.setInformation(information);
+		return resourceDescriptor;
 	}
 
 }
