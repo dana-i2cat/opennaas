89c3699ada1f248c91c6d757471ea7cd5f78d7d6
jordi_puig <jordi_puig@e5892e71-24e5-41bb-b533-28a584b410ba>
2012-01-25 11:36:31 +0000

    OPENNAAS-167 - Create BoD repository Generate unit test skeleton
    git-svn-id: http://svn.i2cat.net/repos/manticore/branches/bod@1756 e5892e71-24e5-41bb-b533-28a584b410ba

---
+++ bundles/org.opennaas.bod.repository/src/test/java/org/opennaas/bod/repository/tests/BODRepositoryTest.java
@@ -0,0 +1,26 @@
+package org.opennaas.bod.repository.tests;
+
+import org.junit.Test;
+import org.opennaas.bod.repository.BODRepository;
+
+public class BODRepositoryTest {
+	BODRepository	bodRepository;
+	String			resourceType	= "bod";
+	String			persistenceUnit	= "ResourceCore";
+
+	@Test
+	public void testCapabilityFactories() {
+
+	}
+
+	@Test
+	public void testCreateRemoveResources() {
+
+	}
+
+	@Test
+	public void testModifyResources() {
+
+	}
+
+}
