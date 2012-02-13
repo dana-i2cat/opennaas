2f4aa06609093b08531f92a4b71c11aacc279e00
isartcanyameres <isart.canyameres@i2cat.net>
2012-02-13 18:28:36 +0100

    Created OSPF actionset skeleton.
    OSPFActionSet class contains actions to manage OSPF.
    blueprint/core.xml has been updated to export OSPFActionSet as a service.
    
    Actions are not implemented (they are stubs).

---
+++ manticore/bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/actions/ospf/EnableOSPFInInterfaceAction.java
@@ -0,0 +1,35 @@
+package net.i2cat.mantychore.actionsets.junos.actions.ospf;
+
+import net.i2cat.mantychore.actionsets.junos.actions.JunosAction;
+
+import org.opennaas.core.resources.action.ActionException;
+import org.opennaas.core.resources.action.ActionResponse;
+import org.opennaas.core.resources.protocol.IProtocolSession;
+
+public class EnableOSPFInInterfaceAction extends JunosAction {
+
+	@Override
+	public void executeListCommand(ActionResponse actionResponse, IProtocolSession protocol) throws ActionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void parseResponse(Object responseMessage, Object model) throws ActionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean checkParams(Object params) throws ActionException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void prepareMessage() throws ActionException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
