5e35df22b13a70664a3742972d9fba352fef0b12
Jordi Puig <jordi.puig@i2cat.net>
2012-02-21 13:15:29 +0100

    OPENNAAS-204/205 - Action activate/deactivate OSPF
    Remove both actions and create an unic action that includes these actions.

--- manticore/bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/OSPFActionSet.java
+++ manticore/bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/OSPFActionSet.java
@@ -2,9 +2,8 @@ package net.i2cat.mantychore.actionsets.junos;
 
 import net.i2cat.mantychore.actionsets.junos.actions.ospf.ActivateOSPFAction;
 import net.i2cat.mantychore.actionsets.junos.actions.ospf.ConfigureOSPFAction;
+import net.i2cat.mantychore.actionsets.junos.actions.ospf.ConfigureOSPFInterfaceStatusAction;
 import net.i2cat.mantychore.actionsets.junos.actions.ospf.DeactivateOSPFAction;
-import net.i2cat.mantychore.actionsets.junos.actions.ospf.DisableOSPFInInterfaceAction;
-import net.i2cat.mantychore.actionsets.junos.actions.ospf.EnableOSPFInInterfaceAction;
 import net.i2cat.mantychore.actionsets.junos.actions.ospf.GetOSPFConfigAction;
 
 import org.opennaas.core.resources.action.ActionSet;
@@ -18,8 +17,8 @@ public class OSPFActionSet extends ActionSet {
 		this.putAction(ActionConstants.OSPF_CONFIGURE, ConfigureOSPFAction.class);
 		this.putAction(ActionConstants.OSPF_ACTIVATE, ActivateOSPFAction.class);
 		this.putAction(ActionConstants.OSPF_DEACTIVATE, DeactivateOSPFAction.class);
-		this.putAction(ActionConstants.OSPF_ENABLE_INTERFACE, EnableOSPFInInterfaceAction.class);
-		this.putAction(ActionConstants.OSPF_DISABLE_INTERFACE, DisableOSPFInInterfaceAction.class);
+		this.putAction(ActionConstants.OSPF_ENABLE_INTERFACE, ConfigureOSPFInterfaceStatusAction.class);
+		this.putAction(ActionConstants.OSPF_DISABLE_INTERFACE, ConfigureOSPFInterfaceStatusAction.class);
 
 		/* add refresh actions */
 		this.refreshActions.add(ActionConstants.OSPF_GET_CONFIGURATION);
