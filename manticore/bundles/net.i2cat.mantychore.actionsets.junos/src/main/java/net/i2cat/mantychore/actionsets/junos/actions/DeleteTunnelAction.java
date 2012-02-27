00832a162a5be52e5e10343affc72c13bb5d8cfa
Jordi Puig <jordi.puig@i2cat.net>
2012-02-27 09:59:31 +0100

    OPENNAAS-195 - Action deleteGRETunnel in Junos10

--- manticore/bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/actions/DeleteTunnelAction.java
+++ manticore/bundles/net.i2cat.mantychore.actionsets.junos/src/main/java/net/i2cat/mantychore/actionsets/junos/actions/DeleteTunnelAction.java
@@ -1,45 +1,141 @@
 package net.i2cat.mantychore.actionsets.junos.actions;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.i2cat.mantychore.actionsets.junos.ActionConstants;
+import net.i2cat.mantychore.commandsets.junos.commands.EditNetconfCommand;
+import net.i2cat.mantychore.commandsets.junos.commons.IPUtilsHelper;
+import net.i2cat.mantychore.model.ComputerSystem;
+import net.i2cat.mantychore.model.EnabledLogicalElement.EnabledState;
+import net.i2cat.mantychore.model.GRETunnelService;
 
 import org.opennaas.core.resources.action.ActionException;
 import org.opennaas.core.resources.action.ActionResponse;
+import org.opennaas.core.resources.command.Response;
 import org.opennaas.core.resources.protocol.IProtocolSession;
 
+/**
+ * @author Jordi Puig
+ */
 public class DeleteTunnelAction extends JunosAction {
 
+	private static final String	VELOCITY_TEMPLATE	= "/VM_files/deleteTunnel.vm";
+
+	private static final String	PROTOCOL_NAME		= "netconf";
+
+	/**
+	 * 
+	 */
 	public DeleteTunnelAction() {
-		super();
-		initialize();
+		setActionID(ActionConstants.DELETETUNNEL);
+		setTemplate(VELOCITY_TEMPLATE);
+		this.protocolName = PROTOCOL_NAME;
 	}
 
-	public void initialize() {
-		this.setActionID(ActionConstants.DELETETUNNEL);
-		setTemplate("");
-		this.protocolName = "netconf";
-	}
-
+	/**
+	 * Send the command to the protocol session
+	 * 
+	 * @param actionResponse
+	 * @param protocol
+	 * @throws ActionException
+	 */
 	@Override
 	public void executeListCommand(ActionResponse actionResponse, IProtocolSession protocol) throws ActionException {
-		// TODO Auto-generated method stub
-
+		try {
+			EditNetconfCommand command = new EditNetconfCommand(getVelocityMessage());
+			command.initialize();
+			Response response = sendCommandToProtocol(command, protocol);
+			actionResponse.addResponse(response);
+		} catch (Exception e) {
+			throw new ActionException(this.actionID, e);
+		}
+		validateAction(actionResponse);
 	}
 
-	@Override
-	public void parseResponse(Object responseMessage, Object model) throws ActionException {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public boolean checkParams(Object params) throws ActionException {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
+	/**
+	 * Create the velocity template to send info to the Junos device
+	 * 
+	 * @throws ActionException
+	 */
 	@Override
 	public void prepareMessage() throws ActionException {
-		// TODO Auto-generated method stub
+		// validate input parameters
+		validate();
 
+		try {
+			String elementName = "";
+			if (((ComputerSystem) modelToUpdate).getElementName() != null) {
+				// is logicalRouter, add LRName param
+				elementName = ((ComputerSystem) modelToUpdate).getElementName();
+			}
+
+			Map<String, Object> extraParams = new HashMap<String, Object>();
+			extraParams.put("disabledState", EnabledState.DISABLED.toString());
+			extraParams.put("enableState", EnabledState.ENABLED.toString());
+			extraParams.put("ipUtilsHelper", IPUtilsHelper.class);
+			extraParams.put("elementName", elementName);
+
+			setVelocityMessage(prepareVelocityCommand(params, template, extraParams));
+		} catch (Exception e) {
+			throw new ActionException(e);
+		}
+	}
+
+	/**
+	 * We do not have to do anything with the response
+	 * 
+	 * @param responseMessage
+	 * @param model
+	 * @throws ActionException
+	 */
+	@Override
+	public void parseResponse(Object responseMessage, Object model) throws ActionException {
+		// Nothing to do
+	}
+
+	/**
+	 * Params must be a GRETunnelService
+	 * 
+	 * @param params
+	 *            it should be a GRETunnelService
+	 * @return false if params is null or is not a GRETunnelService
+	 */
+	@Override
+	public boolean checkParams(Object params) {
+		boolean paramsOK = true;
+		if (params == null || !(params instanceof GRETunnelService)) {
+			paramsOK = false;
+		}
+
+		return paramsOK;
+	}
+
+	/**
+	 * @param template
+	 * @throws ActionException
+	 *             if template is null or empty
+	 */
+	private boolean checkTemplate(String template) throws ActionException {
+		boolean templateOK = true;
+		// The template can not be null or empty
+		if (template == null || template.equals("")) {
+			templateOK = false;
+		}
+
+		return templateOK;
+	}
+
+	/**
+	 * @throws ActionException
+	 */
+	private void validate() throws ActionException {
+		if (!checkTemplate(template)) {
+			throw new ActionException("The path to Velocity template in Action " + getActionID() + " is null");
+		}
+
+		if (!checkParams(params)) {
+			throw new ActionException("Invalid parameters for action " + getActionID());
+		}
 	}
 }
