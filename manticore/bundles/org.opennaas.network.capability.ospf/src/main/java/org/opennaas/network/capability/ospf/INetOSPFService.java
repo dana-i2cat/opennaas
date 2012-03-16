c0961c41a98057e97fcb90cbbd771b717532411d
isartcanyameres <isart.canyameres@i2cat.net>
2012-03-16 19:52:20 +0100

    Implement NetOSPFCapability activateOSPF method.

--- manticore/bundles/org.opennaas.network.capability.ospf/src/main/java/org/opennaas/network/capability/ospf/INetOSPFService.java
+++ manticore/bundles/org.opennaas.network.capability.ospf/src/main/java/org/opennaas/network/capability/ospf/INetOSPFService.java
@@ -8,6 +8,9 @@ public interface INetOSPFService {
 	/**
 	 * Enable OSPF on the network.
 	 * 
+	 * 1) Configures OSPF in all routers of the network. </b> 2) Configures a backbone area in them. </b> 3) Add all interfaces on this routers to
+	 * this area </b> 4) Enables OSPF in all routers </b>
+	 * 
 	 * @return
 	 * @throws CapabilityException
 	 */
