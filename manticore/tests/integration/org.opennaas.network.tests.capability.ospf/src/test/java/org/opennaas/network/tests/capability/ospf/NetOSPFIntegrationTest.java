c4cfcdcb037d045d44f35e075b0b2c35f094fffa
isartcanyameres <isart.canyameres@i2cat.net>
2012-03-16 21:00:25 +0100

    Test activateOSPF queue actions in routers.

--- manticore/tests/integration/org.opennaas.network.tests.capability.ospf/src/test/java/org/opennaas/network/tests/capability/ospf/NetOSPFIntegrationTest.java
+++ manticore/tests/integration/org.opennaas.network.tests.capability.ospf/src/test/java/org/opennaas/network/tests/capability/ospf/NetOSPFIntegrationTest.java
@@ -4,6 +4,8 @@ import static net.i2cat.nexus.tests.OpennaasExamOptions.includeFeatures;
 import static net.i2cat.nexus.tests.OpennaasExamOptions.includeTestHelper;
 import static net.i2cat.nexus.tests.OpennaasExamOptions.noConsole;
 import static net.i2cat.nexus.tests.OpennaasExamOptions.opennaasDistributionConfiguration;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import static org.openengsb.labs.paxexam.karaf.options.KarafDistributionOption.keepRuntimeFolder;
 import static org.ops4j.pax.exam.CoreOptions.options;
 
@@ -12,21 +14,28 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import net.i2cat.mantychore.actionsets.junos.ActionConstants;
+import net.i2cat.mantychore.network.capability.basic.ITopologyManager;
+import net.i2cat.mantychore.queuemanager.IQueueManagerService;
 import net.i2cat.nexus.tests.ResourceHelper;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.opennaas.core.resources.ILifecycle.State;
 import org.opennaas.core.resources.IResource;
 import org.opennaas.core.resources.IResourceManager;
 import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.capability.CapabilityException;
 import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
+import org.opennaas.core.resources.descriptor.Information;
 import org.opennaas.core.resources.descriptor.ResourceDescriptor;
 import org.opennaas.core.resources.protocol.IProtocolManager;
+import org.opennaas.core.resources.protocol.IProtocolSessionManager;
 import org.opennaas.core.resources.protocol.ProtocolException;
+import org.opennaas.core.resources.protocol.ProtocolSessionContext;
+import org.opennaas.network.capability.ospf.INetOSPFService;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
@@ -38,6 +47,7 @@ public class NetOSPFIntegrationTest {
 	protected static final String	CAPABILITY_URI			= "mock://user:pass@host.net:2212/mocksubsystem";
 	protected static final String	QUEUE_CAPABILIY_TYPE	= "netqueue";
 	protected static final String	OSPF_CAPABILIY_TYPE		= "netospf";
+	protected static final String	BASIC_CAPABILIY_TYPE	= "basicNetwork";
 	protected static final String	CAPABILIY_VERSION		= null;
 	protected static final String	RESOURCE_TYPE			= "network";
 	protected static final String	RESOURCE_INFO_NAME		= "OSPF Test";
@@ -55,7 +65,7 @@ public class NetOSPFIntegrationTest {
 	@Configuration
 	public static Option[] configuration() {
 		return options(opennaasDistributionConfiguration(),
-				includeFeatures("opennaas-network, "),
+				includeFeatures("opennaas-network, opennaas-router"),
 				includeTestHelper(),
 				noConsole(),
 				keepRuntimeFolder());
@@ -87,6 +97,10 @@ public class NetOSPFIntegrationTest {
 		// Add OSPF Capability Descriptor
 		List<CapabilityDescriptor> lCapabilityDescriptors = new ArrayList<CapabilityDescriptor>();
 
+		CapabilityDescriptor basicCapabilityDescriptor = ResourceHelper.newCapabilityDescriptor(ACTION_NAME, CAPABILIY_VERSION, BASIC_CAPABILIY_TYPE,
+				CAPABILITY_URI);
+		lCapabilityDescriptors.add(basicCapabilityDescriptor);
+
 		CapabilityDescriptor ospfCapabilityDescriptor = ResourceHelper.newCapabilityDescriptor(ACTION_NAME, CAPABILIY_VERSION, OSPF_CAPABILIY_TYPE,
 				CAPABILITY_URI);
 		lCapabilityDescriptors.add(ospfCapabilityDescriptor);
@@ -117,14 +131,102 @@ public class NetOSPFIntegrationTest {
 	{
 		clearRepository();
 		startResource();
-		Assert.assertTrue(networkResource.getCapabilities().size() == 2);
+		assertEquals(3, networkResource.getCapabilities().size());
 
 		stopResource();
-		Assert.assertTrue(resourceManager.listResources().isEmpty());
+	}
+
+	@Test
+	public void testActivateOSPF() throws ResourceException, ProtocolException {
+		clearRepository();
+		startResource();
+
+		List<IResource> routers = addRoutersToResourceManager();
+		addRoutersToNetwork(routers);
+
+		callActivateOSPF();
+
+		checkRoutersQueueContainsOSPFConfigActions(routers);
+	}
+
+	private void checkRoutersQueueContainsOSPFConfigActions(List<IResource> routers) {
+
+		Information inf = new Information();
+		inf.setType("queue");
+
+		for (IResource router : routers) {
+
+			IQueueManagerService queueCapab = (IQueueManagerService) router.getCapability(inf);
+			assertEquals(4, queueCapab.getActions().size());
+
+			assertEquals(ActionConstants.OSPF_CONFIGURE, queueCapab.getActions().get(0).getActionID());
+			assertEquals(ActionConstants.OSPF_CONFIGURE_AREA, queueCapab.getActions().get(1).getActionID());
+			assertEquals(ActionConstants.OSPF_ADD_INTERFACE_IN_AREA, queueCapab.getActions().get(2).getActionID());
+			assertEquals(ActionConstants.OSPF_ACTIVATE + "/" + ActionConstants.OSPF_DEACTIVATE, queueCapab.getActions().get(3).getActionID());
+		}
+	}
+
+	private void callActivateOSPF() throws CapabilityException {
+
+		Information inf = new Information();
+		inf.setType(OSPF_CAPABILIY_TYPE);
+
+		INetOSPFService capability = (INetOSPFService) networkResource.getCapability(inf);
+		assertNotNull(capability);
+
+		capability.activateOSPF();
+	}
+
+	private void addRoutersToNetwork(List<IResource> routers) throws CapabilityException {
+		Information inf = new Information();
+		inf.setType(BASIC_CAPABILIY_TYPE);
+
+		ITopologyManager topologyCapability = (ITopologyManager) networkResource.getCapability(inf);
+		assertNotNull(topologyCapability);
+
+		for (IResource router : routers) {
+			topologyCapability.addResource(router);
+		}
+	}
+
+	private List<IResource> addRoutersToResourceManager() throws ResourceException, ProtocolException {
+
+		List<IResource> resources = new ArrayList<IResource>(2);
+		resources.add(startRouterResource("router1"));
+		resources.add(startRouterResource("router2"));
+
+		return resources;
+
+	}
+
+	private IResource startRouterResource(String routerName) throws ResourceException, ProtocolException {
+
+		List<CapabilityDescriptor> lCapabilityDescriptors = new ArrayList<CapabilityDescriptor>();
+		CapabilityDescriptor ospfCapabilityDescriptor = ResourceHelper.newCapabilityDescriptor("junos", "10.10",
+				"ospf", CAPABILITY_URI);
+		lCapabilityDescriptors.add(ospfCapabilityDescriptor);
+
+		// Add Queue Capability Descriptor
+		CapabilityDescriptor queueCapabilityDescriptor = ResourceHelper.newQueueCapabilityDescriptor();
+		lCapabilityDescriptors.add(queueCapabilityDescriptor);
+
+		// Router Resource Descriptor
+		ResourceDescriptor resourceDescriptor = ResourceHelper.newResourceDescriptor(lCapabilityDescriptors, "router", RESOURCE_URI,
+				routerName);
+
+		IResource routerResource = resourceManager.createResource(resourceDescriptor);
+
+		// If not exists the protocol session manager, it's created and add the session context
+		addSessionContext(routerResource.getResourceIdentifier().getId());
+
+		// Start resource
+		resourceManager.startResource(routerResource.getResourceIdentifier());
+
+		return routerResource;
 	}
 
 	/**
-	 * Stop and remove the network resourc e
+	 * Stop and remove the network resource
 	 * 
 	 * @throws ResourceException
 	 * @throws ProtocolException
@@ -135,6 +237,26 @@ public class NetOSPFIntegrationTest {
 
 		// Remove resource
 		resourceManager.removeResource(networkResource.getResourceIdentifier());
+	}
+
+	/**
+	 * If not exists the protocol session manager, it's created and add the session context
+	 * 
+	 * @param resourceId
+	 * @throws ProtocolException
+	 */
+	private IProtocolSessionManager addSessionContext(String resourceId) throws ProtocolException {
+		ProtocolSessionContext protocolSessionContext = new ProtocolSessionContext();
+		IProtocolSessionManager protocolSessionManager = protocolManager.getProtocolSessionManager(resourceId);
+
+		protocolSessionContext.addParameter(
+				ProtocolSessionContext.PROTOCOL_URI, RESOURCE_URI);
+		protocolSessionContext.addParameter(ProtocolSessionContext.PROTOCOL,
+				"netconf");
+
+		protocolSessionManager.registerContext(protocolSessionContext);
+
+		return protocolSessionManager;
 	}
 
 }
