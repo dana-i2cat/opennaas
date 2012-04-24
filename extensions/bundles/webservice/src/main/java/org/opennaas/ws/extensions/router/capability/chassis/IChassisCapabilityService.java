429cf4a7b615fcb7dad3fb29bfc4fb23cad68e8c
Jordi Puig <jordi.puig@i2cat.net>
2012-04-24 12:30:09 +0200

    Add Chassis Capability Dummy Service

--- extensions/bundles/webservice/src/main/java/org/opennaas/ws/extensions/router/capability/chassis/IChassisCapabilityService.java
+++ extensions/bundles/webservice/src/main/java/org/opennaas/ws/extensions/router/capability/chassis/IChassisCapabilityService.java
@@ -1,14 +1,6 @@
 package org.opennaas.ws.extensions.router.capability.chassis;
 
-import java.util.List;
-
 import javax.jws.WebService;
-
-import org.opennaas.core.resources.capability.CapabilityException;
-import org.opennaas.extensions.router.model.ComputerSystem;
-import org.opennaas.extensions.router.model.LogicalPort;
-import org.opennaas.extensions.router.model.NetworkPort;
-import org.opennaas.extensions.router.model.ProtocolEndpoint.ProtocolIFType;
 
 /**
  * @author Jordi Puig
@@ -16,147 +8,156 @@ import org.opennaas.extensions.router.model.ProtocolEndpoint.ProtocolIFType;
 @WebService
 public interface IChassisCapabilityService {
 
-	/*
-	 * Interfaces
-	 */
+	public String testMethod();
 
-	/**
-	 * Activates given physical interface (iface) so it can receive/send traffic.
-	 * 
-	 * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
-	 * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device queue
-	 * is required for queued actions to take effect.
-	 * 
-	 * @param iface
-	 *            to activate (must be a physical one)
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void upPhysicalInterface(LogicalPort iface) throws CapabilityException;
-
-	/**
-	 * Deactivates given physical interface (iface) so it can not receive/send traffic.
-	 * 
-	 * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
-	 * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device queue
-	 * is required for queued actions to take effect.
-	 * 
-	 * @param iface
-	 *            to deactivate (must be a physical one)
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void downPhysicalInterface(LogicalPort iface) throws CapabilityException;
-
-	/**
-	 * Creates given logical interface (iface).
-	 * 
-	 * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
-	 * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device queue
-	 * is required for queued actions to take effect.
-	 * 
-	 * @param iface
-	 *            to be created
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void createSubInterface(NetworkPort iface) throws CapabilityException;
-
-	/**
-	 * Deletes given logical interface (iface).
-	 * 
-	 * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
-	 * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device queue
-	 * is required for queued actions to take effect.
-	 * 
-	 * @param iface
-	 *            to be deleted
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void deleteSubInterface(NetworkPort iface) throws CapabilityException;
-
-	/**
-	 * Configures the type of encapsulation to use in given iface.
-	 * 
-	 * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
-	 * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device queue
-	 * is required for queued actions to take effect.
-	 * 
-	 * @param iface
-	 *            to be configured
-	 * @param encapsulationType
-	 *            to use in given iface
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void setEncapsulation(LogicalPort iface, ProtocolIFType encapsulationType) throws CapabilityException;
-
-	/**
-	 * Configures the encapsulation label to use in given iface.
-	 * 
-	 * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
-	 * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device queue
-	 * is required for queued actions to take effect.
-	 * 
-	 * @param iface
-	 *            to use given label
-	 * @param encapsulationLabel
-	 *            to use in given iface
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void setEncapsulationLabel(LogicalPort iface, String encapsulationLabel) throws CapabilityException;
-
-	/*
-	 * Logical Routers
-	 */
-	/**
-	 * Creates a logical router.
-	 * 
-	 * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
-	 * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device queue
-	 * is required for queued actions to take effect.
-	 * 
-	 * @param logicalRouter
-	 *            to be created
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void createLogicalRouter(ComputerSystem logicalRouter) throws CapabilityException;
-
-	/**
-	 * Deletes given logical router.
-	 * 
-	 * @param logicalRouter
-	 *            existing logical router to delete.
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void deleteLogicalRouter(ComputerSystem logicalRouter) throws CapabilityException;
-
-	/**
-	 * Adds given interfaces to given logical router, thus giving control over them to the logical router.
-	 * 
-	 * @param logicalRouter
-	 *            that will receive the interfaces
-	 * @param interfaces
-	 *            to be added to the logical router
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void addInterfacesToLogicalRouter(ComputerSystem logicalRouter, List<LogicalPort> interfaces) throws CapabilityException;
-
-	/**
-	 * Removes given interfaces from given logical router, returning control over them to the physical router.
-	 * 
-	 * 
-	 * @param logicalRouter
-	 *            to remove the interfaces from
-	 * @param interfaces
-	 *            to be removed from the logical router
-	 * @throws CapabilityException
-	 *             if any error occurred. In that case, queue remains untouched.
-	 */
-	public void removeInterfacesFromLogicalRouter(ComputerSystem logicalRouter, List<LogicalPort> interfaces) throws CapabilityException;
+	// /*
+	// * Interfaces
+	// */
+	//
+	// /**
+	// * Activates given physical interface (iface) so it can receive/send traffic.
+	// *
+	// * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
+	// * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device
+	// queue
+	// * is required for queued actions to take effect.
+	// *
+	// * @param iface
+	// * to activate (must be a physical one)
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void upPhysicalInterface(LogicalPort iface) throws CapabilityException;
+	//
+	// /**
+	// * Deactivates given physical interface (iface) so it can not receive/send traffic.
+	// *
+	// * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
+	// * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device
+	// queue
+	// * is required for queued actions to take effect.
+	// *
+	// * @param iface
+	// * to deactivate (must be a physical one)
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void downPhysicalInterface(LogicalPort iface) throws CapabilityException;
+	//
+	// /**
+	// * Creates given logical interface (iface).
+	// *
+	// * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
+	// * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device
+	// queue
+	// * is required for queued actions to take effect.
+	// *
+	// * @param iface
+	// * to be created
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void createSubInterface(NetworkPort iface) throws CapabilityException;
+	//
+	// /**
+	// * Deletes given logical interface (iface).
+	// *
+	// * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
+	// * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device
+	// queue
+	// * is required for queued actions to take effect.
+	// *
+	// * @param iface
+	// * to be deleted
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void deleteSubInterface(NetworkPort iface) throws CapabilityException;
+	//
+	// /**
+	// * Configures the type of encapsulation to use in given iface.
+	// *
+	// * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
+	// * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device
+	// queue
+	// * is required for queued actions to take effect.
+	// *
+	// * @param iface
+	// * to be configured
+	// * @param encapsulationType
+	// * to use in given iface
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void setEncapsulation(LogicalPort iface, ProtocolIFType encapsulationType) throws CapabilityException;
+	//
+	// /**
+	// * Configures the encapsulation label to use in given iface.
+	// *
+	// * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
+	// * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device
+	// queue
+	// * is required for queued actions to take effect.
+	// *
+	// * @param iface
+	// * to use given label
+	// * @param encapsulationLabel
+	// * to use in given iface
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void setEncapsulationLabel(LogicalPort iface, String encapsulationLabel) throws CapabilityException;
+	//
+	// /*
+	// * Logical Routers
+	// */
+	// /**
+	// * Creates a logical router.
+	// *
+	// * Note: This call uses the driver to communicate with the physical device this capability belongs to, and uses actions to modify the device
+	// * state. This call end by adding required actions to the device queue, hence device state is not modified yet. An execution of this device
+	// queue
+	// * is required for queued actions to take effect.
+	// *
+	// * @param logicalRouter
+	// * to be created
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void createLogicalRouter(ComputerSystem logicalRouter) throws CapabilityException;
+	//
+	// /**
+	// * Deletes given logical router.
+	// *
+	// * @param logicalRouter
+	// * existing logical router to delete.
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void deleteLogicalRouter(ComputerSystem logicalRouter) throws CapabilityException;
+	//
+	// /**
+	// * Adds given interfaces to given logical router, thus giving control over them to the logical router.
+	// *
+	// * @param logicalRouter
+	// * that will receive the interfaces
+	// * @param interfaces
+	// * to be added to the logical router
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void addInterfacesToLogicalRouter(ComputerSystem logicalRouter, List<LogicalPort> interfaces) throws CapabilityException;
+	//
+	// /**
+	// * Removes given interfaces from given logical router, returning control over them to the physical router.
+	// *
+	// *
+	// * @param logicalRouter
+	// * to remove the interfaces from
+	// * @param interfaces
+	// * to be removed from the logical router
+	// * @throws CapabilityException
+	// * if any error occurred. In that case, queue remains untouched.
+	// */
+	// public void removeInterfacesFromLogicalRouter(ComputerSystem logicalRouter, List<LogicalPort> interfaces) throws CapabilityException;
 }
--- extensions/bundles/webservice/src/main/java/org/opennaas/ws/extensions/router/capability/chassis/impl/ChassisCapabilityServiceImpl.java
+++ extensions/bundles/webservice/src/main/java/org/opennaas/ws/extensions/router/capability/chassis/impl/ChassisCapabilityServiceImpl.java
@@ -1,14 +1,7 @@
 package org.opennaas.ws.extensions.router.capability.chassis.impl;
 
-import java.util.List;
-
 import javax.jws.WebService;
 
-import org.opennaas.core.resources.capability.CapabilityException;
-import org.opennaas.extensions.router.model.ComputerSystem;
-import org.opennaas.extensions.router.model.LogicalPort;
-import org.opennaas.extensions.router.model.NetworkPort;
-import org.opennaas.extensions.router.model.ProtocolEndpoint.ProtocolIFType;
 import org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService;
 
 /**
@@ -20,133 +13,12 @@ public class ChassisCapabilityServiceImpl implements IChassisCapabilityService {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see
-	 * 
-	 * 
-	 * org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#upPhysicalInterface(org.opennaas.extensions.router.model.LogicalPort
-	 * )
+	 * @see org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#testMethod()
 	 */
 	@Override
-	public void upPhysicalInterface(LogicalPort iface) throws CapabilityException {
+	public String testMethod() {
 		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#downPhysicalInterface(org.opennaas.extensions.router.model.
-	 * LogicalPort)
-	 */
-	@Override
-	public void downPhysicalInterface(LogicalPort iface) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * 
-	 * org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#createSubInterface(org.opennaas.extensions.router.model.NetworkPort
-	 * )
-	 */
-	@Override
-	public void createSubInterface(NetworkPort iface) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * 
-	 * org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#deleteSubInterface(org.opennaas.extensions.router.model.NetworkPort
-	 * )
-	 */
-	@Override
-	public void deleteSubInterface(NetworkPort iface) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * 
-	 * org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#setEncapsulation(org.opennaas.extensions.router.model.LogicalPort
-	 * , org.opennaas.extensions.router.model.ProtocolEndpoint.ProtocolIFType)
-	 */
-	@Override
-	public void setEncapsulation(LogicalPort iface, ProtocolIFType encapsulationType) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#setEncapsulationLabel(org.opennaas.extensions.router.model.
-	 * LogicalPort, java.lang.String)
-	 */
-	@Override
-	public void setEncapsulationLabel(LogicalPort iface, String encapsulationLabel) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#createLogicalRouter(org.opennaas.extensions.router.model.
-	 * ComputerSystem)
-	 */
-	@Override
-	public void createLogicalRouter(ComputerSystem logicalRouter) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#deleteLogicalRouter(org.opennaas.extensions.router.model.
-	 * ComputerSystem)
-	 */
-	@Override
-	public void deleteLogicalRouter(ComputerSystem logicalRouter) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#addInterfacesToLogicalRouter(org.opennaas.extensions.router.
-	 * model.ComputerSystem, java.util.List)
-	 */
-	@Override
-	public void addInterfacesToLogicalRouter(ComputerSystem logicalRouter, List<LogicalPort> interfaces) throws CapabilityException {
-		// TODO Auto-generated method stub
-
-	}
-
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see
-	 * 
-	 * org.opennaas.ws.extensions.router.capability.chassis.IChassisCapabilityService#removeInterfacesFromLogicalRouter(org.opennaas.extensions.router
-	 * .model.ComputerSystem, java.util.List)
-	 */
-	@Override
-	public void removeInterfacesFromLogicalRouter(ComputerSystem logicalRouter, List<LogicalPort> interfaces) throws CapabilityException {
-		// TODO Auto-generated method stub
-
+		return null;
 	}
 
 }
