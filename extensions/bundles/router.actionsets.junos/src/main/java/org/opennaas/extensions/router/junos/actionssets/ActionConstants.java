20102eb0a4afcf9bc04b85f43da27c5454a94a78
isartcanyameres <isart.canyameres@i2cat.net>
2012-05-02 11:31:02 +0200

    Fix error with action names.

--- extensions/bundles/router.actionsets.junos/src/main/java/org/opennaas/extensions/router/junos/actionssets/ActionConstants.java
+++ extensions/bundles/router.actionsets.junos/src/main/java/org/opennaas/extensions/router/junos/actionssets/ActionConstants.java
@@ -18,7 +18,7 @@ public class ActionConstants {
 	public static final String	REMOVE_TAGGEDETHERNET_ENCAPSULATION	= "removeTaggedEthEncapsulation";
 	public static final String	SET_VLANID							= "setVlanId";
 
-	public static final String	CONFIGURESTATUS						= "configureStatus";
+	public static final String	CONFIGURESTATUS						= "configureInterfaceStatus";
 	public static final String	SETINTERFACEDESCRIPTION				= "setInterfaceDescription";
 
 	// LogicalRouters
@@ -52,5 +52,5 @@ public class ActionConstants {
 	public static final String	OSPF_REMOVE_INTERFACE_IN_AREA		= "removeOSPFInterfaceInArea";
 
 	// Static Route actionset
-	public static final String	STATIC_ROUTE_CREATE				= "createStaticRoute";
+	public static final String	STATIC_ROUTE_CREATE					= "createStaticRoute";
 }
