31d6913493e3e960a7d8c6664669d1a7da5009db
Jordi Puig <jordi.puig@i2cat.net>
2012-05-08 20:02:12 +0200

    L2BoDCapability WebService

--- extensions/bundles/bod.capability.l2bod/src/main/java/org/opennaas/extensions/bod/capability/l2bod/RequestConnectionParameters.java
+++ extensions/bundles/bod.capability.l2bod/src/main/java/org/opennaas/extensions/bod/capability/l2bod/RequestConnectionParameters.java
@@ -1,25 +1,39 @@
 package org.opennaas.extensions.bod.capability.l2bod;
 
-import com.google.common.base.Objects;
 import org.joda.time.DateTime;
-
 import org.opennaas.extensions.network.model.topology.Interface;
+
+import com.google.common.base.Objects;
 
 /**
  * Immutable parameters class for RequestConnectionAction.
  */
 public class RequestConnectionParameters
 {
-	public final Interface interface1;
-	public final Interface interface2;
-	public final long capacity;
-	public final int vlanid;
-	public final DateTime startTime;
-	public final DateTime endTime;
+	public Interface	interface1;
+	public Interface	interface2;
+	public long			capacity;
+	public int			vlanid;
+	public DateTime		startTime;
+	public DateTime		endTime;
 
+	/**
+	 * 
+	 */
+	public RequestConnectionParameters() {
+	}
+
+	/**
+	 * @param interface1
+	 * @param interface2
+	 * @param capacity
+	 * @param vlanid
+	 * @param startTime
+	 * @param endTime
+	 */
 	public RequestConnectionParameters(Interface interface1, Interface interface2,
-									   long capacity, int vlanid,
-									   DateTime startTime, DateTime endTime)
+			long capacity, int vlanid,
+			DateTime startTime, DateTime endTime)
 	{
 		this.interface1 = interface1;
 		this.interface2 = interface2;
@@ -33,12 +47,12 @@ public class RequestConnectionParameters
 	public String toString()
 	{
 		return Objects.toStringHelper(this)
-			.add("interface1", interface1)
-			.add("interface2", interface2)
-			.add("capacity", capacity)
-			.add("vlandid", vlanid)
-			.add("startTime", startTime)
-			.add("endTime", endTime)
-			.toString();
+				.add("interface1", interface1)
+				.add("interface2", interface2)
+				.add("capacity", capacity)
+				.add("vlandid", vlanid)
+				.add("startTime", startTime)
+				.add("endTime", endTime)
+				.toString();
 	}
 }
---
+++ extensions/bundles/webservice/src/main/java/org/opennaas/extensions/ws/impl/L2BoDCapabilityServiceImpl.java
@@ -0,0 +1,57 @@
+package org.opennaas.extensions.ws.impl;
+
+import java.util.List;
+
+import javax.jws.WebService;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.opennaas.core.resources.capability.CapabilityException;
+import org.opennaas.extensions.bod.capability.l2bod.IL2BoDCapability;
+import org.opennaas.extensions.bod.capability.l2bod.L2BoDCapability;
+import org.opennaas.extensions.bod.capability.l2bod.RequestConnectionParameters;
+import org.opennaas.extensions.network.model.topology.Interface;
+import org.opennaas.extensions.ws.services.IL2BoDCapabilityService;
+
+/**
+ * @author Jordi Puig
+ */
+@WebService(portName = "L2BoDCapabilityPort", serviceName = "L2BoDCapabilityService", targetNamespace = "http:/www.opennaas.org/ws")
+public class L2BoDCapabilityServiceImpl extends GenericCapabilityServiceImpl implements IL2BoDCapabilityService {
+
+	Log	log	= LogFactory.getLog(L2BoDCapabilityServiceImpl.class);
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.opennaas.extensions.ws.services.IL2BoDCapabilityService#requestConnection(java.lang.String,
+	 * org.opennaas.extensions.bod.capability.l2bod.RequestConnectionParameters)
+	 */
+	@Override
+	public void requestConnection(String resourceId, RequestConnectionParameters parameters) throws CapabilityException {
+		try {
+			IL2BoDCapability iL2BoDCapability = (IL2BoDCapability) getCapability(resourceId, L2BoDCapability.CAPABILITY_TYPE);
+			iL2BoDCapability.requestConnection(parameters);
+		} catch (CapabilityException e) {
+			log.error(e);
+			throw e;
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.opennaas.extensions.ws.services.IL2BoDCapabilityService#shutDownConnection(java.lang.String, java.util.List)
+	 */
+	@Override
+	public void shutDownConnection(String resourceId, List<Interface> listInterfaces) throws CapabilityException {
+		try {
+			IL2BoDCapability iL2BoDCapability = (IL2BoDCapability) getCapability(resourceId, L2BoDCapability.CAPABILITY_TYPE);
+			iL2BoDCapability.shutDownConnection(listInterfaces);
+		} catch (CapabilityException e) {
+			log.error(e);
+			throw e;
+		}
+	}
+
+}
