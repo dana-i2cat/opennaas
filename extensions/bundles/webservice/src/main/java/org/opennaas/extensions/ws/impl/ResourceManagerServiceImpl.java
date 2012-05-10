b2ef9b7c6070c4aef3c2114d093e9df441cc7fa5
isartcanyameres <isart.canyameres@i2cat.net>
2012-05-10 13:03:24 +0200

    Create ws interface and implementation for ResourceManager.

--- extensions/bundles/webservice/src/main/java/org/opennaas/extensions/ws/impl/ResourceManagerServiceImpl.java
+++ extensions/bundles/webservice/src/main/java/org/opennaas/extensions/ws/impl/ResourceManagerServiceImpl.java
@@ -1,38 +1,105 @@
 package org.opennaas.extensions.ws.impl;
 
-import javax.inject.Inject;
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.jws.WebService;
 
 import org.opennaas.core.resources.IResource;
 import org.opennaas.core.resources.IResourceManager;
 import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.ResourceIdentifier;
 import org.opennaas.core.resources.descriptor.ResourceDescriptor;
 import org.opennaas.extensions.ws.services.IResourceManagerService;
 
 /**
- * @author Jordi Puig
+ * @author Isart Canyameres
  */
 @WebService
 public class ResourceManagerServiceImpl implements IResourceManagerService {
 
-	@Inject
 	private IResourceManager	resourceManager;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.opennaas.ws.core.resources.IResourceManagerService#createResource(org.opennaas.core.resources.descriptor.ResourceDescriptor)
-	 */
+	public IResourceManager getResourceManager() {
+		return resourceManager;
+	}
+
+	public void setResourceManager(IResourceManager resourceManager) {
+		this.resourceManager = resourceManager;
+	}
+
 	@Override
-	public String createResource(ResourceDescriptor resourceDescriptor) {
-		String resourceId = "";
-		IResource resource = null;
-		try {
-			resource = resourceManager.createResource(resourceDescriptor);
-			resourceId = resource != null ? resource.getResourceIdentifier().getId() : "-1";
-		} catch (ResourceException e) {
-			resourceId = "-1";
+	public ResourceIdentifier createResource(ResourceDescriptor resourceDescriptor) throws ResourceException {
+		return (ResourceIdentifier) resourceManager.createResource(resourceDescriptor).getResourceIdentifier();
+	}
+
+	@Override
+	public ResourceIdentifier modifyResource(ResourceIdentifier resourceIdentifier, ResourceDescriptor resourceDescriptor) throws ResourceException {
+		return (ResourceIdentifier) resourceManager.modifyResource(resourceIdentifier, resourceDescriptor).getResourceIdentifier();
+	}
+
+	@Override
+	public void removeResource(ResourceIdentifier resourceIdentifier) throws ResourceException {
+		resourceManager.removeResource(resourceIdentifier);
+	}
+
+	@Override
+	public List<ResourceIdentifier> listResourcesByType(String type) {
+		List<IResource> resources = resourceManager.listResourcesByType(type);
+		return getIdentifiers(resources);
+	}
+
+	@Override
+	public List<ResourceIdentifier> listResources() {
+		List<IResource> resources = resourceManager.listResources();
+		return getIdentifiers(resources);
+	}
+
+	@Override
+	public List<String> getResourceTypes() {
+		return resourceManager.getResourceTypes();
+	}
+
+	@Override
+	public ResourceDescriptor getResourceDescriptor(ResourceIdentifier resourceIdentifier) throws ResourceException {
+		return resourceManager.getResource(resourceIdentifier).getResourceDescriptor();
+	}
+
+	@Override
+	public void startResource(ResourceIdentifier resourceIdentifier) throws ResourceException {
+		resourceManager.startResource(resourceIdentifier);
+	}
+
+	@Override
+	public void stopResource(ResourceIdentifier resourceIdentifier) throws ResourceException {
+		resourceManager.stopResource(resourceIdentifier);
+	}
+
+	@Override
+	public void exportResourceDescriptor(ResourceIdentifier resourceIdentifier, String fileName) throws ResourceException {
+		resourceManager.exportResourceDescriptor(resourceIdentifier, fileName);
+	}
+
+	@Override
+	public ResourceIdentifier getIdentifierFromResourceName(String resourceType, String resourceName) throws ResourceException {
+		return (ResourceIdentifier) resourceManager.getIdentifierFromResourceName(resourceType, resourceName);
+	}
+
+	@Override
+	public String getNameFromResourceID(String ID) throws ResourceException {
+		return resourceManager.getNameFromResourceID(ID);
+	}
+
+	@Override
+	public void forceStopResource(ResourceIdentifier resourceIdentifier) throws ResourceException {
+		resourceManager.forceStopResource(resourceIdentifier);
+	}
+
+	private List<ResourceIdentifier> getIdentifiers(List<IResource> resources) {
+		List<ResourceIdentifier> ids = new ArrayList<ResourceIdentifier>(resources.size());
+		for (IResource resource : resources) {
+			ids.add((ResourceIdentifier) resource.getResourceIdentifier());
 		}
-		return resourceId;
+		return ids;
 	}
 }
