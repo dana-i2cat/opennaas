36192627852854856cf4c673c8feaa4ed9a3ad6f
Jordi Puig <jordi.puig@i2cat.net>
2012-05-14 12:42:34 +0200

    Step6 - Configure OSPF

--- web/opennaas-web/src/main/java/org/opennaas/web/actions/Step6Action.java
+++ web/opennaas-web/src/main/java/org/opennaas/web/actions/Step6Action.java
@@ -5,10 +5,13 @@ import java.util.Map;
 import org.apache.struts2.interceptor.SessionAware;
 import org.opennaas.web.utils.ResourcesDemo;
 import org.opennaas.web.ws.OpennaasClient;
+import org.opennaas.ws.ActionException_Exception;
 import org.opennaas.ws.CapabilityException_Exception;
 import org.opennaas.ws.INetOSPFCapabilityService;
 import org.opennaas.ws.INetQueueCapabilityService;
+import org.opennaas.ws.IQueueManagerCapabilityService;
 import org.opennaas.ws.IStaticRouteCapabilityService;
+import org.opennaas.ws.ProtocolException_Exception;
 import org.opennaas.ws.ResourceIdentifier;
 
 import com.opensymphony.xwork2.ActionSupport;
@@ -18,7 +21,11 @@ import com.opensymphony.xwork2.ActionSupport;
  */
 public class Step6Action extends ActionSupport implements SessionAware {
 
-	private Map<String, Object>	session;
+	private Map<String, Object>				session;
+	private IStaticRouteCapabilityService	staticRouteService;
+	private INetOSPFCapabilityService		netOSPFService;
+	private INetQueueCapabilityService		netQueueService;
+	private IQueueManagerCapabilityService	queueService;
 
 	@Override
 	public void setSession(Map<String, Object> session) {
@@ -51,28 +58,29 @@ public class Step6Action extends ActionSupport implements SessionAware {
 		return SUCCESS;
 	}
 
-	private void configureOSPF() throws CapabilityException_Exception {
-		// TODO Auto-generated method stub
-		String networkId = ((ResourceIdentifier) session.get(ResourcesDemo.NETWORK_NAME)).getId();
+	private void configureStaticRoute() throws CapabilityException_Exception, ActionException_Exception, ProtocolException_Exception {
+		staticRouteService = OpennaasClient.getStaticRouteCapabilityService();
+		queueService = OpennaasClient.getQueueManagerCapabilityService();
 
-		// acivate ospf
-		INetOSPFCapabilityService capabilityService = OpennaasClient.getNetOSPFCapabilityService();
-		capabilityService.activateOSPF(networkId);
+		String lrLolaId = ((ResourceIdentifier) session.get(ResourcesDemo.LOGICAL_LOLA_NAME)).getId();
+		String lrMyreId = ((ResourceIdentifier) session.get(ResourcesDemo.LOGICAL_MYRE_NAME)).getId();
 
-		// execute queue
-		INetQueueCapabilityService queueCapabService = OpennaasClient.getNetQueueCapabilityService();
-		queueCapabService.execute(networkId);
-	}
-
-	private void configureStaticRoute() throws CapabilityException_Exception {
-		String routerIdLogicalLola = ((ResourceIdentifier) session.get(ResourcesDemo.LOGICAL_LOLA_NAME)).getId();
-		String routerIdLogicalMyre = ((ResourceIdentifier) session.get(ResourcesDemo.LOGICAL_MYRE_NAME)).getId();
-
-		IStaticRouteCapabilityService capabilityService = OpennaasClient.getStaticRouteCapabilityService();
-		capabilityService.createStaticRoute(routerIdLogicalLola, ResourcesDemo.DEFAULT_ROUTE, ResourcesDemo.DEFAULT_ROUTE_MASK,
+		staticRouteService.createStaticRoute(lrLolaId, ResourcesDemo.DEFAULT_ROUTE, ResourcesDemo.DEFAULT_ROUTE_MASK,
 				ResourcesDemo.LOLA_STATIC_ROUTE);
-		capabilityService.createStaticRoute(routerIdLogicalMyre, ResourcesDemo.DEFAULT_ROUTE, ResourcesDemo.DEFAULT_ROUTE_MASK,
+		staticRouteService.createStaticRoute(lrMyreId, ResourcesDemo.DEFAULT_ROUTE, ResourcesDemo.DEFAULT_ROUTE_MASK,
 				ResourcesDemo.MYRE_STATIC_ROUTE);
 
+		queueService.execute(lrLolaId);
+		queueService.execute(lrMyreId);
 	}
+
+	private void configureOSPF() throws CapabilityException_Exception {
+		netOSPFService = OpennaasClient.getNetOSPFCapabilityService();
+		netQueueService = OpennaasClient.getNetQueueCapabilityService();
+
+		String networkId = ((ResourceIdentifier) session.get(ResourcesDemo.NETWORK_NAME)).getId();
+		netOSPFService.activateOSPF(networkId);
+		netQueueService.execute(networkId);
+	}
+
 }
