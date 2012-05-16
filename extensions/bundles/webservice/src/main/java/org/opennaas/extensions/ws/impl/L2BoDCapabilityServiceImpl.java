c2490ffb7d4eb7a84dfa271a8a1efe6ebea1aa9b
Eli Rigol <eliRigol@gmail.com>
2012-05-16 17:25:37 +0200

    Allow automatic merge.

--- extensions/bundles/webservice/src/main/java/org/opennaas/extensions/ws/impl/L2BoDCapabilityServiceImpl.java
+++ extensions/bundles/webservice/src/main/java/org/opennaas/extensions/ws/impl/L2BoDCapabilityServiceImpl.java
@@ -1,15 +1,24 @@
 package org.opennaas.extensions.ws.impl;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import javax.jws.WebService;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.joda.time.DateTime;
+import org.joda.time.format.ISODateTimeFormat;
+import org.opennaas.core.resources.IResource;
+import org.opennaas.core.resources.ResourceException;
 import org.opennaas.core.resources.capability.CapabilityException;
 import org.opennaas.extensions.bod.capability.l2bod.IL2BoDCapability;
 import org.opennaas.extensions.bod.capability.l2bod.RequestConnectionParameters;
+import org.opennaas.extensions.network.model.NetworkModel;
+import org.opennaas.extensions.network.model.NetworkModelHelper;
 import org.opennaas.extensions.network.model.topology.Interface;
+import org.opennaas.extensions.network.model.topology.NetworkElement;
 import org.opennaas.extensions.ws.services.IL2BoDCapabilityService;
 
 /**
@@ -23,17 +32,21 @@ public class L2BoDCapabilityServiceImpl extends GenericCapabilityService impleme
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see org.opennaas.extensions.ws.services.IL2BoDCapabilityService#requestConnection(java.lang.String,
-	 * org.opennaas.extensions.bod.capability.l2bod.RequestConnectionParameters)
+	 * @see org.opennaas.extensions.ws.services.IL2BoDCapabilityService#requestConnection(java.lang.String, java.lang.String, java.lang.String,
+	 * java.lang.String, java.lang.String, java.lang.String)
 	 */
 	@Override
-	public void requestConnection(String resourceId, RequestConnectionParameters parameters) throws CapabilityException {
+	public void requestConnection(String resourceId, String interfaceName1, String interfaceName2, String vlanid, String capacity,
+			String endTime) throws ResourceException {
 		try {
 			log.info("Start of requestConnection call");
 			IL2BoDCapability iL2BoDCapability = (IL2BoDCapability) getCapability(resourceId, IL2BoDCapability.class);
-			iL2BoDCapability.requestConnection(parameters);
+			iL2BoDCapability.requestConnection(getParameters(resourceId, interfaceName1, interfaceName2, vlanid, capacity, endTime));
 			log.info("End of requestConnection call");
 		} catch (CapabilityException e) {
+			log.error(e);
+			throw e;
+		} catch (ResourceException e) {
 			log.error(e);
 			throw e;
 		}
@@ -45,16 +58,79 @@ public class L2BoDCapabilityServiceImpl extends GenericCapabilityService impleme
 	 * @see org.opennaas.extensions.ws.services.IL2BoDCapabilityService#shutDownConnection(java.lang.String, java.util.List)
 	 */
 	@Override
-	public void shutDownConnection(String resourceId, List<Interface> listInterfaces) throws CapabilityException {
+	public void shutDownConnection(String resourceId, List<String> listInterfaces) throws ResourceException {
 		try {
 			log.info("Start of shutDownConnection call");
 			IL2BoDCapability iL2BoDCapability = (IL2BoDCapability) getCapability(resourceId, IL2BoDCapability.class);
-			iL2BoDCapability.shutDownConnection(listInterfaces);
+			iL2BoDCapability.shutDownConnection(getListInterfaces(resourceId, listInterfaces));
 			log.info("End of shutDownConnection call");
 		} catch (CapabilityException e) {
+			log.error(e);
+			throw e;
+		} catch (ResourceException e) {
 			log.error(e);
 			throw e;
 		}
 	}
 
+	/**
+	 * @param resourceId
+	 * @param listInterfaces
+	 * @return list of Interfaces
+	 * @throws ResourceException
+	 */
+	private List<Interface> getListInterfaces(String resourceId, List<String> listInterfaces) throws ResourceException {
+		List<Interface> list = new ArrayList<Interface>();
+		for (String name : listInterfaces) {
+			list.add(getInterfaceByName(resourceId, name));
+		}
+		return list;
+	}
+
+	/**
+	 * @param endTime
+	 * @param capacity
+	 * @param vlanid
+	 * @param interfaceName2
+	 * @param interfaceName1
+	 * @return
+	 * @throws ResourceException
+	 */
+	private RequestConnectionParameters getParameters(String resourceId, String interfaceName1, String interfaceName2, String vlanid,
+			String capacity, String endTime) throws ResourceException {
+		RequestConnectionParameters params = new RequestConnectionParameters();
+		params.interface1 = getInterfaceByName(resourceId, interfaceName1);
+		params.interface2 = getInterfaceByName(resourceId, interfaceName2);
+		params.vlanid = Integer.valueOf(vlanid);
+		params.capacity = Long.valueOf(capacity);
+		params.endTime = parseISO8601Date(endTime);
+		return params;
+	}
+
+	/**
+	 * @param interfaceName1
+	 * @param interfaceName1
+	 * @return
+	 * @throws ResourceException
+	 */
+	private Interface getInterfaceByName(String resourceId, String ifaceName) throws ResourceException {
+		IResource resource = getResource(resourceId);
+		NetworkModel networkModel = (NetworkModel) resource.getModel();
+		List<NetworkElement> elements = networkModel.getNetworkElements();
+		Interface i =
+				NetworkModelHelper.getInterfaceByName(elements, ifaceName);
+		if (i == null) {
+			throw new NoSuchElementException("No such interface: " + ifaceName);
+		}
+		return i;
+	}
+
+	/**
+	 * @param s
+	 * @return
+	 */
+	private DateTime parseISO8601Date(String s) {
+		DateTime time = ISODateTimeFormat.dateTimeNoMillis().parseDateTime(s);
+		return ((s == null) ? new DateTime() : time);
+	}
 }
