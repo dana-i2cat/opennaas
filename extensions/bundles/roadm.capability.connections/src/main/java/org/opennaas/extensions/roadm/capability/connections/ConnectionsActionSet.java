9aff6f1e86c8026b2cc52d19ca268a473ddb0d19
isartcanyameres <isart.canyameres@i2cat.net>
2012-06-08 11:07:16 +0200

    Make all ActionSet definitions implement IActionSetDefinition.
    This way ActionSet definitions are easyly identified.
    It will also  help redesigning how to define an ActionSet.
    
    By now IActionSetDefinition is an empty iinterface, so it's just to give implementator a common meaning.

--- extensions/bundles/roadm.capability.connections/src/main/java/org/opennaas/extensions/roadm/capability/connections/ConnectionsActionSet.java
+++ extensions/bundles/roadm.capability.connections/src/main/java/org/opennaas/extensions/roadm/capability/connections/ConnectionsActionSet.java
@@ -1,8 +1,8 @@
 package org.opennaas.extensions.roadm.capability.connections;
 
-public class ConnectionsActionSet {
+import org.opennaas.core.resources.action.IActionSetDefinition;
 
+public class ConnectionsActionSet implements IActionSetDefinition {
 	public static final java.lang.String	MAKE_CONNECTION		= "makeConnection";
-
 	public static final java.lang.String	REMOVE_CONNECTION	= "removeConnection";
 }
