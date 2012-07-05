d44ee8090e9e23a8c4fa2989f1f8d9fa1a377ff1
Jordi Puig <jordi.puig@i2cat.net>
2012-07-05 12:15:12 +0200

    StaticRoute published as Rest ws

--- extensions/bundles/router.capability.staticroute/src/main/java/org/opennaas/extensions/router/capability/staticroute/IStaticRouteCapability.java
+++ extensions/bundles/router.capability.staticroute/src/main/java/org/opennaas/extensions/router/capability/staticroute/IStaticRouteCapability.java
@@ -1,21 +1,29 @@
 package org.opennaas.extensions.router.capability.staticroute;
 
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+
 import org.opennaas.core.resources.capability.CapabilityException;
 import org.opennaas.core.resources.capability.ICapability;
 
 /**
  * @author Jordi Puig
  */
+@Path("/staticRoute")
 public interface IStaticRouteCapability extends ICapability {
-
 
 	/**
 	 * Create a static route in the router
+	 * 
 	 * @param netIdIpAdress
 	 * @param maskIpAdress
-	 * @param nextHopIpAddress   
+	 * @param nextHopIpAddress
 	 * @throws CapabilityException
 	 */
-	public void createStaticRoute(String netIdIpAdress, String maskIpAdress, String nextHopIpAddress) throws CapabilityException;
+	@GET
+	@Path("/create/{netIdIpAdress}/{maskIpAdress}/{nextHopIpAddress}")
+	public void createStaticRoute(@PathParam("netIdIpAdress") String netIdIpAdress, @PathParam("maskIpAdress") String maskIpAdress,
+			@PathParam("nextHopIpAddress") String nextHopIpAddress) throws CapabilityException;
 
 }
