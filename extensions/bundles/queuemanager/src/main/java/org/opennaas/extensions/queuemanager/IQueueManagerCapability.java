50f02bc795cd914497bdf3a506cdbe70682b24cd
isartcanyameres <isart.canyameres@i2cat.net>
2012-07-12 13:30:54 +0200

    Export QueueManager using rest and DOSGI.
    Most of the methods are failibg when called.

--- extensions/bundles/queuemanager/src/main/java/org/opennaas/extensions/queuemanager/IQueueManagerCapability.java
+++ extensions/bundles/queuemanager/src/main/java/org/opennaas/extensions/queuemanager/IQueueManagerCapability.java
@@ -2,6 +2,13 @@ package org.opennaas.extensions.queuemanager;
 
 import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
 import org.opennaas.core.resources.action.ActionException;
 import org.opennaas.core.resources.action.IAction;
 import org.opennaas.core.resources.capability.CapabilityException;
@@ -10,16 +17,35 @@ import org.opennaas.core.resources.protocol.ProtocolException;
 import org.opennaas.core.resources.queue.ModifyParams;
 import org.opennaas.core.resources.queue.QueueResponse;
 
+@Path("/")
 public interface IQueueManagerCapability extends ICapability {
 
+	@POST
+	@Path("/execute")
+	@Produces(MediaType.APPLICATION_XML)
 	public QueueResponse execute() throws ProtocolException,
 			CapabilityException, ActionException;
 
+	@POST
+	@Path("/clear")
 	public void clear();
 
+	// TODO move to an other interface:
+	// the fact that this method is not exported through ws suggest it should be placed in an other interface
 	public void queueAction(IAction action) throws CapabilityException;
 
+	@GET
+	@Path("/list")
+	@Produces(MediaType.APPLICATION_XML)
 	public List<IAction> getActions();
 
+	@POST
+	@Path("/listIds")
+	@Produces(MediaType.APPLICATION_XML)
+	public List<String> getActionsId();
+
+	@POST
+	@Path("/modify")
+	@Consumes(MediaType.APPLICATION_XML)
 	public void modify(ModifyParams modifyParams) throws CapabilityException;
 }
