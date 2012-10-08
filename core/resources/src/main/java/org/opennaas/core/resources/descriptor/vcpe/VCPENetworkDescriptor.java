f9fc985bad1c7e70fe59d984b19624ca7b94b91a
isartcanyameres <isart.canyameres@i2cat.net>
2012-10-08 15:12:20 +0200

    Test VCPENetworkDescriptor and make it persistent.
    A test has been added to check resource creation from VCPENetworkDescriptor.
    
    Persistance has been provided to VCPENetworkDescriptor,
    using annotations in the class and adding it to persistence.xml file.

--- core/resources/src/main/java/org/opennaas/core/resources/descriptor/vcpe/VCPENetworkDescriptor.java
+++ core/resources/src/main/java/org/opennaas/core/resources/descriptor/vcpe/VCPENetworkDescriptor.java
@@ -1,6 +1,7 @@
 package org.opennaas.core.resources.descriptor.vcpe;
 
 import javax.persistence.Basic;
+import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.Lob;
 import javax.xml.bind.annotation.XmlElement;
@@ -9,6 +10,7 @@ import javax.xml.bind.annotation.XmlRootElement;
 import org.opennaas.core.resources.descriptor.ResourceDescriptor;
 
 @XmlRootElement
+@Entity
 public class VCPENetworkDescriptor extends ResourceDescriptor {
 
 	@Basic(fetch = FetchType.LAZY)
