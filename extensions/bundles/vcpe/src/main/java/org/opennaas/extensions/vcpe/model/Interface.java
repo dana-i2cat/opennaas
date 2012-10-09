e008cd0a7a4b003271926f2d32e10edacce16020
isartcanyameres <isart.canyameres@i2cat.net>
2012-10-09 10:08:42 +0200

    Rename variable and add getElementByNameInTemplate method in VCPENetworkModelHelper.

--- extensions/bundles/vcpe/src/main/java/org/opennaas/extensions/vcpe/model/Interface.java
+++ extensions/bundles/vcpe/src/main/java/org/opennaas/extensions/vcpe/model/Interface.java
@@ -49,10 +49,10 @@ public class Interface extends VCPENetworkElement {
 				return false;
 		} else if (!name.equals(other.name))
 			return false;
-		if (templateName == null) {
-			if (other.templateName != null)
+		if (nameInTemplate == null) {
+			if (other.nameInTemplate != null)
 				return false;
-		} else if (!templateName.equals(other.templateName))
+		} else if (!nameInTemplate.equals(other.nameInTemplate))
 			return false;
 		if (ipAddress == null) {
 			if (other.ipAddress != null)
--- extensions/bundles/vcpe/src/main/java/org/opennaas/extensions/vcpe/model/helper/VCPENetworkModelHelper.java
+++ extensions/bundles/vcpe/src/main/java/org/opennaas/extensions/vcpe/model/helper/VCPENetworkModelHelper.java
@@ -12,6 +12,21 @@ import org.opennaas.extensions.vcpe.model.VCPETemplate;
 
 public class VCPENetworkModelHelper {
 
+	public static VCPENetworkElement getElementByNameInTemplate(VCPENetworkModel model, String nameInTemplate) {
+		return getElementByNameInTemplate(model.getElements(), nameInTemplate);
+	}
+
+	public static VCPENetworkElement getElementByNameInTemplate(List<VCPENetworkElement> elements, String nameInTemplate) {
+		if (nameInTemplate == null)
+			return null;
+
+		for (VCPENetworkElement element : elements) {
+			if (nameInTemplate.equals(element.getNameInTemplate()))
+				return element;
+		}
+		return null;
+	}
+
 	public static VCPENetworkModel generateSampleModel() {
 
 		List<VCPENetworkElement> phy = generatePhysicalSampleModel();
@@ -32,35 +47,35 @@ public class VCPENetworkModelHelper {
 
 		// vcpe1
 		Router vcpe1 = new Router();
-		vcpe1.setTemplateName(VCPETemplate.VCPE1_ROUTER);
+		vcpe1.setNameInTemplate(VCPETemplate.VCPE1_ROUTER);
 		vcpe1.setName("router:vCPE1");
 
 		Interface inter1 = new Interface();
-		inter1.setTemplateName(VCPETemplate.INTER1_INTERFACE_LOCAL);
+		inter1.setNameInTemplate(VCPETemplate.INTER1_INTERFACE_LOCAL);
 		inter1.setName("fe-0/1/2.1");
 		inter1.setVlanId(1);
 
 		Interface inter1other = new Interface();
-		inter1other.setTemplateName(VCPETemplate.INTER1_INTERFACE_AUTOBAHN);
+		inter1other.setNameInTemplate(VCPETemplate.INTER1_INTERFACE_AUTOBAHN);
 		inter1other.setName("autobahnID:000001.1");
 		inter1other.setVlanId(1);
 
 		Interface down1 = new Interface();
-		down1.setTemplateName(VCPETemplate.DOWN1_INTERFACE_LOCAL);
+		down1.setNameInTemplate(VCPETemplate.DOWN1_INTERFACE_LOCAL);
 		down1.setName("fe-0/1/2.2");
 		down1.setVlanId(2);
 
 		Interface down1other = new Interface();
-		down1other.setTemplateName(VCPETemplate.DOWN1_INTERFACE_AUTOBAHN);
+		down1other.setNameInTemplate(VCPETemplate.DOWN1_INTERFACE_AUTOBAHN);
 		down1other.setName("autobahnID:000001.2");
 		down1other.setVlanId(2);
 
 		Interface up1 = new Interface();
-		up1.setTemplateName(VCPETemplate.UP1_INTERFACE_LOCAL);
+		up1.setNameInTemplate(VCPETemplate.UP1_INTERFACE_LOCAL);
 		up1.setName("lt-0/1/2.1");
 
 		Interface up1other = new Interface();
-		up1other.setTemplateName(VCPETemplate.UP1_INTERFACE_PEER);
+		up1other.setNameInTemplate(VCPETemplate.UP1_INTERFACE_PEER);
 		up1other.setName("lt-0/1/2.1"); // in physical router
 
 		List<Interface> vcpe1Interfaces = new ArrayList<Interface>();
@@ -71,35 +86,35 @@ public class VCPENetworkModelHelper {
 
 		// vcpe2
 		Router vcpe2 = new Router();
-		vcpe2.setTemplateName(VCPETemplate.VCPE2_ROUTER);
+		vcpe2.setNameInTemplate(VCPETemplate.VCPE2_ROUTER);
 		vcpe2.setName("router:vCPE2");
 
 		Interface inter2 = new Interface();
-		inter2.setTemplateName(VCPETemplate.INTER2_INTERFACE_LOCAL);
+		inter2.setNameInTemplate(VCPETemplate.INTER2_INTERFACE_LOCAL);
 		inter2.setName("fe-0/1/2.1");
 		inter2.setVlanId(1);
 
 		Interface inter2other = new Interface();
-		inter2other.setTemplateName(VCPETemplate.INTER2_INTERFACE_AUTOBAHN);
+		inter2other.setNameInTemplate(VCPETemplate.INTER2_INTERFACE_AUTOBAHN);
 		inter2other.setName("autobahnID:000002.1");
 		inter2other.setVlanId(1);
 
 		Interface down2 = new Interface();
-		down2.setTemplateName(VCPETemplate.DOWN2_INTERFACE_LOCAL);
+		down2.setNameInTemplate(VCPETemplate.DOWN2_INTERFACE_LOCAL);
 		down2.setName("fe-0/1/2.2");
 		down2.setVlanId(2);
 
 		Interface down2other = new Interface();
-		down2other.setTemplateName(VCPETemplate.DOWN2_INTERFACE_AUTOBAHN);
+		down2other.setNameInTemplate(VCPETemplate.DOWN2_INTERFACE_AUTOBAHN);
 		down2other.setName("autobahnID:000002.2");
 		down2other.setVlanId(2);
 
 		Interface up2 = new Interface();
-		up2.setTemplateName(VCPETemplate.UP2_INTERFACE_LOCAL);
+		up2.setNameInTemplate(VCPETemplate.UP2_INTERFACE_LOCAL);
 		up2.setName("lt-0/1/2.1"); // in physical router
 
 		Interface up2other = new Interface();
-		up2other.setTemplateName(VCPETemplate.UP2_INTERFACE_PEER);
+		up2other.setNameInTemplate(VCPETemplate.UP2_INTERFACE_PEER);
 		up2other.setName("lt-0/1/2.1");
 
 		List<Interface> vcpe2Interfaces = new ArrayList<Interface>();
@@ -110,12 +125,12 @@ public class VCPENetworkModelHelper {
 
 		// client interfaces
 		Interface client1other = new Interface();
-		client1other.setTemplateName(VCPETemplate.CLIENT1_INTERFACE_AUTOBAHN);
+		client1other.setNameInTemplate(VCPETemplate.CLIENT1_INTERFACE_AUTOBAHN);
 		client1other.setName("autobahnID:000003.1");
 		client1other.setVlanId(2);
 
 		Interface client2other = new Interface();
-		client2other.setTemplateName(VCPETemplate.CLIENT2_INTERFACE_AUTOBAHN);
+		client2other.setNameInTemplate(VCPETemplate.CLIENT2_INTERFACE_AUTOBAHN);
 		client2other.setName("autobahnID:000003.2");
 		client2other.setVlanId(2);
 
@@ -123,33 +138,33 @@ public class VCPENetworkModelHelper {
 
 		// inter link
 		Link linkInter1local = new Link();
-		linkInter1local.setTemplateName(VCPETemplate.INTER1_LINK_LOCAL);
+		linkInter1local.setNameInTemplate(VCPETemplate.INTER1_LINK_LOCAL);
 		linkInter1local.setType("eth");
 		linkInter1local.setSource(inter1);
 		linkInter1local.setSink(inter1other);
 
 		Link linkInter1other = new Link();
-		linkInter1other.setTemplateName(VCPETemplate.INTER_LINK_AUTOBAHN);
+		linkInter1other.setNameInTemplate(VCPETemplate.INTER_LINK_AUTOBAHN);
 		linkInter1other.setType("autobahn");
 		linkInter1other.setId("autobahnID:request:0000001");
 		linkInter1other.setSource(inter1other);
 		linkInter1other.setSink(inter2other);
 
 		Link linkInter2local = new Link();
-		linkInter2local.setTemplateName(VCPETemplate.INTER2_LINK_LOCAL);
+		linkInter2local.setNameInTemplate(VCPETemplate.INTER2_LINK_LOCAL);
 		linkInter2local.setType("eth");
 		linkInter2local.setSource(inter2);
 		linkInter2local.setSink(inter2other);
 
 		// down1 link
 		Link linkDown1ocal = new Link();
-		linkDown1ocal.setTemplateName(VCPETemplate.DOWN1_LINK_LOCAL);
+		linkDown1ocal.setNameInTemplate(VCPETemplate.DOWN1_LINK_LOCAL);
 		linkDown1ocal.setType("eth");
 		linkDown1ocal.setSource(down1);
 		linkDown1ocal.setSink(down1other);
 
 		Link linkDown1other = new Link();
-		linkDown1other.setTemplateName(VCPETemplate.DOWN1_LINK_AUTOBAHN);
+		linkDown1other.setNameInTemplate(VCPETemplate.DOWN1_LINK_AUTOBAHN);
 		linkDown1other.setType("autobahn");
 		linkDown1other.setId("autobahnID:request:0000002");
 		linkDown1other.setSource(down1other);
@@ -157,13 +172,13 @@ public class VCPENetworkModelHelper {
 
 		// down2 link
 		Link linkDown2ocal = new Link();
-		linkDown2ocal.setTemplateName(VCPETemplate.DOWN2_LINK_LOCAL);
+		linkDown2ocal.setNameInTemplate(VCPETemplate.DOWN2_LINK_LOCAL);
 		linkDown2ocal.setType("eth");
 		linkDown2ocal.setSource(down2);
 		linkDown2ocal.setSink(down2other);
 
 		Link linkDown2other = new Link();
-		linkDown2other.setTemplateName(VCPETemplate.DOWN2_LINK_AUTOBAHN);
+		linkDown2other.setNameInTemplate(VCPETemplate.DOWN2_LINK_AUTOBAHN);
 		linkDown2other.setType("autobahn");
 		linkDown2other.setId("autobahnID:request:0000003");
 		linkDown2other.setSource(down2other);
@@ -171,21 +186,21 @@ public class VCPENetworkModelHelper {
 
 		// up1 link
 		Link linkUp1 = new Link();
-		linkUp1.setTemplateName(VCPETemplate.UP1_LINK);
+		linkUp1.setNameInTemplate(VCPETemplate.UP1_LINK);
 		linkUp1.setType("lt");
 		linkUp1.setSource(up1);
 		linkUp1.setSink(up1other);
 
 		// up2 link
 		Link linkUp2 = new Link();
-		linkUp2.setTemplateName(VCPETemplate.UP2_LINK);
+		linkUp2.setNameInTemplate(VCPETemplate.UP2_LINK);
 		linkUp2.setType("lt");
 		linkUp2.setSource(up2);
 		linkUp2.setSink(up2other);
 
 		// virtual links
 		Link inter = new Link();
-		inter.setTemplateName(VCPETemplate.INTER_LINK);
+		inter.setNameInTemplate(VCPETemplate.INTER_LINK);
 		inter.setSource(inter1);
 		inter.setSink(inter2);
 		List<Link> subLinks = new ArrayList<Link>();
@@ -195,7 +210,7 @@ public class VCPENetworkModelHelper {
 		inter.setImplementedBy(subLinks);
 
 		Link linkdown1 = new Link();
-		linkdown1.setTemplateName(VCPETemplate.DOWN1_LINK);
+		linkdown1.setNameInTemplate(VCPETemplate.DOWN1_LINK);
 		linkdown1.setSource(down1);
 		linkdown1.setSink(client1other);
 		subLinks = new ArrayList<Link>();
@@ -204,7 +219,7 @@ public class VCPENetworkModelHelper {
 		linkdown1.setImplementedBy(subLinks);
 
 		Link linkdown2 = new Link();
-		linkdown2.setTemplateName(VCPETemplate.DOWN2_LINK);
+		linkdown2.setNameInTemplate(VCPETemplate.DOWN2_LINK);
 		linkdown2.setSource(down2);
 		linkdown2.setSink(client2other);
 		subLinks = new ArrayList<Link>();
@@ -240,31 +255,31 @@ public class VCPENetworkModelHelper {
 	private static List<VCPENetworkElement> generatePhysicalSampleModel() {
 
 		Router r1 = new Router();
-		r1.setTemplateName(VCPETemplate.CPE1_PHY_ROUTER);
+		r1.setNameInTemplate(VCPETemplate.CPE1_PHY_ROUTER);
 		r1.setName("router:lola");
 
 		Interface inter1 = new Interface();
-		inter1.setTemplateName(VCPETemplate.INTER1_PHY_INTERFACE_LOCAL);
+		inter1.setNameInTemplate(VCPETemplate.INTER1_PHY_INTERFACE_LOCAL);
 		inter1.setName("fe-0/1/2");
 
 		Interface inter1other = new Interface();
-		inter1other.setTemplateName(VCPETemplate.INTER1_PHY_INTERFACE_AUTOBAHN);
+		inter1other.setNameInTemplate(VCPETemplate.INTER1_PHY_INTERFACE_AUTOBAHN);
 		inter1other.setName("autobahnID:000001");
 
 		Interface down1 = new Interface();
-		down1.setTemplateName(VCPETemplate.DOWN1_PHY_INTERFACE_LOCAL);
+		down1.setNameInTemplate(VCPETemplate.DOWN1_PHY_INTERFACE_LOCAL);
 		down1.setName("fe-0/1/2");
 
 		Interface down1other = new Interface();
-		down1other.setTemplateName(VCPETemplate.DOWN1_PHY_INTERFACE_AUTOBAHN);
+		down1other.setNameInTemplate(VCPETemplate.DOWN1_PHY_INTERFACE_AUTOBAHN);
 		down1other.setName("autobahnID:000001");
 
 		Interface up1 = new Interface();
-		up1.setTemplateName(VCPETemplate.UP1_PHY_INTERFACE_LOCAL);
+		up1.setNameInTemplate(VCPETemplate.UP1_PHY_INTERFACE_LOCAL);
 		up1.setName("lt-1/2/0");
 
 		Interface client1 = new Interface();
-		client1.setTemplateName(VCPETemplate.CLIENT1_PHY_INTERFACE_AUTOBAHN);
+		client1.setNameInTemplate(VCPETemplate.CLIENT1_PHY_INTERFACE_AUTOBAHN);
 		client1.setName("autobahnID:000003");
 
 		List<Interface> r1Interfaces = new ArrayList<Interface>();
@@ -274,31 +289,31 @@ public class VCPENetworkModelHelper {
 		r1.setInterfaces(r1Interfaces);
 
 		Router r2 = new Router();
-		r2.setTemplateName(VCPETemplate.CPE2_PHY_ROUTER);
+		r2.setNameInTemplate(VCPETemplate.CPE2_PHY_ROUTER);
 		r2.setName("router:myre");
 
 		Interface inter2 = new Interface();
-		inter2.setTemplateName(VCPETemplate.INTER2_PHY_INTERFACE_LOCAL);
+		inter2.setNameInTemplate(VCPETemplate.INTER2_PHY_INTERFACE_LOCAL);
 		inter2.setName("fe-0/1/2");
 
 		Interface inter2other = new Interface();
-		inter2other.setTemplateName(VCPETemplate.INTER2_PHY_INTERFACE_AUTOBAHN);
+		inter2other.setNameInTemplate(VCPETemplate.INTER2_PHY_INTERFACE_AUTOBAHN);
 		inter2other.setName("autobahnID:000002");
 
 		Interface down2 = new Interface();
-		down2.setTemplateName(VCPETemplate.DOWN2_PHY_INTERFACE_LOCAL);
+		down2.setNameInTemplate(VCPETemplate.DOWN2_PHY_INTERFACE_LOCAL);
 		down2.setName("fe-0/1/2");
 
 		Interface down2other = new Interface();
-		down2other.setTemplateName(VCPETemplate.DOWN2_PHY_INTERFACE_AUTOBAHN);
+		down2other.setNameInTemplate(VCPETemplate.DOWN2_PHY_INTERFACE_AUTOBAHN);
 		down2other.setName("autobahnID:000002");
 
 		Interface up2 = new Interface();
-		up2.setTemplateName(VCPETemplate.UP2_PHY_INTERFACE_LOCAL);
+		up2.setNameInTemplate(VCPETemplate.UP2_PHY_INTERFACE_LOCAL);
 		up2.setName("lt-1/2/0");
 
 		Interface client2 = new Interface();
-		client2.setTemplateName(VCPETemplate.CLIENT2_PHY_INTERFACE_AUTOBAHN);
+		client2.setNameInTemplate(VCPETemplate.CLIENT2_PHY_INTERFACE_AUTOBAHN);
 		client2.setName("autobahnID:000003");
 
 		List<Interface> r2Interfaces = new ArrayList<Interface>();
