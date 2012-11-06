656e4d52541f45db96ac576427feabd8c87b7d3a
Jordi Puig <jordi.puig@i2cat.net>
2012-11-06 12:08:36 +0100

    Some methods and classes renamed in MacBridge resource. Too large.

--- extensions/bundles/macbridge.ios.resource/src/main/java/org/opennaas/extensions/macbridge/ios/resource/actionssets/actions/vlanawarebridge/AddStaticVLANRegistrationAction.java
+++ extensions/bundles/macbridge.ios.resource/src/main/java/org/opennaas/extensions/macbridge/ios/resource/actionssets/actions/vlanawarebridge/AddStaticVLANRegistrationAction.java
@@ -1,5 +1,11 @@
 package org.opennaas.extensions.macbridge.ios.resource.actionssets.actions.vlanawarebridge;
 
+import org.opennaas.core.resources.action.Action;
+import org.opennaas.core.resources.action.ActionException;
+import org.opennaas.core.resources.action.ActionResponse;
+import org.opennaas.core.resources.protocol.IProtocolSession;
+import org.opennaas.core.resources.protocol.IProtocolSessionManager;
+import org.opennaas.core.resources.protocol.ProtocolException;
 import org.opennaas.extensions.capability.macbridge.model.MACBridge;
 import org.opennaas.extensions.capability.macbridge.model.PortConfiguration;
 import org.opennaas.extensions.capability.macbridge.model.StaticVLANRegistrationEntry;
@@ -16,32 +22,25 @@ import org.opennaas.extensions.macbridge.ios.resource.commandsets.commands.Switc
 import org.opennaas.extensions.macbridge.ios.resource.commandsets.commands.SwitchportModeAccessCommand;
 import org.opennaas.extensions.macbridge.ios.resource.commandsets.commands.SwitchportTrunkAllowedVLANAddCommand;
 
-import org.opennaas.core.resources.action.Action;
-import org.opennaas.core.resources.action.ActionException;
-import org.opennaas.core.resources.action.ActionResponse;
-import org.opennaas.core.resources.protocol.IProtocolSession;
-import org.opennaas.core.resources.protocol.IProtocolSessionManager;
-import org.opennaas.core.resources.protocol.ProtocolException;
-
 /**
  * @author Eduard Grasa
  */
-public class AddStaticVLANRegistrationEntryToFilteringDatabaseAction extends Action {
+public class AddStaticVLANRegistrationAction extends Action {
 
 	/**
 	 * 
 	 */
-	public AddStaticVLANRegistrationEntryToFilteringDatabaseAction() {
+	public AddStaticVLANRegistrationAction() {
 		super();
-		this.setActionID(VLANAwareBridgeActionSet.ADD_STATIC_VLAN_REGISTRATION_ENTRY_TO_FILTERING_DATABASE);
+		this.setActionID(VLANAwareBridgeActionSet.ADD_STATIC_VLAN_REGISTRATION);
 	}
 
 	@Override
 	public boolean checkParams(Object params) throws ActionException {
-		if (params == null || ! (params instanceof StaticVLANRegistrationEntry)){
+		if (params == null || !(params instanceof StaticVLANRegistrationEntry)) {
 			return false;
 		}
-		
+
 		return true;
 	}
 
@@ -49,29 +48,29 @@ public class AddStaticVLANRegistrationEntryToFilteringDatabaseAction extends Act
 	public ActionResponse execute(IProtocolSessionManager protocolSessionManager) throws ActionException {
 		IOSCommand command = null;
 		StaticVLANRegistrationEntry entry = (StaticVLANRegistrationEntry) this.getParams();
-		
-		try{
+
+		try {
 			IProtocolSession protocol = protocolSessionManager.obtainSessionByProtocol("cli", false);
 			protocol.connect();
-			
-			//Login and enter enable mode
+
+			// Login and enter enable mode
 			command = new EnableCommand();
 			protocol.sendReceive(command.getCommand());
-			command = new EnablePasswordCommand((String)protocol.getSessionContext().getSessionParameters().get("protocol.enablepassword"));
+			command = new EnablePasswordCommand((String) protocol.getSessionContext().getSessionParameters().get("protocol.enablepassword"));
 			protocol.sendReceive(command.getCommand());
-			
-			//Add the VLANs to the ports
+
+			// Add the VLANs to the ports
 			command = new ConfigureTerminalCommand();
 			protocol.sendReceive(command.getCommand());
 			PortConfiguration portConfiguration = null;
-			for(int i=0; i<entry.getPortConfigurations().size(); i++){
+			for (int i = 0; i < entry.getPortConfigurations().size(); i++) {
 				portConfiguration = entry.getPortConfigurations().get(i);
 				command = new InterfaceCommand(portConfiguration.getPortInterfaceId());
 				protocol.sendReceive(command.getCommand());
-				if (portConfiguration.isTagged()){
+				if (portConfiguration.isTagged()) {
 					command = new SwitchportTrunkAllowedVLANAddCommand(entry.getVlanID());
 					protocol.sendReceive(command.getCommand());
-				}else{
+				} else {
 					command = new SwitchportCommand(true);
 					protocol.sendReceive(command.getCommand());
 					command = new SwitchportAccessVLANCommand(entry.getVlanID(), false);
@@ -86,15 +85,15 @@ public class AddStaticVLANRegistrationEntryToFilteringDatabaseAction extends Act
 			}
 			command = new ExitCommand();
 			protocol.sendReceive(command.getCommand());
-			
+
 			protocol.disconnect();
-		}catch(ProtocolException ex){
+		} catch (ProtocolException ex) {
 			throw new ActionException(ex);
 		}
-		
+
 		MACBridge macBridgeModel = (MACBridge) this.getModelToUpdate();
 		macBridgeModel.getFilteringDatabase().getStaticVLANRegistrations().put(new Integer(entry.getVlanID()), entry);
-		
+
 		return ActionResponse.okResponse(this.getActionID());
 	}
 
