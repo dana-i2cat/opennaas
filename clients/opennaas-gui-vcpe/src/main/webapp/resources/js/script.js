724736b8aebc15feb5360b91524752b448b04895
Manel Jara <manel.jara@i2cat.net>
2012-11-23 12:02:49 +0100

    Added progress bar window with animated gif

--- clients/opennaas-gui-vcpe/src/main/webapp/resources/js/script.js
+++ clients/opennaas-gui-vcpe/src/main/webapp/resources/js/script.js
@@ -7,10 +7,10 @@
  */
 function updateHeader() {
 	$.ajax({
-		type: "GET",
-		url: "/opennaas-vcpe/secure/vcpeNetwork/getAjax",
-		success: function(data) {
-		    $('#ajaxUpdate').html(data);			    
+		type : "GET",
+		url : "/opennaas-vcpe/secure/vcpeNetwork/getAjax",
+		success : function(data) {
+			$('#ajaxUpdate').html(data);
 		}
 	});
 }
@@ -23,10 +23,11 @@ function updateHeader() {
  */
 function isVLANFree(vcpeId, vlan) {
 	$.ajax({
-		type: "GET",
-		url: "/opennaas-vcpe/secure/vcpeNetwork/isVLANFree?vcpeId=" + vcpeId + "&vlan=" +  vlan,
-		success: function(data) {
-		    $('#ajaxUpdate').html(data);			    
+		type : "GET",
+		url : "/opennaas-vcpe/secure/vcpeNetwork/isVLANFree?vcpeId=" + vcpeId
+				+ "&vlan=" + vlan,
+		success : function(data) {
+			$('#ajaxUpdate').html(data);
 		}
 	});
 }
@@ -39,10 +40,11 @@ function isVLANFree(vcpeId, vlan) {
  */
 function isIPFree(vcpeId, ip) {
 	$.ajax({
-		type: "GET",
-		url: "/opennaas-vcpe/secure/vcpeNetwork/isIPFree?vcpeId=" + vcpeId + "&ip=" + ip,
-		success: function(data) {
-		    $('#ajaxUpdate').html(data);			    
+		type : "GET",
+		url : "/opennaas-vcpe/secure/vcpeNetwork/isIPFree?vcpeId=" + vcpeId
+				+ "&ip=" + ip,
+		success : function(data) {
+			$('#ajaxUpdate').html(data);
 		}
 	});
 }
@@ -56,10 +58,11 @@ function isIPFree(vcpeId, ip) {
  */
 function isInterfaceFree(vcpeId, iface, port) {
 	$.ajax({
-		type: "GET",
-		url: "/opennaas-vcpe/secure/vcpeNetwork/isInterfaceFree?vcpeId=" + vcpeId + "&iface=" + iface +"&port=" + port,
-		success: function(data) {
-		    $('#ajaxUpdate').html(data);			    
+		type : "GET",
+		url : "/opennaas-vcpe/secure/vcpeNetwork/isInterfaceFree?vcpeId="
+				+ vcpeId + "&iface=" + iface + "&port=" + port,
+		success : function(data) {
+			$('#ajaxUpdate').html(data);
 		}
 	});
 }
@@ -70,7 +73,7 @@ function isInterfaceFree(vcpeId, iface, port) {
  */
 $(function() {
 	// only apply accordion styles when createVCPENetwork.jsp is loaded
-	if($("#createVCPENetwork").length) {
+	if ($("#createVCPENetwork").length) {
 		jsPlumbNecessary = true;
 		/* vCPE client block */
 		$("#vcpe").accordion({
@@ -87,14 +90,14 @@ $(function() {
 				setJSPlumbStuff(vCPEvisible);
 			}
 		});
-		
+
 		/* BoD block */
 		$("#bod").accordion({
 			collapsible : true,
 			icons : false,
 			autoHeight : true,
 			heightStyle : "content",
-			active: false,
+			active : false,
 			beforeActivate : function() {
 				clearJSPlumbStuff();
 			},
@@ -102,8 +105,7 @@ $(function() {
 				setJSPlumbStuff(vCPEvisible);
 			}
 		});
-		
-		
+
 		/* Client block */
 		$("#client").accordion({
 			collapsible : true,
@@ -116,61 +118,88 @@ $(function() {
 				setJSPlumbStuff(vCPEvisible);
 			}
 		});
-		
-		
-		/* Protocols */
-		$( "#bgp" ).accordion({
-			collapsible: true,
-			icons: false,
-			heightStyle : "content",
-			active: false,
-			beforeActivate : function() {
-				clearJSPlumbStuff();
-			},
-			activate : function() {
-				setJSPlumbStuff(vCPEvisible);
-			}
-			});
-		$( "#vrrp" ).accordion({
-			collapsible: true,
-			icons: false,
-			heightStyle : "content",
-			active: false,
-			beforeActivate : function() {
-				clearJSPlumbStuff();
-			},
-			activate : function() {
-				setJSPlumbStuff(vCPEvisible);
-			}
-			});
-		
-		/* Routers */
-		$( "#lr_master" ).accordion({
-			collapsible: false,
-			icons: false,
-			heightStyle: "content"
-			});
-		$( "#lr_backup" ).accordion({
-			collapsible: false,
-			icons: false,
-			heightStyle: "content"
-			});
-	}
-	
-	/* Buttons */
-	$( "#button" ).button();
-	$( "#button2" ).button();
-	$( "#button3" ).button();
-	$( "#button4" ).button();
-	$( "#button5" ).button();
-	$( "#button6" ).button();
-	$( "#button7" ).button();
-	$( "#button8" ).button();
-	$( "#button9" ).button();
-	$( "#button10" ).button();
-	$( "#button11" ).button();
-	$( "#submitButton" ).button();
 
+		/* Protocols */
+		$("#bgp").accordion({
+			collapsible : true,
+			icons : false,
+			heightStyle : "content",
+			active : false,
+			beforeActivate : function() {
+				clearJSPlumbStuff();
+			},
+			activate : function() {
+				setJSPlumbStuff(vCPEvisible);
+			}
+		});
+		$("#vrrp").accordion({
+			collapsible : true,
+			icons : false,
+			heightStyle : "content",
+			active : false,
+			beforeActivate : function() {
+				clearJSPlumbStuff();
+			},
+			activate : function() {
+				setJSPlumbStuff(vCPEvisible);
+			}
+		});
+
+		/* Routers */
+		$("#lr_master").accordion({
+			collapsible : false,
+			icons : false,
+			heightStyle : "content"
+		});
+		$("#lr_backup").accordion({
+			collapsible : false,
+			icons : false,
+			heightStyle : "content"
+		});
+	}
+
+	/* Buttons */
+	$("#button").button();
+	$("#button2").button();
+	$("#button3").button();
+	$("#button4").button();
+	$("#button5").button();
+	$("#button6").button();
+	$("#button7").button();
+	$("#button8").button();
+	$("#button9").button();
+	$("#button10").button();
+	$("#button11").button();
+	$("#submitButton").button();
+
+	$("#waitingButton").button({
+		icons : {
+			primary : 'ui-icon-newwin'
+		}
+	});
+
+	/**
+	 * Waiting dialog
+	 */
+	// Link to open the dialog
+	$("#submitButton").click(function(event) {
+		$("#pleaseWait").dialog("open");
+	});
+
+	$("#pleaseWait").dialog({
+		modal : true,
+		autoOpen : false,
+		width : 400,
+		resizable : false,
+		draggable : false,
+		closeOnEscape : false,
+		open : function() {
+			$('.ui-widget-overlay').hide().fadeIn();
+			$(".ui-dialog-titlebar-close", $(this).parent()).hide();
+		},
+		show : "fade",
+		hide : "fade"
+	});
 });
 
 /**
@@ -181,25 +210,34 @@ function setJSPlumbStuff(setExtra) {
 	// WAN master & backup -- lola & myre
 	addConnection("up_master", "lr_master", "acc_body", 0.5, 1, 0.5, 0, false);
 	addConnection("up_backup", "lr_backup", "acc_body", 0.5, 1, 0.5, 0, false);
-	
+
 	// lola & myre -- down & inter, master & backup
-	addConnection("lr_master", "client_master", "acc_body", 0.25, 1, 0.7, 0, false);
-	addConnection("lr_master", "inter_master", "acc_body", 0.75, 1, 0.5, 0, false);
-	addConnection("lr_backup", "inter_backup", "acc_body", 0.275, 1, 0.5, 0, false);
-	addConnection("lr_backup", "client_backup", "acc_body", 0.71, 1, 0.2, 0, false);
-	
+	addConnection("lr_master", "client_master", "acc_body", 0.25, 1, 0.7, 0,
+			false);
+	addConnection("lr_master", "inter_master", "acc_body", 0.75, 1, 0.5, 0,
+			false);
+	addConnection("lr_backup", "inter_backup", "acc_body", 0.275, 1, 0.5, 0,
+			false);
+	addConnection("lr_backup", "client_backup", "acc_body", 0.71, 1, 0.2, 0,
+			false);
+
 	// inter master -- inter backup
-	addConnection("inter_master", "inter_backup", "acc_body", 1, 0.5, 0, 0.5, false);
-	
+	addConnection("inter_master", "inter_backup", "acc_body", 1, 0.5, 0, 0.5,
+			false);
+
 	// client master & client backup -- client down master & client down backup
-	if(setExtra) {
-		addConnection("client_master", "client_down_master", "body", 0.5, 1, 0.5, 0, true);
-		addConnection("client_backup", "client_down_backup", "body", 0.5, 1, 0.5, 0, true);
+	if (setExtra) {
+		addConnection("client_master", "client_down_master", "body", 0.5, 1,
+				0.5, 0, true);
+		addConnection("client_backup", "client_down_backup", "body", 0.5, 1,
+				0.5, 0, true);
 	}
-	
+
 	// client down master & client down backup -- client
-	addConnection("client_down_master", "client", "body", 0.5, 1, 0.16, 0, false);
-	addConnection("client_down_backup", "client", "body", 0.5, 1, 0.845, 0, false);	
+	addConnection("client_down_master", "client", "body", 0.5, 1, 0.16, 0,
+			false);
+	addConnection("client_down_backup", "client", "body", 0.5, 1, 0.845, 0,
+			false);
 }
 
 // add a connection and its endpoints
@@ -213,7 +251,7 @@ function addConnection(origin, destination, parent, originAnchorX,
 		anchor : [ destinationAnchorX, destinationAnchorY ],
 		container : parent
 	});
-	if(extra) {
+	if (extra) {
 		extra_endpoints.push(originEndpoint);
 		extra_endpoints.push(destinationEndpoint);
 	} else {
@@ -227,7 +265,7 @@ function addConnection(origin, destination, parent, originAnchorX,
 		container : $("#" + parent)
 	});
 
-	if(extra) {
+	if (extra) {
 		extra_connections.push(connection);
 	} else {
 		intra_connections.push(connection);
@@ -247,7 +285,7 @@ function clearJSPlumbStuff() {
 			connection.removeAllOverlays();
 		}
 	}
-	
+
 	if (extra_connections != null) {
 		var connection = null;
 		while ((connection = extra_connections.pop()) != null) {
@@ -262,7 +300,7 @@ function clearJSPlumbStuff() {
 			endpoint.detachAll();
 		}
 	}
-	
+
 	if (extra_endpoints != null) {
 		var endpoint = null;
 		while ((endpoint = extra_endpoints.pop()) != null) {
@@ -276,12 +314,12 @@ var jsPlumbNecessary = false;
 
 // jsPlumb instance
 var jsP;
-//jsPlumb intra accordion endpoints
+// jsPlumb intra accordion endpoints
 var intra_endpoints = new Array();
 // jsPlumb intra accordion connections
 var intra_connections = new Array();
 
-//jsPlumb extra accordion endpoints
+// jsPlumb extra accordion endpoints
 var extra_endpoints = new Array();
 // jsPlumb extra accordion connections
 var extra_connections = new Array();
@@ -289,8 +327,11 @@ var extra_connections = new Array();
 // state of vCPE accordion
 var vCPEvisible = true;
 
+/**
+ * jQuery onLoad
+ */
 $(function() {
-	if(jsPlumbNecessary){
+	if (jsPlumbNecessary) {
 		// initialize jsPlumb instance
 		jsP = jsPlumb.getInstance({
 			PaintStyle : {
@@ -302,13 +343,13 @@ $(function() {
 			Connector : "Straight",
 			Endpoint : "Blank"
 		});
-	
+
 		// initialize endpoints and connections arrays
 		intra_endpoints = new Array();
 		intra_connections = new Array();
 		extra_endpoints = new Array();
 		extra_connections = new Array();
-	
+
 		// draw jsPlumb stuff when view stuff is ready
 		jsPlumb.ready(function() {
 			jsPlumb.importDefaults({
