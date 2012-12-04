b5c8957b9a80e0bc7ae1f7008ce3798b7ac35338
isartcanyameres <isart.canyameres@i2cat.net>
2012-12-04 17:14:10 +0100

    Implement equals in all vcpe model (some were missing), and make the model marsallable to xml.

--- extensions/bundles/vcpe/src/main/java/org/opennaas/extensions/vcpe/model/VRRP.java
+++ extensions/bundles/vcpe/src/main/java/org/opennaas/extensions/vcpe/model/VRRP.java
@@ -3,18 +3,27 @@
  */
 package org.opennaas.extensions.vcpe.model;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlIDREF;
+
 /**
  * @author Jordi
  */
+@XmlAccessorType(XmlAccessType.FIELD)
 public class VRRP {
 
 	private String		virtualIPAddress;
 	private Integer		group;
 	private Integer		priorityMaster;
 	private Integer		priorityBackup;
+	@XmlIDREF
 	private Router		masterRouter;
+	@XmlIDREF
 	private Interface	masterInterface;
+	@XmlIDREF
 	private Router		backupRouter;
+	@XmlIDREF
 	private Interface	backupInterface;
 
 	/**
@@ -153,6 +162,73 @@ public class VRRP {
 	@Override
 	public String toString() {
 		return "VRRP [virtualIPAddress=" + virtualIPAddress + ", group=" + group + ", priorityMaster=" + priorityMaster + ", priorityBackup=" + priorityBackup + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((backupInterface == null) ? 0 : backupInterface.hashCode());
+		result = prime * result + ((backupRouter == null) ? 0 : backupRouter.hashCode());
+		result = prime * result + ((group == null) ? 0 : group.hashCode());
+		result = prime * result + ((masterInterface == null) ? 0 : masterInterface.hashCode());
+		result = prime * result + ((masterRouter == null) ? 0 : masterRouter.hashCode());
+		result = prime * result + ((priorityBackup == null) ? 0 : priorityBackup.hashCode());
+		result = prime * result + ((priorityMaster == null) ? 0 : priorityMaster.hashCode());
+		result = prime * result + ((virtualIPAddress == null) ? 0 : virtualIPAddress.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		VRRP other = (VRRP) obj;
+		if (backupInterface == null) {
+			if (other.backupInterface != null)
+				return false;
+		} else if (!backupInterface.equals(other.backupInterface))
+			return false;
+		if (backupRouter == null) {
+			if (other.backupRouter != null)
+				return false;
+		} else if (!backupRouter.equals(other.backupRouter))
+			return false;
+		if (group == null) {
+			if (other.group != null)
+				return false;
+		} else if (!group.equals(other.group))
+			return false;
+		if (masterInterface == null) {
+			if (other.masterInterface != null)
+				return false;
+		} else if (!masterInterface.equals(other.masterInterface))
+			return false;
+		if (masterRouter == null) {
+			if (other.masterRouter != null)
+				return false;
+		} else if (!masterRouter.equals(other.masterRouter))
+			return false;
+		if (priorityBackup == null) {
+			if (other.priorityBackup != null)
+				return false;
+		} else if (!priorityBackup.equals(other.priorityBackup))
+			return false;
+		if (priorityMaster == null) {
+			if (other.priorityMaster != null)
+				return false;
+		} else if (!priorityMaster.equals(other.priorityMaster))
+			return false;
+		if (virtualIPAddress == null) {
+			if (other.virtualIPAddress != null)
+				return false;
+		} else if (!virtualIPAddress.equals(other.virtualIPAddress))
+			return false;
+		return true;
 	}
 
 }
