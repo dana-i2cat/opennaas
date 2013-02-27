776f6abf9597a253b2936f4550c677225a82e02e
isartcanyameres <isart.canyameres@i2cat.net>
2013-02-27 11:45:08 +0100

    Created MPTemplateDefaultValuesLoader
    Created MPTemplateDefaultValuesLoader which reads default values from a configuration file and populates model with them.

--- extensions/bundles/vcpe/src/test/java/org/opennaas/extensions/vcpe/test/mp/MPModelTest.java
+++ extensions/bundles/vcpe/src/test/java/org/opennaas/extensions/vcpe/test/mp/MPModelTest.java
@@ -1,5 +1,8 @@
 package org.opennaas.extensions.vcpe.test.mp;
 
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.xml.bind.JAXBException;
 
 import junit.framework.Assert;
@@ -7,10 +10,13 @@ import junit.framework.Assert;
 import org.junit.Test;
 import org.opennaas.core.resources.ObjectSerializer;
 import org.opennaas.core.resources.SerializationException;
+import org.opennaas.extensions.vcpe.manager.templates.mp.MPTemplateDefaultValuesLoader;
 import org.opennaas.extensions.vcpe.manager.templates.mp.MPTemplateModelBuilder;
 import org.opennaas.extensions.vcpe.model.VCPENetworkModel;
 
 public class MPModelTest {
+
+	private static final String	SUGGESTOR_CONFIG_PATH	= "/org.opennaas.extensions.vcpe.manager.templates.mp.suggestor.defaults.cfg";
 
 	@Test
 	public void marshalUnmarsalTest() throws JAXBException, SerializationException {
@@ -24,6 +30,32 @@ public class MPModelTest {
 
 		Assert.assertEquals(model, loaded);
 		Assert.assertEquals(xml, xml2);
+	}
+
+	@Test
+	public void physicalSuggestionDefaults() throws IOException {
+
+		Properties props = new Properties();
+		props.load(this.getClass().getResourceAsStream(SUGGESTOR_CONFIG_PATH));
+
+		MPTemplateDefaultValuesLoader loader = new MPTemplateDefaultValuesLoader();
+		loader.initialize(props);
+
+		VCPENetworkModel suggestion = loader.loadDefaultPhysicalModel();
+
+	}
+
+	@Test
+	public void logicalSuggestionDefaults() throws IOException {
+
+		Properties props = new Properties();
+		props.load(this.getClass().getResourceAsStream(SUGGESTOR_CONFIG_PATH));
+
+		MPTemplateDefaultValuesLoader loader = new MPTemplateDefaultValuesLoader();
+		loader.initialize(props);
+
+		VCPENetworkModel suggestion = loader.loadDefaultLogicalModel();
+
 	}
 
 }
