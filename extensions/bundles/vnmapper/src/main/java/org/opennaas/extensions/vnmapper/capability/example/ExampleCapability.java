c36cf4fe65c68f9aeccdacc94aacb29bb1b6ce1b
Adrian Rosello <adrian_rosello@i2cat.net>
2013-04-03 17:35:22 +0200

    Capability and command refactor:
     - Capability accepts the VNTRequest instead of the file url and the InPNetwork.
    
     - InPNetwork is built in capability, not in command.

--- extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/capability/example/ExampleCapability.java
+++ extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/capability/example/ExampleCapability.java
@@ -1,16 +1,24 @@
 package org.opennaas.extensions.vnmapper.capability.example;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opennaas.core.resources.ActivatorException;
+import org.opennaas.core.resources.IModel;
+import org.opennaas.core.resources.IResource;
+import org.opennaas.core.resources.ResourceManager;
 import org.opennaas.core.resources.action.IAction;
 import org.opennaas.core.resources.action.IActionSet;
 import org.opennaas.core.resources.capability.AbstractCapability;
 import org.opennaas.core.resources.capability.CapabilityException;
 import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
 import org.opennaas.core.resources.descriptor.ResourceDescriptorConstants;
+import org.opennaas.extensions.network.model.NetworkModel;
+import org.opennaas.extensions.network.model.NetworkModelHelper;
+import org.opennaas.extensions.network.model.topology.Device;
+import org.opennaas.extensions.network.model.topology.Link;
 import org.opennaas.extensions.queuemanager.IQueueManagerCapability;
 
 /**
@@ -70,14 +78,7 @@ public class ExampleCapability extends AbstractCapability implements IExampleCap
 		}
 	}
 
-	/**
-	 * @param the
-	 *            user name
-	 * @return the greeting message
-	 * 
-	 */
-	@Override
-	public String sayHello(String VNRequest, InPNetwork net) throws CapabilityException {
+	public void sayHello(VNTRequest request) throws CapabilityException {
 
 		// //// run the matching and mapping/////
 		// Global.rowNum=8;
@@ -92,21 +93,21 @@ public class ExampleCapability extends AbstractCapability implements IExampleCap
 		// net=net.readPNetworkFromXMLFile("src\\marketplace\\network.xml");
 		// net.printNetwork();
 		// //
-		VNTRequest vnt = new VNTRequest();
-		vnt = vnt.readVNTRequestFromXMLFile(VNRequest);
-		vnt.printVNTNetwork();
+		InPNetwork net = getInPNetwork();
+
+		request.printVNTNetwork();
 
 		try {
 
 			VNTMapper mapper = new VNTMapper();
 			MappingResult mres = new MappingResult();
 			ArrayList<ArrayList> matchingResult = new ArrayList<ArrayList>();
-			int matchingRes = mapper.matchVirtualNetwork(vnt, net, matchingResult);
+			int matchingRes = mapper.matchVirtualNetwork(request, net, matchingResult);
 			if (matchingRes == 1)
 			{
 				System.out.println("successful Matching");
 				InPNetwork temp = (InPNetwork) ObjectCopier.deepCopy(net);
-				int result = mapper.VNTMapping(vnt, temp, 0, matchingResult, 3, mres);
+				int result = mapper.VNTMapping(request, temp, 0, matchingResult, 3, mres);
 				if (result == 1)
 				{
 					System.out.println("successful Mapping");
@@ -118,7 +119,94 @@ public class ExampleCapability extends AbstractCapability implements IExampleCap
 			System.out.println("Exception");
 		}
 
-		return "Hello " + VNRequest;
 	}
 
+	private InPNetwork getInPNetwork() throws CapabilityException {
+
+		try {
+			ResourceManager resourcemng = (ResourceManager) Activator.getResourceManagerService();
+			List<IResource> res1 = resourcemng.listResourcesByType("network");
+			IModel networkModel = res1.get(0).getModel();
+			InPNetwork net = getInPNetworkFromNetworkTopology(networkModel);
+			// System.out.println(" network links num : " + links.size());
+			// for(int i=0;i<links.size();i++)
+			// {
+			// System.out.println(" link : " + links.get(i).getSource().getName());
+			// System.out.println(" link : " + links.get(i).getSource().getDevice().getName());
+			//
+			// }
+
+			// System.out.println(" devices num : " + devices.size());
+			// for(int i=0;i<devices.size();i++)
+			// {
+			// System.out.println(" device : " + devices.get(i).getName());
+			//
+			// }
+
+			// /// generate the network object
+
+			net.printNetwork();
+			return net;
+		} catch (ActivatorException ae) {
+			throw new CapabilityException(ae);
+		}
+
+	}
+
+	private InPNetwork getInPNetworkFromNetworkTopology(IModel networkModel) {
+
+		List<Link> links = NetworkModelHelper.getLinks((NetworkModel) networkModel);
+		List<Device> devices = NetworkModelHelper.getDevices((NetworkModel) networkModel);
+
+		InPNetwork net = new InPNetwork();
+
+		for (int i = 0; i < devices.size(); i++) {
+			PNode n = new PNode();
+			n.id = i;
+			n.pnodeID = devices.get(i).getName();
+			n.capacity = 16;
+			n.pathNum = 0;
+			net.nodes.add(n);
+			net.nodeNum++;
+		}
+
+		for (int i = 0; i < net.nodeNum; i++) {
+			net.connections.add(new ArrayList<PLink>());
+			for (int j = 0; j < net.nodeNum; j++) {
+				net.connections.get(i).add(new PLink());
+			}
+		}
+
+		for (int i = 0; i < links.size(); i++)
+		{
+
+			int node1 = -1;
+
+			int node2 = -1;
+			for (int j = 0; j < devices.size() && (node1 == -1 || node2 == -1); j++) {
+				if (devices.get(j).getName().equals(links.get(i).getSource().getDevice().getName()))
+					node1 = j;
+				if (devices.get(j).getName().equals(links.get(i).getSink().getDevice().getName()))
+					node2 = j;
+			}
+
+			if ((node1 != -1) && (node2 != -1))
+			{
+				net.connections.get(node1).get(node2).id = 1;
+
+				net.connections.get(node1).get(node2).node1Id = node1;
+
+				net.connections.get(node1).get(node2).node2Id = node2;
+
+				net.connections.get(node1).get(node2).capacity = 100;
+
+				net.connections.get(node1).get(node2).delay = 1;
+
+				net.links.add(net.connections.get(node1).get(node2));
+			} else
+
+				System.out.println("Error: couldn't find end point of a link");
+		}
+		return net;
+	}
 }
--- extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/capability/example/shell/ExampleCommand.java
+++ extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/capability/example/shell/ExampleCommand.java
@@ -1,21 +1,11 @@
 package org.opennaas.extensions.vnmapper.capability.example.shell;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.apache.felix.gogo.commands.Argument;
 import org.apache.felix.gogo.commands.Command;
 import org.opennaas.core.resources.IResource;
-import org.opennaas.core.resources.ResourceManager;
 import org.opennaas.core.resources.shell.GenericKarafCommand;
-import org.opennaas.extensions.network.model.NetworkModel;
-import org.opennaas.extensions.network.model.NetworkModelHelper;
-import org.opennaas.extensions.network.model.topology.Device;
-import org.opennaas.extensions.network.model.topology.Link;
 import org.opennaas.extensions.vnmapper.capability.example.ExampleCapability;
-import org.opennaas.extensions.vnmapper.capability.example.InPNetwork;
-import org.opennaas.extensions.vnmapper.capability.example.PLink;
-import org.opennaas.extensions.vnmapper.capability.example.PNode;
+import org.opennaas.extensions.vnmapper.capability.example.VNTRequest;
 
 /**
  * 
@@ -36,85 +26,12 @@ public class ExampleCommand extends GenericKarafCommand {
 		printInitCommand("sayHello");
 		try {
 
-			// ///////////
-			ResourceManager resourcemng = (ResourceManager) getResourceManager();
-			List<IResource> res1 = resourcemng.listResourcesByType("network");
-			List<Link> links = NetworkModelHelper.getLinks((NetworkModel) res1.get(0).getModel());
-			// System.out.println(" network links num : " + links.size());
-			// for(int i=0;i<links.size();i++)
-			// {
-			// System.out.println(" link : " + links.get(i).getSource().getName());
-			// System.out.println(" link : " + links.get(i).getSource().getDevice().getName());
-			//
-			// }
-
-			List<Device> devices = NetworkModelHelper.getDevices((NetworkModel) res1.get(0).getModel());
-			// System.out.println(" devices num : " + devices.size());
-			// for(int i=0;i<devices.size();i++)
-			// {
-			// System.out.println(" device : " + devices.get(i).getName());
-			//
-			// }
-
-			// /// generate the network object
-			InPNetwork net = new InPNetwork();
-
-			for (int i = 0; i < devices.size(); i++) {
-				PNode n = new PNode();
-				n.id = i;
-				n.pnodeID = devices.get(i).getName();
-				n.capacity = 16;
-				n.pathNum = 0;
-				net.nodes.add(n);
-				net.nodeNum++;
-			}
-
-			for (int i = 0; i < net.nodeNum; i++) {
-				net.connections.add(new ArrayList<PLink>());
-				for (int j = 0; j < net.nodeNum; j++) {
-					net.connections.get(i).add(new PLink());
-				}
-			}
-
-			for (int i = 0; i < links.size(); i++)
-			{
-
-				int node1 = -1;
-
-				int node2 = -1;
-				for (int j = 0; j < devices.size() && (node1 == -1 || node2 == -1); j++) {
-					if (devices.get(j).getName().equals(links.get(i).getSource().getDevice().getName()))
-						node1 = j;
-					if (devices.get(j).getName().equals(links.get(i).getSink().getDevice().getName()))
-						node2 = j;
-				}
-
-				if ((node1 != -1) && (node2 != -1))
-				{
-					net.connections.get(node1).get(node2).id = 1;
-
-					net.connections.get(node1).get(node2).node1Id = node1;
-
-					net.connections.get(node1).get(node2).node2Id = node2;
-
-					net.connections.get(node1).get(node2).capacity = 100;
-
-					net.connections.get(node1).get(node2).delay = 1;
-
-					net.links.add(net.connections.get(node1).get(node2));
-				} else
-
-					System.out.println("Error: couldn't find end point of a link");
-			}
-
-			net.printNetwork();
-
-			// ///////////
+			VNTRequest vnt = new VNTRequest();
+			vnt = vnt.readVNTRequestFromXMLFile(username);
 
 			IResource resource = getResourceFromFriendlyName(resourceName);
 			ExampleCapability capab = (ExampleCapability) resource.getCapabilityByType(ExampleCapability.CAPABILITY_TYPE);
-			String greeting = capab.sayHello(username, net);
-			printInfo(resourceName + " says : " + greeting);
+			capab.sayHello(vnt);
 
 		} catch (Exception e) {
 			printError("Error greeting from resource " + resourceName);
