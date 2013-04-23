7dff54f1d43fa4c328f450a85c7ade217351f4fa
Adrian Rosello <adrian_rosello@i2cat.net>
2013-04-23 12:52:00 +0200

    Print methods avoided.

--- extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/MapperHelper.java
+++ extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/MapperHelper.java
@@ -1,7 +1,6 @@
 package org.opennaas.extensions.vnmapper;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.Random;
 
 public class MapperHelper {
@@ -26,51 +25,22 @@ public class MapperHelper {
 		return getRandInt(nodeMin, nodeMax, r);
 	}
 
-	public static void printMatchingResult(ArrayList<ArrayList<Integer>> matchingResult)
+	public static String matchingResultToString(ArrayList<ArrayList<Integer>> matchingResult)
 	{
+		String str = "";
+
 		for (int i = 0; i < matchingResult.size(); i++)
 		{
-			System.out.print(i + ":");
+			str += i + ": ";
 			for (int j = 0; j < matchingResult.get(i).size(); j++)
 			{
 				int t = Integer.valueOf(matchingResult.get(i).get(j).toString());
-				System.out.print(t + "-");
+				str += t + "- ";
 			}
-			System.out.println();
+			str += "\n";
 		}
-	}
 
-	public static void printMappingResult(MappingResult mres)
-	{
-		System.out.println("Mapping Result: ");
-		System.out.println("Nodes: ");
-		for (int i = 0; i < mres.getNodes().size(); i++)
-		{
-			int t = Integer.valueOf(mres.getNodes().get(i).toString());
-			if (t > 0)
-				System.out.println(i + " : " + t);
-		}
-		System.out.println("Links: ");
-
-		for (int i = 0; i < mres.getLinks().size(); i++)
-			for (int j = 0; j < mres.getLinks().get(i).size(); j++)
-			{
-				int t = Integer.valueOf(mres.getLinks().get(i).get(j).toString());
-				if (t > 0)
-					System.out.println(i + "-" + j + " : " + t);
-			}
-
-		System.out.println("------------------");
-	}
-
-	public static void printSet(IntSet s)
-	{
-		Iterator<Integer> it = s.iterator();
-
-		while (it.hasNext())
-		{
-			System.out.println("set : " + it.next());
-		}
+		return str;
 	}
 
 }
