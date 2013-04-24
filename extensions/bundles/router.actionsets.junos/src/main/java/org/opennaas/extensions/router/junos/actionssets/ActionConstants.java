31090a5c70bb89a517d87d38fe9c043ab3149ebf
Adrian Rosello <adrian_rosello@i2cat.net>
2013-04-24 11:08:22 +0200

    Add and remove IPs actions skeleton.

--- extensions/bundles/router.actionsets.junos/src/main/java/org/opennaas/extensions/router/junos/actionssets/ActionConstants.java
+++ extensions/bundles/router.actionsets.junos/src/main/java/org/opennaas/extensions/router/junos/actionssets/ActionConstants.java
@@ -1,13 +1,13 @@
 package org.opennaas.extensions.router.junos.actionssets;
 
 import org.opennaas.extensions.router.capability.bgp.BGPActionSet;
+import org.opennaas.extensions.router.capability.chassis.ChassisActionSet;
 import org.opennaas.extensions.router.capability.gretunnel.GRETunnelActionSet;
 import org.opennaas.extensions.router.capability.ip.IPActionSet;
 import org.opennaas.extensions.router.capability.ospf.OSPFActionSet;
+import org.opennaas.extensions.router.capability.ospfv3.OSPFv3ActionSet;
 import org.opennaas.extensions.router.capability.staticroute.StaticRouteActionSet;
 import org.opennaas.extensions.router.capability.vrrp.VRRPActionSet;
-import org.opennaas.extensions.router.capability.chassis.ChassisActionSet;
-import org.opennaas.extensions.router.capability.ospfv3.OSPFv3ActionSet;
 
 public class ActionConstants {
 
@@ -38,6 +38,10 @@ public class ActionConstants {
 	public static final String	SETIPv4								= IPActionSet.SET_IPv4;
 	public static final String	SETIPv6								= IPActionSet.SET_IPv6;
 	public static final String	SETINTERFACEDESCRIPTION				= IPActionSet.SET_INTERFACE_DESCRIPTION;
+	public static final String	ADDIPv4								= IPActionSet.ADD_IPv4;
+	public static final String	ADDIPv6								= IPActionSet.ADD_IPv6;
+	public static final String	REMOVEIPv4							= IPActionSet.REMOVE_IPv4;
+	public static final String	REMOVEIPv6							= IPActionSet.REMOVE_IPv6;
 
 	// TODO THE SAME NAME FOR THE OTHER LIST LOGICAL ROUTERS
 	public static final String	GETLOGICALROUTERS					= "getLogicalRouters";
---
+++ extensions/bundles/router.actionsets.junos/src/main/java/org/opennaas/extensions/router/junos/actionssets/actions/ip/RemoveIPv4Action.java
@@ -0,0 +1,34 @@
+package org.opennaas.extensions.router.junos.actionssets.actions.ip;
+
+import org.opennaas.core.resources.action.ActionException;
+import org.opennaas.core.resources.action.ActionResponse;
+import org.opennaas.core.resources.protocol.IProtocolSession;
+import org.opennaas.extensions.router.junos.actionssets.actions.JunosAction;
+
+public class RemoveIPv4Action extends JunosAction {
+
+	@Override
+	public void executeListCommand(ActionResponse actionResponse, IProtocolSession protocol) throws ActionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void parseResponse(Object responseMessage, Object model) throws ActionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean checkParams(Object params) throws ActionException {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void prepareMessage() throws ActionException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
