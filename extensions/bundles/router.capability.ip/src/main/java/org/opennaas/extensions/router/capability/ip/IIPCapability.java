a1164d15b0ef7c801f7cd4a2a5ea64c62b2deb32
Adrian Rosello <adrian_rosello@i2cat.net>
2013-04-24 11:02:38 +0200

    addIP and removeIP added to IPCapability.

--- extensions/bundles/router.capability.ip/src/main/java/org/opennaas/extensions/router/capability/ip/IIPCapability.java
+++ extensions/bundles/router.capability.ip/src/main/java/org/opennaas/extensions/router/capability/ip/IIPCapability.java
@@ -34,6 +34,54 @@ public interface IIPCapability extends ICapability {
 			throws CapabilityException;
 
 	/**
+	 * Set the description for the given interface
+	 * 
+	 * @param iface
+	 * @param ip
+	 * @throws CapabilityException
+	 */
+	@POST
+	@Path("/setInterfaceDescription")
+	@Consumes(MediaType.APPLICATION_XML)
+	public void setInterfaceDescription(LogicalPort iface) throws CapabilityException;
+
+	@POST
+	@Path("/addIPv4")
+	@Consumes(MediaType.APPLICATION_XML)
+	public void addIPv4(SetIpAddressRequest request)
+			throws CapabilityException;
+
+	@POST
+	@Path("/addIPv6")
+	@Consumes(MediaType.APPLICATION_XML)
+	public void addIPv6(SetIpAddressRequest request)
+			throws CapabilityException;
+
+	@POST
+	@Path("/addIP")
+	@Consumes(MediaType.APPLICATION_XML)
+	public void addIP(SetIpAddressRequest request)
+			throws CapabilityException;
+
+	@POST
+	@Path("/removeIPv4")
+	@Consumes(MediaType.APPLICATION_XML)
+	public void removeIPv4(SetIpAddressRequest request)
+			throws CapabilityException;
+
+	@POST
+	@Path("/removeIPv6")
+	@Consumes(MediaType.APPLICATION_XML)
+	public void removeIPv6(SetIpAddressRequest request)
+			throws CapabilityException;
+
+	@POST
+	@Path("/removeIP")
+	@Consumes(MediaType.APPLICATION_XML)
+	public void removeIP(SetIpAddressRequest request)
+			throws CapabilityException;
+
+	/**
 	 * Set the given ip to the logical device
 	 * 
 	 * @param params
@@ -50,16 +98,22 @@ public interface IIPCapability extends ICapability {
 
 	public void setIP(LogicalDevice logicalDevice, String ipAddress) throws CapabilityException;
 
-	/**
-	 * Set the description for the given interface
-	 * 
-	 * @param iface
-	 * @param ip
-	 * @throws CapabilityException
-	 */
-	@POST
-	@Path("/setInterfaceDescription")
-	@Consumes(MediaType.APPLICATION_XML)
-	public void setInterfaceDescription(LogicalPort iface) throws CapabilityException;
+	public void addIPv4(LogicalDevice logicalDevice, IPProtocolEndpoint ip)
+			throws CapabilityException;
+
+	public void addIPv6(LogicalDevice logicalDevice, IPProtocolEndpoint ip) throws CapabilityException;
+
+	public void addIP(LogicalDevice logicalDevice, IPProtocolEndpoint ip) throws CapabilityException;
+
+	public void addIP(LogicalDevice logicalDevice, String ipAddress) throws CapabilityException;
+
+	public void removeIPv4(LogicalDevice logicalDevice, IPProtocolEndpoint ip)
+			throws CapabilityException;
+
+	public void removeIPv6(LogicalDevice logicalDevice, IPProtocolEndpoint ip) throws CapabilityException;
+
+	public void removeIP(LogicalDevice logicalDevice, IPProtocolEndpoint ip) throws CapabilityException;
+
+	public void removeIP(LogicalDevice logicalDevice, String ipAddress) throws CapabilityException;
 
 }
