6ea70928cebc6869e0ef31bd19d5282bfae59dce
isartcanyameres <isart.canyameres@i2cat.net>
2013-06-03 12:39:58 +0200

    Clear resources command.
    This command removes all resources in platform, stopping them first if they are active.

---
+++ core/resources/src/main/java/org/opennaas/core/resources/shell/ClearResourcesCommand.java
@@ -0,0 +1,20 @@
+package org.opennaas.core.resources.shell;
+
+import org.apache.felix.gogo.commands.Command;
+
+@Command(scope = "resource", name = "clear", description = "Remove all resources in the platform (stopping active ones)")
+public class ClearResourcesCommand extends GenericKarafCommand {
+
+	@Override
+	protected Object doExecute() throws Exception {
+		printInitCommand("clear resources");
+		try {
+			getResourceManager().destroyAllResources();
+		} catch (Exception e) {
+			printError("Error celaring resources");
+			printError(e);
+		}
+		printEndCommand();
+		return null;
+	}
+}
