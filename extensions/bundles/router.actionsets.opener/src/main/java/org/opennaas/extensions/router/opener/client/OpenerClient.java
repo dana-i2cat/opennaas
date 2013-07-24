81303b0ab238fcb7a74f3c98667b9e11a0025088
isartcanyameres <isart.canyameres@i2cat.net>
2013-07-24 09:02:59 +0200

    Remove unused OpenerClient class.

--- extensions/bundles/router.actionsets.opener/src/main/java/org/opennaas/extensions/router/opener/client/OpenerClient.java
+++
@@ -1,9 +0,0 @@
-package org.opennaas.extensions.router.opener.client;
-
-public class OpenerClient {
-	
-	//CXF REST client: org.apache.cxf.jaxrs.client.WebClient
-	//http://cxf.apache.org/docs/jax-rs-client-api.html#JAX-RSClientAPI-CXFWebClientAPI
-
-
-}
