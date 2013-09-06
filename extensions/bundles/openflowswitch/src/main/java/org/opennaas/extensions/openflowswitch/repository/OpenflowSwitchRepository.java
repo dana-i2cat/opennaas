c89ac29a2c1f81103567f7656fd3fdb20ef55660
Adrian Rosello <adrian.rosello@i2cat.net>
2013-09-06 10:47:25 +0200

    Openflow Switch bundle skeleton.
    It contains Activator and repo.

---
+++ extensions/bundles/openflowswitch/src/main/java/org/opennaas/extensions/openflowswitch/repository/OpenflowSwitchRepository.java
@@ -0,0 +1,28 @@
+package org.opennaas.extensions.openflowswitch.repository;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.opennaas.core.resources.ResourceRepository;
+import org.opennaas.core.resources.capability.ICapabilityFactory;
+
+public class OpenflowSwitchRepository extends ResourceRepository {
+
+	Log	log	= LogFactory.getLog(OpenflowSwitchRepository.class);
+
+	public OpenflowSwitchRepository(String resourceType) {
+		super(resourceType);
+	}
+
+	public void capabilityFactoryAdded(ICapabilityFactory capabilityFactory) {
+		log.info("Adding factory: " + capabilityFactory.getType());
+		this.capabilityFactories.put(capabilityFactory.getType(), capabilityFactory);
+	}
+
+	public void capabilityFactoryDeleted(ICapabilityFactory capabilityFactory) {
+		if (capabilityFactory != null) {
+			log.info("Deleting factory: " + capabilityFactory.getType());
+			this.capabilityFactories.remove(capabilityFactory.getType());
+		}
+	}
+
+}
