6f15430aa1cb02cc0967d2f9ed9e4825094d26a5
Adrian Rosello <adrian.rosello@i2cat.net>
2013-09-17 18:14:27 +0200

    OFSwtich model serialization corrected.

--- extensions/bundles/openflowswitch/src/main/java/org/opennaas/extensions/openflowswitch/model/FloodlightOFFlow.java
+++ extensions/bundles/openflowswitch/src/main/java/org/opennaas/extensions/openflowswitch/model/FloodlightOFFlow.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
 
+@XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
 public class FloodlightOFFlow {
 
