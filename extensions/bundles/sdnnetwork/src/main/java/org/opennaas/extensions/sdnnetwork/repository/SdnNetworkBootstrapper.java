c1ac4825978267b2b413c99dba5dab88322a60ad
isartcanyameres <isart.canyameres@i2cat.net>
2013-09-18 18:02:55 +0200

    Implement Bootstrapper that does not reset model.

--- extensions/bundles/sdnnetwork/src/main/java/org/opennaas/extensions/sdnnetwork/repository/SdnNetworkBootstrapper.java
+++ extensions/bundles/sdnnetwork/src/main/java/org/opennaas/extensions/sdnnetwork/repository/SdnNetworkBootstrapper.java
@@ -1,8 +1,12 @@
 package org.opennaas.extensions.sdnnetwork.repository;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.opennaas.core.resources.IModel;
 import org.opennaas.core.resources.IResourceBootstrapper;
 import org.opennaas.core.resources.Resource;
 import org.opennaas.core.resources.ResourceException;
+import org.opennaas.extensions.sdnnetwork.model.SDNNetworkModel;
 
 /**
  * 
@@ -10,23 +14,32 @@ import org.opennaas.core.resources.ResourceException;
  *
  */
 public class SdnNetworkBootstrapper implements IResourceBootstrapper {
+	
+	Log log	= LogFactory.getLog(SdnNetworkBootstrapper.class);
+	
+	private IModel	oldModel;
 
 	@Override
 	public void bootstrap(Resource resource) throws ResourceException {
-		// TODO Auto-generated method stub
+		log.info("Loading bootstrap to start resource...");
+		oldModel = resource.getModel();
+		resource.setModel(new SDNNetworkModel());
 
 	}
 
 	@Override
 	public void revertBootstrap(Resource resource) throws ResourceException {
-		// TODO Auto-generated method stub
-
+		resource.setModel(oldModel);
 	}
 
+	/**
+	 * Since we're not going to read information from real switches, we would not implement the reset model method,
+	 * so no one can delete the modelinformation. Model will be reset only during bootstrap.
+	 * 
+	 */
 	@Override
 	public void resetModel(Resource resource) throws ResourceException {
-		// TODO Auto-generated method stub
-
+		log.info("Ignoring resetModel signal.");
 	}
 
 }
