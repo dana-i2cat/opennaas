6e2ec933ada049a067d97722176f8291447f247e
Adrian Rosello <adrian.rosello@i2cat.net>
2013-09-18 10:53:50 +0200

    CheckParams method for createOFForwardingRule action.

--- extensions/bundles/openflowswitch.driver.floodlight/src/main/java/org/opennaas/extensions/openflowswitch/driver/floodlight/actionssets/actions/CreateOFForwardingAction.java
+++ extensions/bundles/openflowswitch.driver.floodlight/src/main/java/org/opennaas/extensions/openflowswitch/driver/floodlight/actionssets/actions/CreateOFForwardingAction.java
@@ -4,8 +4,12 @@ import org.opennaas.core.resources.action.ActionException;
 import org.opennaas.core.resources.action.ActionResponse;
 import org.opennaas.core.resources.protocol.IProtocolSessionManager;
 import org.opennaas.extensions.openflowswitch.driver.floodlight.actionssets.FloodlightAction;
+import org.opennaas.extensions.openflowswitch.model.FloodlightOFAction;
+import org.opennaas.extensions.openflowswitch.model.FloodlightOFFlow;
 
 public class CreateOFForwardingAction extends FloodlightAction {
+
+	private static final String	FORWARDING_ACTION	= "output";
 
 	@Override
 	public ActionResponse execute(IProtocolSessionManager protocolSessionManager) throws ActionException {
@@ -15,8 +19,31 @@ public class CreateOFForwardingAction extends FloodlightAction {
 
 	@Override
 	public boolean checkParams(Object params) throws ActionException {
-		return false;
+
+		if (params == null || !(params instanceof FloodlightOFFlow))
+			throw new ActionException("Invalid parameters for action " + this.actionID);
+
+		FloodlightOFFlow flowRule = (FloodlightOFFlow) params;
+
+		if (flowRule.getName().isEmpty())
+			throw new ActionException("No flow id given to params in action " + this.actionID);
+
+		for (FloodlightOFAction action : flowRule.getActions()) {
+
+			if (action.getType().isEmpty())
+				throw new ActionException("No OFAction type given to params in action " + this.actionID);
+
+			if (action.getValue().isEmpty())
+				throw new ActionException("No OFAction value given to params in action " + this.actionID);
+
+			if (!(action.getType().equals(FORWARDING_ACTION)))
+				throw new ActionException(
+						"Wrong action type given to params in " + this.actionID + ". Expected was \"" + FORWARDING_ACTION + "\", but \"" + action
+								.getType() + "\" was given.");
+
+		}
+
+		return true;
 
 	}
-
 }
