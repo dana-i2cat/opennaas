bbffecbcd9fcbf72efa1abdc9775cb6932d6b0d1
Adrian Rosello <adrian.rosello@i2cat.net>
2013-09-25 09:57:10 +0200

    Integration test for capability.

--- itests/openflowswitch/src/test/java/org/opennaas/itests/openflowswitch/OpenflowForwardingCapabilityIntegrationTest.java
+++ itests/openflowswitch/src/test/java/org/opennaas/itests/openflowswitch/OpenflowForwardingCapabilityIntegrationTest.java
@@ -26,14 +26,18 @@ import org.opennaas.core.resources.IModel;
 import org.opennaas.core.resources.IResource;
 import org.opennaas.core.resources.IResourceManager;
 import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.capability.CapabilityException;
 import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
 import org.opennaas.core.resources.descriptor.ResourceDescriptor;
 import org.opennaas.core.resources.helpers.ResourceHelper;
 import org.opennaas.core.resources.protocol.IProtocolManager;
+import org.opennaas.core.resources.protocol.IProtocolSessionManager;
 import org.opennaas.core.resources.protocol.ProtocolException;
 import org.opennaas.extensions.openflowswitch.capability.IOpenflowForwardingCapability;
 import org.opennaas.extensions.openflowswitch.capability.OpenflowForwardingCapability;
 import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.FloodlightProtocolSession;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.client.FloodlightMockClientFactory;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.client.IFloodlightStaticFlowPusherClient;
 import org.opennaas.extensions.openflowswitch.model.FloodlightOFAction;
 import org.opennaas.extensions.openflowswitch.model.FloodlightOFFlow;
 import org.opennaas.extensions.openflowswitch.model.OFFlowTable;
@@ -47,36 +51,37 @@ import org.ops4j.pax.exam.spi.reactors.EagerSingleStagedReactorFactory;
 import org.ops4j.pax.exam.util.Filter;
 import org.osgi.service.blueprint.container.BlueprintContainer;
 
+//import static org.opennaas.itests.helpers.OpennaasExamOptions.openDebugSocket;
+
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(EagerSingleStagedReactorFactory.class)
 public class OpenflowForwardingCapabilityIntegrationTest {
 
-	private final static Log				log					= LogFactory.getLog(OpenflowForwardingCapabilityIntegrationTest.class);
+	private final static Log	log					= LogFactory.getLog(OpenflowForwardingCapabilityIntegrationTest.class);
 
-	private static final String				SWITCH_ID			= "00:00:00:00:00:00:00:01";
+	private static final String	SWITCH_ID			= "00:00:00:00:00:00:00:01";
 
-	private static final String				ACTIONSET_NAME		= "floodlight";
-	private static final String				ACTIONSET_VERSION	= "0.90";
-	private static final String				CAPABILITY_URI		= "mock://user:pass@host.net:2212/mocksubsystem";
-	private static final String				RESOURCE_TYPE		= "openflowswitch";
-	private static final String				RESOURCE_URI		= "mock://user:pass@host.net:2212/mocksubsystem";
-	private static final String				RESOURCE_INFO_NAME	= "ofswitch";
+	private static final String	ACTIONSET_NAME		= "floodlight";
+	private static final String	ACTIONSET_VERSION	= "0.90";
+	private static final String	CAPABILITY_URI		= "mock://user:pass@host.net:2212/mocksubsystem";
+	private static final String	RESOURCE_TYPE		= "openflowswitch";
+	private static final String	RESOURCE_URI		= "mock://user:pass@host.net:2212/mocksubsystem";
+	private static final String	RESOURCE_INFO_NAME	= "ofswitch";
 
-	private static final String				PROTOCOL			= FloodlightProtocolSession.FLOODLIGHT_PROTOCOL_TYPE;
-	private static final String				SWITCH_ID_NAME		= FloodlightProtocolSession.SWITCHID_CONTEXT_PARAM_NAME;
+	private static final String	PROTOCOL			= FloodlightProtocolSession.FLOODLIGHT_PROTOCOL_TYPE;
+	private static final String	SWITCH_ID_NAME		= FloodlightProtocolSession.SWITCHID_CONTEXT_PARAM_NAME;
 
-	private IOpenflowForwardingCapability	ofForwardingCapability;
-	private IResource						ofSwitchResource;
-
-	@Inject
-	private IResourceManager				resourceManager;
+	private IResource			ofSwitchResource;
 
 	@Inject
-	private IProtocolManager				protocolManager;
+	private IResourceManager	resourceManager;
+
+	@Inject
+	private IProtocolManager	protocolManager;
 
 	@Inject
 	@Filter("(osgi.blueprint.container.symbolicname=org.opennaas.extensions.openflowswitch.driver.floodlight)")
-	private BlueprintContainer				floodlightDriverBundleContainer;
+	private BlueprintContainer	floodlightDriverBundleContainer;
 
 	@Configuration
 	public static Option[] configuration() {
@@ -111,8 +116,14 @@ public class OpenflowForwardingCapabilityIntegrationTest {
 
 	}
 
+	/**
+	 * This test checks that, when a flow is created, the action populates the model if there's no error in its execution.
+	 * 
+	 * @throws ResourceException
+	 * @throws ProtocolException
+	 */
 	@Test
-	public void createOpenflowForwardingRuleTest() throws ResourceException {
+	public void createOpenflowForwardingRuleTest() throws ResourceException, ProtocolException {
 
 		IModel model = ofSwitchResource.getModel();
 		Assert.assertNotNull("Openflowswitch model should not be null after starting.", model);
@@ -145,9 +156,79 @@ public class OpenflowForwardingCapabilityIntegrationTest {
 
 		capability.createOpenflowForwardingRule(forwardingRule);
 
+		Assert.assertNotNull("Openflowswitch model should contain forwardingRule tables.", switchModel.getOfTables());
+		Assert.assertEquals("Openflowswitch model should contain one forwardingRule table after action execution.", 1, switchModel.getOfTables()
+				.size());
+
+		Assert.assertEquals(SWITCH_ID, switchModel.getSwitchId());
+
+		OFFlowTable ofNewTable = switchModel.getOfTables().get(0);
+
+		Assert.assertNotNull("Openflow rules should not be null in forwardingRules table.", ofNewTable.getOfForwardingRules());
+		Assert.assertEquals("Openflow table should contain one forwarding rule.", 1, ofNewTable.getOfForwardingRules().size());
+		Assert.assertEquals("Model forwarding rule should be the one created for the action.", forwardingRule,
+				ofNewTable.getOfForwardingRules().get(0));
+
+		FloodlightOFFlow newForwardingRule = new FloodlightOFFlow();
+		newForwardingRule.setName("flow1");
+		newForwardingRule.setPriority("1");
+
+		FloodlightOFAction newFloodlightAction = new FloodlightOFAction();
+		newFloodlightAction.setType("output");
+		newFloodlightAction.setValue("dstPort=12");
+
+		List<FloodlightOFAction> newActions = new ArrayList<FloodlightOFAction>();
+		newActions.add(newFloodlightAction);
+
+		newForwardingRule.setActions(newActions);
+
+		capability.createOpenflowForwardingRule(newForwardingRule);
+
+		Assert.assertNotNull("Openflowswitch model should contain forwardingRule tables.", switchModel.getOfTables());
+		Assert.assertEquals("Openflowswitch model should contain one forwardingRule table after action execution.", 1, switchModel.getOfTables()
+				.size());
+
+		Assert.assertEquals(SWITCH_ID, switchModel.getSwitchId());
+
+		OFFlowTable ofModelTable = switchModel.getOfTables().get(0);
+
+		Assert.assertNotNull("Openflow rules should not be null in forwardingRules table.", ofModelTable.getOfForwardingRules());
+		Assert.assertEquals("Openflow table should contain two forwarding rules.", 2, ofModelTable.getOfForwardingRules().size());
+		Assert.assertEquals("Model forwarding rule should be the one created for the action.", forwardingRule,
+				ofModelTable.getOfForwardingRules().get(0));
+		Assert.assertEquals("Model forwarding rule should be the one created for the action.", newForwardingRule,
+				ofModelTable.getOfForwardingRules().get(1));
 	}
 
-	public void startResource() throws ResourceException, ProtocolException {
+	/**
+	 * Test create a forwarding rule with a wrong actionId, since forwardingCapability supports only "output" actions.
+	 * 
+	 * @throws ResourceException
+	 */
+	@Test(expected = CapabilityException.class)
+	public void wrongActionTest() throws ResourceException {
+
+		IOpenflowForwardingCapability capability = (IOpenflowForwardingCapability) ofSwitchResource
+				.getCapabilityByInterface(IOpenflowForwardingCapability.class);
+
+		FloodlightOFFlow forwardingRule = new FloodlightOFFlow();
+		forwardingRule.setName("flow1");
+		forwardingRule.setPriority("1");
+
+		FloodlightOFAction floodlightAction = new FloodlightOFAction();
+		floodlightAction.setType("wrongActionName");
+		floodlightAction.setValue("dstPort=12");
+
+		List<FloodlightOFAction> actions = new ArrayList<FloodlightOFAction>();
+		actions.add(floodlightAction);
+
+		forwardingRule.setActions(actions);
+
+		capability.createOpenflowForwardingRule(forwardingRule);
+
+	}
+
+	private void startResource() throws ResourceException, ProtocolException {
 		/* initialize model */
 		List<CapabilityDescriptor> lCapabilityDescriptors = new ArrayList<CapabilityDescriptor>();
 
@@ -173,6 +254,21 @@ public class OpenflowForwardingCapabilityIntegrationTest {
 
 		// Start resource
 		resourceManager.startResource(ofSwitchResource.getResourceIdentifier());
+
+		prepareClient();
+
+	}
+
+	private void prepareClient() throws ProtocolException {
+
+		IProtocolSessionManager sessionManager = protocolManager.getProtocolSessionManager(ofSwitchResource.getResourceIdentifier().getId());
+
+		FloodlightProtocolSession session = (FloodlightProtocolSession) sessionManager.obtainSessionByProtocol(
+				FloodlightProtocolSession.FLOODLIGHT_PROTOCOL_TYPE, false);
+
+		session.setClientFactory(new FloodlightMockClientFactory());
+		IFloodlightStaticFlowPusherClient client = session.getClientFactory().createClient(session.getSessionContext());
+		session.setFloodlightClient(client);
 
 	}
 
