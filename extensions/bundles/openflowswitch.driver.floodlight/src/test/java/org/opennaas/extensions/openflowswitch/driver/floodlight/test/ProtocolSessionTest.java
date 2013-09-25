28c8450d3e238b18f842ef8be4eb8ab3cd1bfa57
isartcanyameres <isart.canyameres@i2cat.net>
2013-09-25 10:07:16 +0200

    Test for floodlight client.

---
+++ extensions/bundles/openflowswitch.driver.floodlight/src/test/java/org/opennaas/extensions/openflowswitch/driver/floodlight/test/ProtocolSessionTest.java
@@ -0,0 +1,85 @@
+package org.opennaas.extensions.openflowswitch.driver.floodlight.test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.opennaas.core.resources.protocol.ProtocolException;
+import org.opennaas.core.resources.protocol.ProtocolSessionContext;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.FloodlightProtocolSession;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.FloodlightProtocolSessionFactory;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.client.IFloodlightStaticFlowPusherClient;
+import org.opennaas.extensions.openflowswitch.model.FloodlightOFAction;
+import org.opennaas.extensions.openflowswitch.model.FloodlightOFFlow;
+import org.opennaas.extensions.openflowswitch.model.FloodlightOFMatch;
+
+public class ProtocolSessionTest {
+	
+	private static final String FLOODLIGHT_URI = "http://dev.ofertie.i2cat.net:6633";
+	private static final String SWITCH_ID = "00:00:00:00:00:00:00:03";
+	private static final String SESSION_ID = "0001";
+	
+	ProtocolSessionContext context;
+	FloodlightProtocolSession session;
+	IFloodlightStaticFlowPusherClient client;
+	
+	FloodlightOFFlow flow;
+	
+	
+	@Before
+	public void initSessionAndClient() throws ProtocolException {
+		context = generateContext();
+		session = (FloodlightProtocolSession) new FloodlightProtocolSessionFactory().createProtocolSession(SESSION_ID, context);
+		session.connect();
+		client = session.getFloodlightClientForUse();
+	}
+	
+	@Before
+	public void initFlow() throws ProtocolException {
+		
+		flow = new FloodlightOFFlow();
+		flow.setSwitchId(SWITCH_ID);
+		flow.setName("flow-mod-1");
+		flow.setPriority("32768");
+		flow.setActive(true);
+
+		FloodlightOFMatch match = new FloodlightOFMatch();
+		match.setIngressPort("2");
+		flow.setMatch(match);
+
+		FloodlightOFAction action = new FloodlightOFAction();
+		action.setType("output");
+		action.setValue("3");
+
+		flow.setActions(Arrays.asList(action));
+	}
+	
+	
+	@After
+	public void disconnect() throws ProtocolException {
+		session.disconnect();
+	}
+
+	private ProtocolSessionContext generateContext() {
+		ProtocolSessionContext context = new ProtocolSessionContext();
+		context.addParameter(ProtocolSessionContext.PROTOCOL, FloodlightProtocolSession.FLOODLIGHT_PROTOCOL_TYPE);
+		context.addParameter(ProtocolSessionContext.PROTOCOL_URI, FLOODLIGHT_URI);
+		context.addParameter(ProtocolSessionContext.AUTH_TYPE, "noauth");
+		context.addParameter(FloodlightProtocolSession.SWITCHID_CONTEXT_PARAM_NAME, SWITCH_ID);
+		return context;
+	}
+	
+	@Test
+	public void clientTest() {
+		client.addFlow(flow);
+		List<FloodlightOFFlow> flows = client.getFlows(SWITCH_ID);
+		Assert.assertTrue("getFlows() contains created flow", flows.contains(flow));
+		client.deleteFlow(flow.getName());
+		flows = client.getFlows(SWITCH_ID);
+		Assert.assertFalse("getFlows() does not contain deleted flow", flows.contains(flow));
+	}
+
+}
