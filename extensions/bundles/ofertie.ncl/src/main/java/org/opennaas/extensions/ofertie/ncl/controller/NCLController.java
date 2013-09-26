d27e8992789407ec385652d2bebee26d21d2e275
Adrian Rosello <adrian.rosello@i2cat.net>
2013-09-26 09:02:45 +0200

    First draft for allocateFlowmethod in NCLController.

--- extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/controller/NCLController.java
+++ extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/controller/NCLController.java
@@ -1,21 +1,55 @@
 package org.opennaas.extensions.ofertie.ncl.controller;
 
+import org.opennaas.core.resources.ActivatorException;
+import org.opennaas.core.resources.IResource;
+import org.opennaas.core.resources.IResourceManager;
+import org.opennaas.core.resources.ResourceException;
+import org.opennaas.extensions.ofertie.ncl.Activator;
 import org.opennaas.extensions.ofertie.ncl.controller.api.INCLController;
 import org.opennaas.extensions.ofertie.ncl.provisioner.api.exceptions.FlowAllocationException;
 import org.opennaas.extensions.ofertie.ncl.provisioner.api.model.FlowRequest;
 import org.opennaas.extensions.ofertie.ncl.provisioner.api.model.Route;
+import org.opennaas.extensions.sdnnetwork.capability.ofprovision.IOFProvisioningNetworkCapability;
+import org.opennaas.extensions.sdnnetwork.model.SDNNetworkOFFlow;
 
 public class NCLController implements INCLController {
 
 	@Override
 	public String allocateFlow(FlowRequest flowRequest, Route route,
 			String networkId) throws FlowAllocationException {
-		
-		// TODO retrieve SDN network from OpenNaaS using networkId
-		// TODO delegate to its OpenflowProvisioningNetworkCapability
-		// TODO obtain flowId from result and return it
-		
-		return String.valueOf(flowRequest.getTos());
+
+		try {
+
+			SDNNetworkOFFlow flowWithRoute = parseFlowRequestIntoSDNFlow(flowRequest, route);
+
+			IResource networkResource = getResource(networkId);
+			IOFProvisioningNetworkCapability provisionCapab = (IOFProvisioningNetworkCapability) networkResource
+					.getCapabilityByInterface(IOFProvisioningNetworkCapability.class);
+
+			String flowId = provisionCapab.allocateOFFlow(flowWithRoute);
+
+			return flowId;
+
+		} catch (ActivatorException e) {
+			throw new FlowAllocationException(e);
+		} catch (ResourceException e) {
+			throw new FlowAllocationException(e);
+		}
+	}
+
+	private IResource getResource(String networkId) throws ActivatorException, ResourceException {
+
+		IResourceManager resourceManager = Activator.getResourceManagerService();
+		IResource resource = resourceManager.getResourceById(networkId);
+
+		return resource;
+	}
+
+	private SDNNetworkOFFlow parseFlowRequestIntoSDNFlow(FlowRequest flowRequest, Route route) {
+
+		// TODO merge with sdnnetwork needed before implementing method.
+
+		return null;
 	}
 
 }
