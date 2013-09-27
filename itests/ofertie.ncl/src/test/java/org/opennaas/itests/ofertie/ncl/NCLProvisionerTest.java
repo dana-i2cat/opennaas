126b76c8fbb9eeb6b15bfce1463f7a262cf06c04
Adrian Rosello <adrian.rosello@i2cat.net>
2013-09-27 14:33:18 +0200

    Test temporal commit.

--- itests/ofertie.ncl/src/test/java/org/opennaas/itests/ofertie/ncl/NCLProvisionerTest.java
+++ itests/ofertie.ncl/src/test/java/org/opennaas/itests/ofertie/ncl/NCLProvisionerTest.java
@@ -7,63 +7,163 @@ import static org.opennaas.itests.helpers.OpennaasExamOptions.noConsole;
 import static org.opennaas.itests.helpers.OpennaasExamOptions.opennaasDistributionConfiguration;
 import static org.ops4j.pax.exam.CoreOptions.options;
 
-import org.junit.Before;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.opennaas.core.resources.IResource;
 import org.opennaas.core.resources.IResourceManager;
-import org.opennaas.extensions.ofertie.ncl.controller.api.INCLController;
+import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
+import org.opennaas.core.resources.descriptor.ResourceDescriptor;
+import org.opennaas.core.resources.helpers.ResourceHelper;
+import org.opennaas.core.resources.protocol.IProtocolManager;
+import org.opennaas.core.resources.protocol.IProtocolSessionManager;
+import org.opennaas.core.resources.protocol.ProtocolException;
 import org.opennaas.extensions.ofertie.ncl.provisioner.api.INCLProvisioner;
-import org.ops4j.pax.exam.Inject;
+import org.opennaas.extensions.openflowswitch.capability.OpenflowForwardingCapability;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.FloodlightProtocolSession;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.client.FloodlightMockClientFactory;
+import org.opennaas.extensions.openflowswitch.driver.floodlight.protocol.client.IFloodlightStaticFlowPusherClient;
+import org.opennaas.extensions.sdnnetwork.capability.ofprovision.OFProvisioningNetworkCapability;
+import org.opennaas.itests.helpers.InitializerTestHelper;
 import org.ops4j.pax.exam.Option;
 import org.ops4j.pax.exam.junit.Configuration;
 import org.ops4j.pax.exam.junit.ExamReactorStrategy;
 import org.ops4j.pax.exam.junit.JUnit4TestRunner;
 import org.ops4j.pax.exam.spi.reactors.EagerSingleStagedReactorFactory;
-import org.ops4j.pax.exam.util.Filter;
-import org.osgi.service.blueprint.container.BlueprintContainer;
 
 @RunWith(JUnit4TestRunner.class)
 @ExamReactorStrategy(EagerSingleStagedReactorFactory.class)
 public class NCLProvisionerTest {
 
+	private IResource			switch3;
+	private IResource			switch4;
+	private IResource			switch5;
+	private IResource			switch6;
+	private IResource			switch7;
+	private IResource			switch8;
+
+	private IResource			sdnNetResource;
+
+	private static final String	SWITCH_3_ID						= "00:00:00:00:00:00:00:03";
+	private static final String	SWITCH_4_ID						= "00:00:00:00:00:00:00:04";
+	private static final String	SWITCH_5_ID						= "00:00:00:00:00:00:00:05";
+	private static final String	SWITCH_6_ID						= "00:00:00:00:00:00:00:06";
+	private static final String	SWITCH_7_ID						= "00:00:00:00:00:00:00:07";
+	private static final String	SWITCH_8_ID						= "00:00:00:00:00:00:00:08";
+
+	private static final String	FLOODLIGHT_ACTIONSET_NAME		= "floodlight";
+	private static final String	FLOODLIGHT_ACTIONSET_VERSION	= "0.90";
+	private static final String	FLOODLIGHT_PROTOCOL				= FloodlightProtocolSession.FLOODLIGHT_PROTOCOL_TYPE;
+
+	private static final String	OFSWITCH_RESOURCE_TYPE			= "openflowswitch";
+	private static final String	SWITCH_ID_NAME					= FloodlightProtocolSession.SWITCHID_CONTEXT_PARAM_NAME;
+
+	private static final String	CAPABILITY_URI					= "mock://user:pass@host.net:2212/mocksubsystem";
+	private static final String	RESOURCE_URI					= "mock://user:pass@host.net:2212/mocksubsystem";
+
+	private static final String	SDN_ACTIONSET_NAME				= "internal";
+	private static final String	SDN_ACTIONSET_VERSION			= "1.0";
+
+	private static final String	SDN_RESOURCE_NAME				= "sdnNetwork";
+	private static final String	SDNNETWORK_RESOURCE_TYPE		= "sdnnetwork";
+
+	@Inject
+	private IProtocolManager	protocolManager;
+
 	@Inject
 	private IResourceManager	resourceManager;
 
 	@Inject
-	private INCLController		nclController;
-
-	@Inject
 	private INCLProvisioner		provisioner;
-
-	@Inject
-	@Filter("(osgi.blueprint.container.symbolicname=org.opennaas.extensions.ofertie.ncl)")
-	private BlueprintContainer	ofertieNCLBundleContainer;
-
-	@Inject
-	@Filter("(osgi.blueprint.container.symbolicname=org.opennaas.extensions.openflowswitch)")
-	private BlueprintContainer	ofSwitchBundleContainer;
-
-	@Inject
-	@Filter("(osgi.blueprint.container.symbolicname=org.opennaas.extensions.openflowswitch.driver.floodlight)")
-	private BlueprintContainer	floodlightDriverBundleContainer;
 
 	@Configuration
 	public static Option[] configuration() {
 		return options(opennaasDistributionConfiguration(),
-				includeFeatures("opennaas-ofertie-ncl"),
+				includeFeatures("opennaas-ofertie-ncl", "opennaas-openflow-switch", "opennaas-openflow-switch-driver-floodlight"),
 				includeTestHelper(),
 				noConsole(),
 				keepRuntimeFolder());
 	}
 
-	@Before
-	public void startResources() {
-
-	}
-
 	@Test
-	public void test() {
+	public void test() throws ResourceException, ProtocolException {
+		createSwitches();
+		createSDNNetwork();
 
 	}
 
+	private void createSwitches() throws ResourceException, ProtocolException {
+		createSwitch(switch3, SWITCH_3_ID);
+		createSwitch(switch4, SWITCH_4_ID);
+		createSwitch(switch5, SWITCH_5_ID);
+		createSwitch(switch6, SWITCH_6_ID);
+		createSwitch(switch7, SWITCH_7_ID);
+		createSwitch(switch8, SWITCH_8_ID);
+	}
+
+	private void createSDNNetwork() throws ResourceException {
+		List<CapabilityDescriptor> lCapabilityDescriptors = new ArrayList<CapabilityDescriptor>();
+
+		CapabilityDescriptor provisionCapab = ResourceHelper.newCapabilityDescriptor(SDN_ACTIONSET_NAME,
+				SDN_ACTIONSET_VERSION, OFProvisioningNetworkCapability.CAPABILITY_TYPE, CAPABILITY_URI);
+
+		lCapabilityDescriptors.add(provisionCapab);
+
+		ResourceDescriptor resourceDescriptor = ResourceHelper.newResourceDescriptor(lCapabilityDescriptors, SDNNETWORK_RESOURCE_TYPE,
+				RESOURCE_URI, SDN_RESOURCE_NAME);
+
+		sdnNetResource = resourceManager.createResource(resourceDescriptor);
+
+		resourceManager.startResource(sdnNetResource.getResourceIdentifier());
+
+	}
+
+	private void createSwitch(IResource switchResource, String switchId) throws ResourceException, ProtocolException {
+		List<CapabilityDescriptor> lCapabilityDescriptors = new ArrayList<CapabilityDescriptor>();
+
+		CapabilityDescriptor ofForwardingDescriptor = ResourceHelper.newCapabilityDescriptor(FLOODLIGHT_ACTIONSET_NAME,
+				FLOODLIGHT_ACTIONSET_VERSION, OpenflowForwardingCapability.CAPABILITY_TYPE, CAPABILITY_URI);
+		lCapabilityDescriptors.add(ofForwardingDescriptor);
+
+		lCapabilityDescriptors.add(ofForwardingDescriptor);
+
+		// OFSwitch Resource Descriptor
+		ResourceDescriptor resourceDescriptor = ResourceHelper.newResourceDescriptor(lCapabilityDescriptors, OFSWITCH_RESOURCE_TYPE,
+				RESOURCE_URI, switchId);
+
+		switchResource = resourceManager.createResource(resourceDescriptor);
+
+		Map<String, Object> sessionParameters = new HashMap<String, Object>();
+		sessionParameters.put(SWITCH_ID_NAME, switchId);
+
+		// If not exists the protocol session manager, it's created and add the session context
+		InitializerTestHelper.addSessionContextWithSessionParams(protocolManager, switchResource.getResourceIdentifier().getId(), RESOURCE_URI,
+				FLOODLIGHT_PROTOCOL, sessionParameters);
+
+		// Start resource
+		resourceManager.startResource(switchResource.getResourceIdentifier());
+
+		prepareClientForSwitch(switchResource);
+
+	}
+
+	private void prepareClientForSwitch(IResource switchResource) throws ProtocolException {
+
+		IProtocolSessionManager sessionManager = protocolManager.getProtocolSessionManager(switchResource.getResourceIdentifier().getId());
+
+		FloodlightProtocolSession session = (FloodlightProtocolSession) sessionManager.obtainSessionByProtocol(
+				FloodlightProtocolSession.FLOODLIGHT_PROTOCOL_TYPE, false);
+
+		session.setClientFactory(new FloodlightMockClientFactory());
+		IFloodlightStaticFlowPusherClient client = session.getClientFactory().createClient(session.getSessionContext());
+		session.setFloodlightClient(client);
+
+	}
 }
