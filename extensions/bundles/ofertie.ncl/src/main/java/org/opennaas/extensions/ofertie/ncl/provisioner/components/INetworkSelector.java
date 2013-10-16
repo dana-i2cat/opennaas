08cf28ecfdd5289fe858c6621babdf6549c1f3b9
isartcanyameres <isart.canyameres@i2cat.net>
2013-10-16 18:03:18 +0200

    Implement deallocate method in the NCL Provisioner. OPENNAAS-1139

--- extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/provisioner/components/INetworkSelector.java
+++ extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/provisioner/components/INetworkSelector.java
@@ -4,6 +4,22 @@ import org.opennaas.extensions.ofertie.ncl.provisioner.api.model.FlowRequest;
 
 public interface INetworkSelector {
 
+	/**
+	 * Finds a network able to allocate given flowRequest
+	 * 
+	 * @param flowRequest
+	 * @return id of a network able to allocate given flowRequest
+	 * @throws Exception
+	 */
 	public String findNetworkForRequest(FlowRequest flowRequest) throws Exception;
+
+	/**
+	 * Finds the network containing a flow with given flowId
+	 * 
+	 * @param flowId
+	 * @return id of the network containing a flow with given flowId
+	 * @throws Exception
+	 */
+	public String findNetworkForFlowId(String flowId) throws Exception;
 
 }
