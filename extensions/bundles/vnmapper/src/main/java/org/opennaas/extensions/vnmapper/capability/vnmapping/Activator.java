a23c6c66408b7d02b9556f4af3da13b9e4e1ec4b
adrian-rosello <adrian.rosello@i2cat.net>
2013-10-30 12:31:32 +0100

    Vnmapper feature and remove unnecessary information from bundle.

--- extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/capability/vnmapping/Activator.java
+++ extensions/bundles/vnmapper/src/main/java/org/opennaas/extensions/vnmapper/capability/vnmapping/Activator.java
@@ -1,19 +1,12 @@
 package org.opennaas.extensions.vnmapper.capability.vnmapping;
 
-import java.util.Properties;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opennaas.core.resources.AbstractActivator;
 import org.opennaas.core.resources.ActivatorException;
 import org.opennaas.core.resources.IResourceManager;
-import org.opennaas.core.resources.action.IActionSet;
-import org.opennaas.core.resources.descriptor.ResourceDescriptorConstants;
-import org.opennaas.extensions.queuemanager.IQueueManagerCapability;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Filter;
-import org.osgi.framework.InvalidSyntaxException;
 
 /**
  * 
@@ -47,73 +40,6 @@ public class Activator extends AbstractActivator implements BundleActivator {
 	 */
 	public void stop(BundleContext context) throws Exception {
 
-	}
-
-	/**
-	 * Get the Queue Manager Service
-	 * 
-	 * @param resourceId
-	 * @return IQueueManagerCapability
-	 * @throws ActivatorException
-	 */
-	public static IQueueManagerCapability getQueueManagerService(String resourceId) throws ActivatorException {
-
-		try {
-			log.debug("Calling QueueManagerService");
-			return (IQueueManagerCapability) getServiceFromRegistry(context, createFilterQueueManager(resourceId));
-		} catch (InvalidSyntaxException e) {
-			throw new ActivatorException(e);
-		}
-	}
-
-	/**
-	 * Necessary to get some capability type
-	 * 
-	 * @param resourceId
-	 * @return Filter
-	 * @throws InvalidSyntaxException
-	 */
-	protected static Filter createFilterQueueManager(String resourceId) throws InvalidSyntaxException {
-
-		Properties properties = new Properties();
-		properties.setProperty(ResourceDescriptorConstants.CAPABILITY, "queue");
-		properties.setProperty(ResourceDescriptorConstants.CAPABILITY_NAME, resourceId);
-		return createServiceFilter(IQueueManagerCapability.class.getName(), properties);
-	}
-
-	/**
-	 * Get the connections action set service
-	 * 
-	 * @param name
-	 * @param version
-	 * @return IActionSet
-	 * @throws ActivatorException
-	 */
-	public static IActionSet getExampleActionSetService(String name, String version) throws ActivatorException {
-
-		try {
-			log.debug("Calling VNMappingActionSetService");
-			return (IActionSet) getServiceFromRegistry(context, createFilterExampleActionSet(name, version));
-		} catch (InvalidSyntaxException e) {
-			throw new ActivatorException(e);
-		}
-	}
-
-	/**
-	 * Necessary to get some capability type
-	 * 
-	 * @param name
-	 * @param version
-	 * @return Filter
-	 * @throws InvalidSyntaxException
-	 */
-	private static Filter createFilterExampleActionSet(String name, String version) throws InvalidSyntaxException {
-
-		Properties properties = new Properties();
-		properties.setProperty(ResourceDescriptorConstants.ACTION_CAPABILITY, "example");
-		properties.setProperty(ResourceDescriptorConstants.ACTION_NAME, name);
-		properties.setProperty(ResourceDescriptorConstants.ACTION_VERSION, version);
-		return createServiceFilter(IActionSet.class.getName(), properties);
 	}
 
 	public static IResourceManager getResourceManagerService() throws ActivatorException {
