e45b45c4345e8a4c735589abf85987e4b13047e4
isartcanyameres <isart.canyameres@i2cat.net>
2013-11-29 13:48:29 +0100

    NCLController accepts SDNNetworkFlow as parameter in allocateFlow().
    NCLController API and implementation has changed in order to use SDNNetworkFlows instead of FlowRequests.
    - Logic for Tranlating from FlowRequest to SDNNetworkFlow has been moved to a dedicated component (RequestToFlowLogic) that is part of the provisioner.
    - Logic for creating Flow from already allocated SDNNetworkFlow(s) has been moved to NCLProvisioner.
    - NCLController is now very similar to SDNNetwork IOFProvisioningNetworkCapability.
    
    Some tests have been updated.
    
    This patch fixes issue:
    http://jira.i2cat.net:8080/browse/OPENNAAS-1182

---
+++ extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/provisioner/components/mockup/RequestToFlowLogic.java
@@ -0,0 +1,28 @@
+package org.opennaas.extensions.ofertie.ncl.provisioner.components.mockup;
+
+import org.opennaas.extensions.ofertie.ncl.helpers.FlowRequestParser;
+import org.opennaas.extensions.ofertie.ncl.provisioner.api.model.FlowRequest;
+import org.opennaas.extensions.ofertie.ncl.provisioner.components.IRequestToFlowsLogic;
+import org.opennaas.extensions.sdnnetwork.model.Route;
+import org.opennaas.extensions.sdnnetwork.model.SDNNetworkOFFlow;
+
+public class RequestToFlowLogic implements IRequestToFlowsLogic {
+
+	public static final String	DEFAULT_FLOW_PRIORITY	= "32000";
+
+	@Override
+	public SDNNetworkOFFlow getRequiredFlowsToSatisfyRequest(FlowRequest flowRequest, Route route) {
+
+		SDNNetworkOFFlow flowWithRoute = FlowRequestParser.parseFlowRequestIntoSDNFlow(flowRequest, route);
+		flowWithRoute.setActive(true);
+		flowWithRoute.setPriority(DEFAULT_FLOW_PRIORITY);
+		// FIXME requesting a flow that won't filter by IP, by now
+		flowWithRoute.getMatch().setSrcIp(null);
+		flowWithRoute.getMatch().setDstIp(null);
+		// FIXME requesting a flow that won't filter by ToS, by now
+		flowWithRoute.getMatch().setTosBits(null);
+
+		return flowWithRoute;
+	}
+
+}
