4971b1980ff77f30f7087134d54dff01a11c9ba9
logoff <juliocarlos.barrera@i2cat.net>
2013-12-19 10:21:25 +0100

    Statistics Poller added and configured.

--- extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/monitoring/NCLMonitoring.java
+++ extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/monitoring/NCLMonitoring.java
@@ -3,6 +3,8 @@ package org.opennaas.extensions.ofertie.ncl.monitoring;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Timer;
+import java.util.TimerTask;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
@@ -22,10 +24,11 @@ import org.opennaas.extensions.ofnetwork.events.LinkCongestionEvent;
  */
 public class NCLMonitoring {
 
-	private static final Log	log						= LogFactory.getLog(NCLMonitoring.class);
+	private static final Log	log							= LogFactory.getLog(NCLMonitoring.class);
 
-	private static final String	NCL_MONITORING_FILE_ID	= "org.ofertie.ncl.monitoring";
-	private static final String	THROUGHPUT_THRESHOLD	= "throughput_threshold";
+	private static final String	NCL_MONITORING_FILE_ID		= "org.ofertie.ncl.monitoring";
+	private static final String	THROUGHPUT_THRESHOLD		= "throughput_threshold";
+	private static final String	STATISCS_POLLER_FREQUENCY	= "statistics_poller_freq";
 
 	private IEventManager		eventManager;
 
@@ -34,20 +37,20 @@ public class NCLMonitoring {
 	private NCLModel			nclModel;
 
 	private String				bandwidthThreshold;
+	private int					statisticsPollerFreq;
 
-	public NCLMonitoring() {
+	public void init() {
 		log.info("Initializing Ofertie NCL monitoring...");
-		initilize();
-		log.info("Ofertie NCL monitoring initialized");
-	}
 
-	private void initilize() {
-		// get configuration file
+		// get configuration file properties
 		ConfigurationAdminUtil configurationAdmin = new ConfigurationAdminUtil(Activator.getBundleContext());
 		try {
 			bandwidthThreshold = configurationAdmin.getProperty(NCL_MONITORING_FILE_ID, THROUGHPUT_THRESHOLD);
+			statisticsPollerFreq = Integer.parseInt(configurationAdmin.getProperty(NCL_MONITORING_FILE_ID, STATISCS_POLLER_FREQUENCY));
 		} catch (IOException e) {
 			log.error("Error getting configuration!", e);
+		} catch (NumberFormatException e) {
+			log.error("Number format error getting statistics_poller_freq configuration property!", e);
 		}
 
 		// get event manager service
@@ -56,6 +59,13 @@ public class NCLMonitoring {
 		} catch (ActivatorException e) {
 			log.error("Error getting Eent Manager Service!", e);
 		}
+
+		// initialize statistics poller
+		TimerTask statisticsPoller = new StatisticsPoller();
+		Timer timer = new Timer("NCL Monitoring statistics poller", true);
+		timer.scheduleAtFixedRate(statisticsPoller, 0, statisticsPollerFreq * 1000);
+
+		log.info("Ofertie NCL monitoring initialized");
 	}
 
 	private void notifyCongestion(String switchId, int portId) {
@@ -81,6 +91,18 @@ public class NCLMonitoring {
 
 	public void setNclModel(NCLModel nclModel) {
 		this.nclModel = nclModel;
+	}
+
+	/**
+	 * Thread class to execute the periodic poller to get the statistics from the network
+	 */
+	private class StatisticsPoller extends TimerTask {
+
+		@Override
+		public void run() {
+			log.info("\n\nERASE ME!!! Plloignstatistics!!\n\n");
+		}
+
 	}
 
 }
