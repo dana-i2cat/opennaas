259b03a3df82290127fbcd2b820dac3863fb3a16
adrian-rosello <adrian.rosello@i2cat.net>
2014-01-14 11:18:19 +0100

    Add apache_v2 license to extensions.

--- extensions/bundles/router.model/src/main/java/org/opennaas/extensions/router/model/PolicyActionInPolicyRule.java
+++ extensions/bundles/router.model/src/main/java/org/opennaas/extensions/router/model/PolicyActionInPolicyRule.java
@@ -1,5 +1,26 @@
 package org.opennaas.extensions.router.model;
 
+/*
+ * #%L
+ * OpenNaaS :: CIM Model
+ * %%
+ * Copyright (C) 2007 - 2014 Fundació Privada i2CAT, Internet i Innovació a Catalunya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 /**
  * A PolicyRule aggregates zero or more instances of the PolicyAction class, via the PolicyActionInPolicyRule association. A Rule that aggregates zero
  * Actions is not valid--it may, however, be in the process of being entered into a PolicyRepository or being defined for a System. Alternately, the
--- extensions/bundles/router.model/src/main/java/org/opennaas/extensions/router/model/PolicySetComponent.java
+++ extensions/bundles/router.model/src/main/java/org/opennaas/extensions/router/model/PolicySetComponent.java
@@ -1,5 +1,26 @@
 package org.opennaas.extensions.router.model;
 
+/*
+ * #%L
+ * OpenNaaS :: CIM Model
+ * %%
+ * Copyright (C) 2007 - 2014 Fundació Privada i2CAT, Internet i Innovació a Catalunya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+
 /**
  * PolicySetComponent is a concrete aggregation that collects instances of the subclasses of PolicySet (i.e., PolicyGroups and PolicyRules). Instances
  * are collected in sets that use the same decision strategy. They are prioritized relative to each other, within the set, using the Priority property
