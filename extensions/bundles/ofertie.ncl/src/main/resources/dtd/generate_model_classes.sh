fd0b5cfbdabe4e6afa2430e6980e0f44765ce6c2
logoff <juliocarlos.barrera@i2cat.net>
2014-01-14 11:26:04 +0100

    Move generated classes to correct API package.

--- extensions/bundles/ofertie.ncl/src/main/resources/dtd/generate_model_classes.sh
+++ extensions/bundles/ofertie.ncl/src/main/resources/dtd/generate_model_classes.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-xjc -dtd -d ../../java/ -p org.opennaas.extensions.ofertie.ncl.provisioner.model.qos qos_policy_request.dtd
-xjc -dtd -d ../../java/ -p org.opennaas.extensions.ofertie.ncl.provisioner.model.monitoring qos_monitoring.dtd
+xjc -dtd -d ../../java/ -p org.opennaas.extensions.ofertie.ncl.provisioner.api.model qos_policy_request.dtd
+xjc -dtd -d ../../java/ -p org.opennaas.extensions.ofertie.ncl.monitoring.api.model qos_monitoring.dtd
