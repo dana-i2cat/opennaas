0e64528ce3b78aadb2567ccfa4aff4067f1c740a
logoff <juliocarlos.barrera@i2cat.net>
2014-01-15 15:51:59 +0100

    Genearte Java classes using Maven plugin and DTDs files.

--- extensions/bundles/ofertie.ncl/src/main/resources/dtd/generate_model_classes.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-xjc -dtd -d ../../java/ -p org.opennaas.extensions.ofertie.ncl.provisioner.api.model qos_policy_request.dtd
-xjc -dtd -d ../../java/ -p org.opennaas.extensions.ofertie.ncl.monitoring.api.model qos_monitoring.dtd
