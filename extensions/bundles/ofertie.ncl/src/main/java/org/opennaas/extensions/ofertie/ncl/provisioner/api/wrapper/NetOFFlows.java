6c21217438c7cd03024d410d0bab013650fc928b
logoff <juliocarlos.barrera@i2cat.net>
2014-01-20 12:47:11 +0100

    Add equals and hashCode methods.

--- extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/provisioner/api/wrapper/NetOFFlows.java
+++ extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/provisioner/api/wrapper/NetOFFlows.java
@@ -37,4 +37,29 @@ public class NetOFFlows {
 		this.list = list;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((list == null) ? 0 : list.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		NetOFFlows other = (NetOFFlows) obj;
+		if (list == null) {
+			if (other.list != null)
+				return false;
+		} else if (!list.equals(other.list))
+			return false;
+		return true;
+	}
+
 }
