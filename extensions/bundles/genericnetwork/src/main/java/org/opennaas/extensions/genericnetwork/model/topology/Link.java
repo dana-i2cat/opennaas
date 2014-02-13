61784e82b6e722ff2ccd5077308d17ce33eee37a
isartcanyameres <isart.canyameres@i2cat.net>
2014-02-13 10:40:29 +0100

    Link is no longer an XmlRootElement as it uses XmlIDREFs.

--- extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/model/topology/Link.java
+++ extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/model/topology/Link.java
@@ -3,14 +3,12 @@ package org.opennaas.extensions.genericnetwork.model.topology;
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlIDREF;
-import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * 
  * @author Isart Canyameres Gimenez (i2cat)
  * 
  */
-@XmlRootElement(namespace = "opennaas.api")
 @XmlAccessorType(XmlAccessType.FIELD)
 public class Link extends TopologyElement {
 
