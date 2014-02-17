8499b6ab6a907e87076f260f2cdf8394ecf36f22
isartcanyameres <isart.canyameres@i2cat.net>
2014-02-17 16:26:10 +0100

    Define NetTopologyInternalActionset like other actionsets in same bundle.
    This patch:
    - moves LoadTopologyAction to a different package
    - defines NetTopologyInternalActionSetImplementation
    - and publishes the actionset as an OSGi service using blueprint.

--- extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/actionsets/internal/nettopology/actions/LoadTopologyAction.java
+++ extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/actionsets/internal/nettopology/actions/LoadTopologyAction.java
@@ -1,4 +1,4 @@
-package org.opennaas.extensions.genericnetwork.driver.internal.actionsets.actions.nettopology;
+package org.opennaas.extensions.genericnetwork.actionsets.internal.nettopology.actions;
 
 /*
  * #%L
