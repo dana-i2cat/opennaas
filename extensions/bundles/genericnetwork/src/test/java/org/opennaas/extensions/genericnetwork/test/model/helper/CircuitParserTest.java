19a10249fe374746954c9a8165de335103a7c7ac
adrian-rosello <adrian.rosello@i2cat.net>
2014-02-18 18:01:47 +0100

    Typo error.

--- extensions/bundles/genericnetwork/src/test/java/org/opennaas/extensions/genericnetwork/test/model/helper/CircuitParserTest.java
+++ extensions/bundles/genericnetwork/src/test/java/org/opennaas/extensions/genericnetwork/test/model/helper/CircuitParserTest.java
@@ -1,5 +1,25 @@
 package org.opennaas.extensions.genericnetwork.test.model.helper;
 
+/*
+ * #%L
+ * OpenNaaS :: Generic Network
+ * %%
+ * Copyright (C) 2007 - 2014 Fundació Privada i2CAT, Internet i Innovació a Catalunya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.apache.commons.lang.StringUtils;
 import org.junit.Assert;
 import org.junit.Test;
@@ -65,12 +85,12 @@ public class CircuitParserTest {
 		QoSPolicy qos = circuit.getQos();
 		Assert.assertEquals(JITTER_MIN, String.valueOf(qos.getMinJitter()));
 		Assert.assertEquals(JITTER_MAX, String.valueOf(qos.getMaxJitter()));
-		Assert.assertEquals(LATENCY_MIN, String.valueOf(qos.getMinLantency()));
+		Assert.assertEquals(LATENCY_MIN, String.valueOf(qos.getMinLatency()));
 		Assert.assertEquals(LATENCY_MAX, String.valueOf(qos.getMaxLatency()));
 		Assert.assertEquals(THROUGHPUT_MIN, String.valueOf(qos.getMinThroughput()));
 		Assert.assertEquals(THROUGHPUT_MAX, String.valueOf(qos.getMaxThroughput()));
 		Assert.assertEquals(PACKET_LOSS_MIN, String.valueOf(qos.getMinPacketLoss()));
-		Assert.assertEquals(PACKET_LOSS_MAX, String.valueOf(qos.getMaxPaquetLoss()));
+		Assert.assertEquals(PACKET_LOSS_MAX, String.valueOf(qos.getMaxPacketLoss()));
 
 	}
 
