9057d418d536eed14aa84343e111cd48d8d9e879
adrian-rosello <adrian.rosello@i2cat.net>
2014-02-19 11:20:42 +0100

    Add hashcode and equals to CircuitCollection.

--- extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/capability/nclprovisioner/api/CircuitCollection.java
+++ extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/capability/nclprovisioner/api/CircuitCollection.java
@@ -1,5 +1,25 @@
 package org.opennaas.extensions.genericnetwork.capability.nclprovisioner.api;
 
+/*
+ * #%L
+ * OpenNaaS :: Generic Network
+ * %%
+ * Copyright (C) 2007 - 2014 Fundació Privada i2CAT, Internet i Innovació a Catalunya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.Serializable;
 import java.util.Collection;
 
@@ -33,6 +53,31 @@ public class CircuitCollection implements Serializable {
 
 	public void setCircuits(Collection<Circuit> circuits) {
 		this.circuits = circuits;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((circuits == null) ? 0 : circuits.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CircuitCollection other = (CircuitCollection) obj;
+		if (circuits == null) {
+			if (other.circuits != null)
+				return false;
+		} else if (!circuits.equals(other.circuits))
+			return false;
+		return true;
 	}
 
 }
