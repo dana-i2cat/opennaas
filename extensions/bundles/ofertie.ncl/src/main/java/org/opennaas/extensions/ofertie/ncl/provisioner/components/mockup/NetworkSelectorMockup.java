d22572ba4b404420e223b84186b0368ad142c4f1
adrian-rosello <adrian.rosello@i2cat.net>
2014-02-21 10:53:08 +0100

    Change NetworkSelector logic to load network id from file.

--- extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/provisioner/components/mockup/NetworkSelectorMockup.java
+++ extensions/bundles/ofertie.ncl/src/main/java/org/opennaas/extensions/ofertie/ncl/provisioner/components/mockup/NetworkSelectorMockup.java
@@ -20,14 +20,50 @@ package org.opennaas.extensions.ofertie.ncl.provisioner.components.mockup;
  * #L%
  */
 
+import java.io.IOException;
+
+import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.opennaas.core.resources.IResourceManager;
+import org.opennaas.core.resources.ResourceException;
+import org.opennaas.core.resources.configurationadmin.ConfigurationAdminUtil;
+import org.opennaas.extensions.ofertie.ncl.Activator;
 import org.opennaas.extensions.ofertie.ncl.provisioner.api.model.QosPolicyRequest;
 import org.opennaas.extensions.ofertie.ncl.provisioner.components.INetworkSelector;
-import org.opennaas.extensions.ofnetwork.repository.OFNetworkRepository;
 
 public class NetworkSelectorMockup implements INetworkSelector {
 
+	private static final Log	log	= LogFactory.getLog(NetworkSelectorMockup.class);
+
 	private IResourceManager	resourceManager;
+	private String				resourceType;
+	private String				resourceName;
+
+	public String getNetworkType() {
+		return resourceType;
+	}
+
+	private static final String	RESOURCE_ID_FILE	= "org.ofertie.ncl.network";
+	private static final String	RESOURCE_NAME_KEY	= "resource.name";
+	private static final String	RESOURCE_TYPE_KEY	= "resource.type";
+
+	public void init() throws IOException, ConfigurationException {
+
+		log.debug("Reading configuration file " + RESOURCE_ID_FILE);
+
+		ConfigurationAdminUtil configurationAdmin = new ConfigurationAdminUtil(Activator.getBundleContext());
+
+		resourceName = configurationAdmin.getProperty(RESOURCE_ID_FILE, RESOURCE_NAME_KEY);
+		resourceType = configurationAdmin.getProperty(RESOURCE_ID_FILE, RESOURCE_TYPE_KEY);
+
+		if (StringUtils.isEmpty(resourceName) || StringUtils.isEmpty(resourceType))
+			throw new ConfigurationException(RESOURCE_ID_FILE + " file should contain attributes " + RESOURCE_NAME_KEY + " and " + RESOURCE_TYPE_KEY);
+
+		log.info("NetworkSelector initalized with configured resource " + resourceType + ":" + resourceName);
+
+	}
 
 	/**
 	 * @return the resourceManager
@@ -44,25 +80,41 @@ public class NetworkSelectorMockup implements INetworkSelector {
 		this.resourceManager = resourceManager;
 	}
 
+	public String getResourceType() {
+		return resourceType;
+	}
+
+	public void setResourceType(String resourceType) {
+		this.resourceType = resourceType;
+	}
+
+	public String getResourceName() {
+		return resourceName;
+	}
+
+	public void setResourceName(String resourceName) {
+		this.resourceName = resourceName;
+	}
+
 	@Override
 	public String findNetworkForRequest(QosPolicyRequest qosPolicyRequest)
 			throws Exception {
-		return getFirstSDNNetworkInOpenNaaS();
+		return getConfiguredNetwork();
 	}
 
 	@Override
 	public String findNetworkForFlowId(String flowId) throws Exception {
-		return getFirstSDNNetworkInOpenNaaS();
+		return getConfiguredNetwork();
 	}
 
 	/**
-	 * It assumes OpenNaaS supports SDN networks and there is at least one of them active.
+	 * Returns the id of the resource identified by the resourceType and resourceName set in the configuration file.
 	 * 
 	 * @return
+	 * @throws ResourceException
 	 */
-	private String getFirstSDNNetworkInOpenNaaS() {
-		return resourceManager.listResourcesByType(OFNetworkRepository.OF_NETWORK_RESOURCE_TYPE).get(0)
-				.getResourceIdentifier().getId();
+	private String getConfiguredNetwork() throws ResourceException {
+		return resourceManager.getIdentifierFromResourceTypeName(resourceType, resourceName);
 	}
 
 }
