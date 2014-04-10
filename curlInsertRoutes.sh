b97317e131c04189107cad6307b20e7e42ba2071
Josep <jbatalle3@gmail.com>
2014-04-10 16:06:14 +0200

    Script to insert static routes given files

--- curlInsertRoutes.sh
+++ curlInsertRoutes.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 echo "Insert flows curl..."
 #response=$(curl -H "Content-Type: application/octet-stream" -H "Accept: text/plain" -X POST http://admin:123456@localhost:8888/opennaas/vrf/routing/insertRouteFromFile/vrf.json)
-response=$(curl -H "Content-Type: application/json"  -H "Accept: text/plain" -d @vrf.json http://admin:123456@localhost:8888/opennaas/vrf/staticrouting/insertRouteFromFile/vrf)
+file=vrf.json
+if [ ! -z "$1" ]
+	then
+		file="$1"
+fi
+echo $file
+response=$(curl -H "Content-Type: application/json"  -H "Accept: text/plain" -d @$file http://admin:123456@localhost:8888/opennaas/vrf/staticrouting/insertRouteFromFile/vrf)
 echo $response
 echo "Insert done..."
