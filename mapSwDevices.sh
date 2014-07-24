abf60e9eda89cacbaae6fffe0d2ef1a8e8b5404d
Josep <jbatalle3@gmail.com>
2014-07-24 12:45:51 +0200

    Removed scripts

--- mapSwDevices.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#requires the dpid and resourceId {$1}/{$2}
-dpid=$1
-resourceId=$2
-echo "Mapping openflow switches to OpenNaaS resource."
-response=$(curl -H "Content-Type: application/xml" -X GET http://admin:123456@localhost:8888/opennaas/sdnnetwork/sdn1/ofprovisionnet/mapDeviceResource/$dpid/$resourceId)
-echo $response
