dd90fccae8df3be8684b1e5beec553a7dfbd86cd
adrian-rosello <adrian.rosello@i2cat.net>
2014-10-07 12:30:16 +0200

    CircuitStatisticsCapability skeleton.

---
+++ extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/capability/circuitstatistics/CircuitStatisticsCapability.java
@@ -0,0 +1,76 @@
+package org.opennaas.extensions.genericnetwork.capability.circuitstatistics;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.opennaas.core.resources.action.IAction;
+import org.opennaas.core.resources.action.IActionSet;
+import org.opennaas.core.resources.capability.AbstractCapability;
+import org.opennaas.core.resources.capability.CapabilityException;
+import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
+import org.opennaas.extensions.genericnetwork.Activator;
+
+/**
+ * 
+ * @author Adrián Roselló Rey (i2CAT)
+ *
+ */
+public class CircuitStatisticsCapability extends AbstractCapability implements ICircuitStatisticsCapability {
+
+	public static final String	CAPABILITY_TYPE	= "circuitstatistics";
+
+	private Log					log				= LogFactory.getLog(CircuitStatisticsCapability.class);
+
+	private String				resourceId		= "";
+
+	public CircuitStatisticsCapability(CapabilityDescriptor descriptor, String resourceId) {
+		super(descriptor);
+		this.resourceId = resourceId;
+		log.debug("Built new Circuit Statistics Capability");
+	}
+
+	@Override
+	public String getCapabilityName() {
+		return this.CAPABILITY_TYPE;
+	}
+
+	@Override
+	public void activate() throws CapabilityException {
+		registerService(Activator.getContext(), CAPABILITY_TYPE, getResourceType(), getResourceName(),
+				CircuitStatisticsCapability.class.getName());
+		super.activate();
+	}
+
+	@Override
+	public void deactivate() throws CapabilityException {
+		unregisterService();
+		super.deactivate();
+	}
+
+	@Override
+	public void queueAction(IAction action) throws CapabilityException {
+		throw new UnsupportedOperationException("Not Implemented. This capability is not using the queue.");
+	}
+
+	@Override
+	public IActionSet getActionSet() throws CapabilityException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	// ############################################
+	// ### ICircuitStatisticsCapability methods ###
+	// ############################################
+
+	@Override
+	public void reportStatistics(String csvStatistics) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public String getStatistics() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/capability/circuitstatistics/ICircuitStatisticsCapability.java
@@ -0,0 +1,28 @@
+package org.opennaas.extensions.genericnetwork.capability.circuitstatistics;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.opennaas.core.resources.capability.ICapability;
+
+/**
+ * 
+ * @author Adrián Roselló Rey (i2CAT)
+ *
+ */
+@Path("/")
+public interface ICircuitStatisticsCapability extends ICapability {
+
+	@PUT
+	@Consumes(MediaType.TEXT_PLAIN)
+	public void reportStatistics(String csvStatistics);
+
+	@GET
+	@Produces(MediaType.TEXT_PLAIN)
+	public String getStatistics();
+
+}
