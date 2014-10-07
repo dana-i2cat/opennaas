c15eb125a8f53080dec049d52b24a933235942dd
adrian-rosello <adrian.rosello@i2cat.net>
2014-10-07 18:00:33 +0200

    GetCircuitStatistics implementation and features for openCSV.
    Conflicts:
    	extensions/bundles/genericnetwork/pom.xml
    	pom.xml

--- extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/capability/circuitstatistics/CircuitStatisticsCapability.java
+++ extensions/bundles/genericnetwork/src/main/java/org/opennaas/extensions/genericnetwork/capability/circuitstatistics/CircuitStatisticsCapability.java
@@ -1,11 +1,32 @@
 package org.opennaas.extensions.genericnetwork.capability.circuitstatistics;
 
+/*
+ * #%L
+ * OpenNaaS :: Generic Network
+ * %%
+ * Copyright (C) 2007 - 2014 Fundació Privada i2CAT, Internet i Innovació a Catalunya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.IOException;
 import java.io.StringReader;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+import org.apache.commons.io.output.StringBuilderWriter;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opennaas.core.resources.action.IAction;
@@ -19,6 +40,7 @@ import org.opennaas.extensions.genericnetwork.model.GenericNetworkModel;
 import org.opennaas.extensions.genericnetwork.model.TimePeriod;
 
 import au.com.bytecode.opencsv.CSVReader;
+import au.com.bytecode.opencsv.CSVWriter;
 
 /**
  * 
@@ -131,9 +153,45 @@ public class CircuitStatisticsCapability extends AbstractCapability implements I
 	}
 
 	@Override
-	public String getStatistics(TimePeriod timePeriod) {
-		// TODO Auto-generated method stub
-		return null;
+	public String getStatistics(TimePeriod timePeriod) throws CapabilityException {
+		try {
+			GenericNetworkModel model = (GenericNetworkModel) resource.getModel();
+			Set<CircuitStatistics> circuitStatistics = model.getCircuitStatistics();
+
+			String csvStatistics = writeToCSV(circuitStatistics);
+
+			return csvStatistics;
+		} catch (IOException io) {
+			log.error("Error parsing cirucir statistics to CSV.", io);
+			throw new CapabilityException(io);
+		}
 	}
 
+	private String writeToCSV(Set<CircuitStatistics> circuitStatistics) throws IOException {
+
+		StringBuilder sb = new StringBuilder();
+		CSVWriter writer = new CSVWriter(new StringBuilderWriter(sb), CSVWriter.DEFAULT_SEPARATOR, CSVWriter.NO_QUOTE_CHARACTER);
+		try {
+
+			for (CircuitStatistics currentStatistic : circuitStatistics) {
+
+				String[] csvStatistic = new String[8];
+				csvStatistic[0] = String.valueOf(currentStatistic.getTimePeriod().getStartTime());
+				csvStatistic[1] = String.valueOf(currentStatistic.getTimePeriod().getEndTime());
+				csvStatistic[2] = String.valueOf(currentStatistic.getSlaFlowId());
+				csvStatistic[3] = String.valueOf(currentStatistic.getThroughput());
+				csvStatistic[4] = String.valueOf(currentStatistic.getPacketLoss());
+				csvStatistic[5] = String.valueOf(currentStatistic.getDelay());
+				csvStatistic[6] = String.valueOf(currentStatistic.getJitter());
+				csvStatistic[7] = String.valueOf(currentStatistic.getFlowData());
+
+				writer.writeNext(csvStatistic);
+
+			}
+
+			return sb.toString();
+		} finally {
+			writer.close();
+		}
+	}
 }
--- extensions/bundles/genericnetwork/src/test/java/org/opennaas/extensions/genericnetwork/test/capability/circuitstatistics/CircuitStatisticsCapabilityTest.java
+++ extensions/bundles/genericnetwork/src/test/java/org/opennaas/extensions/genericnetwork/test/capability/circuitstatistics/CircuitStatisticsCapabilityTest.java
@@ -1,8 +1,30 @@
 package org.opennaas.extensions.genericnetwork.test.capability.circuitstatistics;
 
+/*
+ * #%L
+ * OpenNaaS :: Generic Network
+ * %%
+ * Copyright (C) 2007 - 2014 Fundació Privada i2CAT, Internet i Innovació a Catalunya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Set;
 
@@ -14,6 +36,7 @@ import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
 import org.opennaas.core.resources.descriptor.Information;
 import org.opennaas.extensions.genericnetwork.capability.circuitstatistics.CircuitStatisticsCapability;
 import org.opennaas.extensions.genericnetwork.model.CircuitStatistics;
+import org.opennaas.extensions.genericnetwork.model.TimePeriod;
 
 /**
  * 
@@ -25,22 +48,22 @@ public class CircuitStatisticsCapabilityTest {
 	private static final String		CIRCUIT_STATISTICS_CSV_URL	= "/circuitStatistics.csv";
 
 	private static final long		FIRST_CIRCUIT_START_TIME	= Long.valueOf("12378612378613").longValue();
-	private static final long		FIRST_CIRCUIT_END_TIME		= Long.valueOf("22378612378613").longValue();	;
+	private static final long		FIRST_CIRCUIT_END_TIME		= Long.valueOf("22378612378613").longValue();
 	private static final String		FIRST_CIRCUIT_FLOW_ID		= "1";
-	private static final Object		FIRST_CIRCUIT_THROUGHPUT	= "10";
+	private static final String		FIRST_CIRCUIT_THROUGHPUT	= "10";
 	private static final String		FIRST_CIRCUIT_PACKGET_LOSS	= "2";
 	private static final String		FIRST_CIRCUIT_DELAY			= "0";
 	private static final String		FIRST_CIRCUIT_JITTER		= "0";
-	private static final Object		FIRST_CIRCUIT_FLOW_DATA		= "1234";
+	private static final String		FIRST_CIRCUIT_FLOW_DATA		= "1234";
 
 	private static final long		SECOND_CIRCUIT_START_TIME	= Long.valueOf("12378612378614").longValue();
 	private static final long		SECOND_CIRCUIT_END_TIME		= Long.valueOf("32378912378123").longValue();
 	private static final String		SECOND_CIRCUIT_FLOW_ID		= "2";
 	private static final String		SECOND_CIRCUIT_THROUGHPUT	= "0";
-	private static final Object		SECOND_CIRCUIT_PACKGET_LOSS	= "1";
+	private static final String		SECOND_CIRCUIT_PACKGET_LOSS	= "1";
 	private static final String		SECOND_CIRCUIT_JITTER		= "1";
 	private static final String		SECOND_CIRCUIT_DELAY		= "1";
-	private static final Object		SECOND_CIRCUIT_FLOW_DATA	= "1233";
+	private static final String		SECOND_CIRCUIT_FLOW_DATA	= "1233";
 
 	private CapabilityDescriptor	capabilityDescriptor;
 
@@ -115,5 +138,47 @@ public class CircuitStatisticsCapabilityTest {
 		Assert.assertEquals("Delay of second circuit statistics should be " + SECOND_CIRCUIT_DELAY, SECOND_CIRCUIT_DELAY,
 				secondStatistics.getDelay());
 
+	}
+
+	@Test
+	public void writeToCSVTest() throws SecurityException, NoSuchMethodException, IllegalArgumentException, IllegalAccessException,
+			InvocationTargetException, IOException {
+
+		CircuitStatistics firstStatistics = generateCircuitStatistics(FIRST_CIRCUIT_START_TIME, FIRST_CIRCUIT_END_TIME, FIRST_CIRCUIT_FLOW_ID,
+				FIRST_CIRCUIT_THROUGHPUT, FIRST_CIRCUIT_PACKGET_LOSS, FIRST_CIRCUIT_DELAY, FIRST_CIRCUIT_JITTER, FIRST_CIRCUIT_FLOW_DATA);
+		CircuitStatistics secondStatistics = generateCircuitStatistics(SECOND_CIRCUIT_START_TIME, SECOND_CIRCUIT_END_TIME, SECOND_CIRCUIT_FLOW_ID,
+				SECOND_CIRCUIT_THROUGHPUT, SECOND_CIRCUIT_PACKGET_LOSS, SECOND_CIRCUIT_DELAY, SECOND_CIRCUIT_JITTER, SECOND_CIRCUIT_FLOW_DATA);
+
+		Set<CircuitStatistics> circuitStatistics = new HashSet<CircuitStatistics>();
+		circuitStatistics.addAll(Arrays.asList(firstStatistics, secondStatistics));
+
+		// call private method by reflection
+		Method method = circuitStatisticsCapab.getClass().getDeclaredMethod("writeToCSV", Set.class);
+		method.setAccessible(true);
+
+		@SuppressWarnings("unchecked")
+		String parsedCSV = (String) method.invoke(circuitStatisticsCapab, circuitStatistics);
+
+		String expectedCSV = IOUtils.toString(this.getClass().getResourceAsStream(CIRCUIT_STATISTICS_CSV_URL));
+
+		Assert.assertEquals("Parsed CSV should be equals to the expected one.", expectedCSV, parsedCSV);
+
+	}
+
+	private CircuitStatistics generateCircuitStatistics(long circuitStartTime, long circuitEndTime, String circuiSLAtFlowId,
+			String circuitThroughput, String circuitPackgetLoss, String circuitDelay, String circuitJitter, String circuitFlowData) {
+
+		TimePeriod timePeriod = new TimePeriod(circuitStartTime, circuitEndTime);
+		CircuitStatistics circuitStatistics = new CircuitStatistics();
+
+		circuitStatistics.setTimePeriod(timePeriod);
+		circuitStatistics.setSlaFlowId(circuiSLAtFlowId);
+		circuitStatistics.setThroughput(circuitThroughput);
+		circuitStatistics.setPacketLoss(circuitPackgetLoss);
+		circuitStatistics.setDelay(circuitDelay);
+		circuitStatistics.setJitter(circuitJitter);
+		circuitStatistics.setFlowData(circuitFlowData);
+
+		return circuitStatistics;
 	}
 }
