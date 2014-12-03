ac9623146d9124d78770c5e401064f9719741e9d
logoff <juliocarlos.barrera@i2cat.net>
2014-12-03 11:59:03 +0100

    Add JSON provider for client.

--- extensions/bundles/abno/src/main/java/org/opennaas/extensions/abno/client/ABNOClientFactory.java
+++ extensions/bundles/abno/src/main/java/org/opennaas/extensions/abno/client/ABNOClientFactory.java
@@ -1,8 +1,29 @@
 package org.opennaas.extensions.abno.client;
 
+/*
+ * #%L
+ * OpenNaaS :: XIFI ABNO
+ * %%
+ * Copyright (C) 2007 - 2014 Fundació Privada i2CAT, Internet i Innovació a Catalunya
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.apache.cxf.common.util.ProxyClassLoader;
 import org.apache.cxf.jaxrs.client.JAXRSClientFactoryBean;
 import org.opennaas.core.resources.protocol.ProtocolSessionContext;
+import org.opennaas.extensions.abno.client.json.ABNOJSONProvider;
 
 /**
  * {@link IABNOClient} factory
@@ -22,6 +43,7 @@ public class ABNOClientFactory {
 		classLoader.addLoader(JAXRSClientFactoryBean.class.getClassLoader());
 
 		JAXRSClientFactoryBean bean = new JAXRSClientFactoryBean();
+		bean.setProvider(new ABNOJSONProvider());
 		bean.setAddress(uri);
 		bean.setResourceClass(IABNOClient.class);
 		bean.setClassLoader(classLoader);
