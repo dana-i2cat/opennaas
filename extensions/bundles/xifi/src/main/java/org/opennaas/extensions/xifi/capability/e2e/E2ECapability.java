6aec0401d27bc5f6f3b06b7974b93a7512f0bf05
logoff <juliocarlos.barrera@i2cat.net>
2015-02-10 17:31:05 +0100

    Add XIFI configuration file processing.

--- extensions/bundles/xifi/src/main/java/org/opennaas/extensions/xifi/capability/e2e/E2ECapability.java
+++ extensions/bundles/xifi/src/main/java/org/opennaas/extensions/xifi/capability/e2e/E2ECapability.java
@@ -20,15 +20,22 @@ package org.opennaas.extensions.xifi.capability.e2e;
  * #L%
  */
 
+import java.io.IOException;
+import java.util.Map.Entry;
+import java.util.Properties;
+
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.opennaas.core.resources.action.IAction;
 import org.opennaas.core.resources.action.IActionSet;
 import org.opennaas.core.resources.capability.AbstractCapability;
 import org.opennaas.core.resources.capability.CapabilityException;
+import org.opennaas.core.resources.configurationadmin.ConfigurationAdminUtil;
 import org.opennaas.core.resources.descriptor.CapabilityDescriptor;
 import org.opennaas.extensions.xifi.Activator;
 import org.opennaas.extensions.xifi.capability.e2e.api.ConnectEndsRequest;
+import org.opennaas.extensions.xifi.capability.e2e.model.Region;
+import org.opennaas.extensions.xifi.capability.e2e.model.XIFIConfiguration;
 
 /**
  * XIFI {@link IE2ECapability} implementation
@@ -38,9 +45,19 @@ import org.opennaas.extensions.xifi.capability.e2e.api.ConnectEndsRequest;
  */
 public class E2ECapability extends AbstractCapability implements IE2ECapability {
 
-	private static final Log	log			= LogFactory.getLog(E2ECapability.class);
+	private static final Log	log						= LogFactory.getLog(E2ECapability.class);
 
-	private String				resourceId	= "";
+	private String				resourceId				= "";
+
+	// configuration file and properties
+	private static final String	XIFI_FILE				= "xifi.regions";
+	private static final String	ABNO_ENDPOINT			= "abno.endpoint";
+	private static final String	REGION					= "region";
+	private static final String	REGION_RYU				= ".ryu";
+	private static final String	REGION_OPENSTACK		= ".openstack";
+	private static final String	REGION_AUTOBAHN_IFACE	= ".autobahn.iface";
+
+	private XIFIConfiguration	xifiConfiguration;
 
 	public E2ECapability(CapabilityDescriptor descriptor, String resourceId) {
 		super(descriptor);
@@ -58,6 +75,15 @@ public class E2ECapability extends AbstractCapability implements IE2ECapability 
 	public void activate() throws CapabilityException {
 		registerService(Activator.getContext(), CAPABILITY_TYPE, getResourceType(), getResourceName(), E2ECapability.class.getName());
 		super.activate();
+
+		// read XIFI Configuration
+		try {
+			readAndCheckConfiguration();
+		} catch (Exception e) {
+			log.error("Error reading XIFI configuration.", e);
+			throw new CapabilityException(e);
+		}
+		log.info("XIFI configuration read:\n" + xifiConfiguration);
 	}
 
 	@Override
@@ -81,4 +107,65 @@ public class E2ECapability extends AbstractCapability implements IE2ECapability 
 		throw new UnsupportedOperationException("This capability does not contain actionset.");
 	}
 
+	private void readAndCheckConfiguration() throws Exception {
+		log.info("Reading XIFI configuration from file " + XIFI_FILE + "...");
+
+		xifiConfiguration = new XIFIConfiguration();
+
+		// get configuration file properties
+		ConfigurationAdminUtil configurationAdmin = new ConfigurationAdminUtil(Activator.getContext());
+		try {
+			Properties properties = ConfigurationAdminUtil.getProperties(Activator.getContext(), XIFI_FILE);
+
+			xifiConfiguration.setABNOEndpoint(configurationAdmin.getProperty(XIFI_FILE, ABNO_ENDPOINT));
+
+			for (Entry<Object, Object> entry : properties.entrySet()) {
+				String key = (String) entry.getKey();
+				log.trace("Analizing property: " + entry);
+
+				if (key.startsWith(REGION + ".") && key.lastIndexOf('.') == ((REGION + ".").length() - 1)) {
+
+					String regionName = (String) entry.getValue();
+					log.trace("Region found: " + regionName);
+
+					// create the Region
+					Region region = new Region();
+					region.setName(regionName);
+
+					String regionKey = key.substring((REGION + ".").length());
+
+					// fill fields
+					String ryuEndpoint = configurationAdmin.getProperty(XIFI_FILE, REGION + "." + regionKey + REGION_RYU);
+					region.setRyuEndpoint(ryuEndpoint);
+					String openStackEndpoint = configurationAdmin.getProperty(XIFI_FILE, REGION + "." + regionKey + REGION_OPENSTACK);
+					region.setOpenstackEndpoint(openStackEndpoint);
+					String autobahnIface = configurationAdmin.getProperty(XIFI_FILE, REGION + "." + regionKey + REGION_AUTOBAHN_IFACE);
+					region.setAutoBAHNInterface(autobahnIface);
+
+					// add the region
+					xifiConfiguration.getRegions().add(region);
+				}
+			}
+
+		} catch (IOException e) {
+			log.error("Error getting configuration!");
+			throw e;
+		}
+		log.info("XIFI configuration read.");
+
+		// check configuration
+		log.info("Checking XIFI configuration...");
+		if (xifiConfiguration.getAbnoEndpoint() == null)
+			throw new Exception("ABNO Endpoint not set!");
+		for (Region region : xifiConfiguration.getRegions()) {
+			if (region.getName() == null)
+				throw new Exception("Region name not set!");
+			if (region.getRyuEndpoint() == null)
+				throw new Exception("Ryu endpoint for region " + region.getName() + " not set!");
+			if (region.getOpenstackEndpoint() == null)
+				throw new Exception("OpenStack endpoint for region " + region.getName() + " not set!");
+		}
+
+		log.info("XIFI configuration checked.");
+	}
 }
